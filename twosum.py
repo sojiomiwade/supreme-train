55642a4ae85e6e7505bada3d2aa55ca08d3f9c0d
Soji Omiwade <omiwade@gmail.com>
2023-09-02 23:49:45 -0700

    add sorting

--- twosum.py
+++ twosum.py
@@ -18,6 +18,24 @@ class Solution:
             if nums[idx] in comp:
                 return [idx, comp[nums[idx]]]
             comp[target-nums[idx]] = idx
+
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        orignums = nums[:]
+        nums.sort()
+        thesum = nums[0] + nums[-1]
+        left, right = 0, len(nums) - 1
+        while thesum != target:
+            if thesum < target:
+                left += 1
+            else: 
+                right -= 1
+            thesum = nums[left] + nums[right]
+        res = []
+        for i in range(len(nums)):
+            if orignums[i] in (nums[left], nums[right]):
+                res.append(i)
+        return res
 '''
 comp = {7: 0, }
 retval = [1, 0]
