de8e5ea28ff1d16b25dc869ccca146f64a645358
Soji Omiwade <omiwade@gmail.com>
2023-09-07 11:40:33 -0700

    add tabulation

--- deletion_distance.py
+++ deletion_distance.py
@@ -60,4 +60,24 @@ def dd(s: str, t: str) -> int:
 s, t = 'ab', 'qcb'
 print(dd(s, t)) # 3
 s, t = 'some', 'thing'
+
+
+#tabulation
+def deletion_distance(str1, str2):
+  m, n = len(str1), len(str2)
+  d = [[0 for j in range(n+1)] for i in range(m+1)]
+  for j in range(n+1):
+    d[0][j] = j
+  for i in range(m+1):
+    d[i][0] = i
+  for i in range(1,m+1):
+    for j in range(1,n+1):
+      if str1[i-1] == str2[j-1]:
+        d[i][j] = d[i-1][j-1]
+      else:
+        d[i][j] = 1 + min(d[i-1][j], d[i][j-1])
+  return d[m][n]
+
+s, t = "heat", "hit"
+print(deletion_distance(s,t))
 print(dd(s, t)) # 9
