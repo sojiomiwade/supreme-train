3c249d59a3a5f742a2bc3f088fffc8a8ed0195e3
Soji Omiwade <omiwade@gmail.com>
2023-09-08 13:07:55 -0700

    25-30m, went a little fast on the coding and used zip; not even needed

--- one_away.py
+++ one_away.py
@@ -73,3 +73,76 @@ print(one_away(s, t)) # true, insert
 s, t = 'ab', ''
 print(one_away(s, t)) # true, insert
 
+#---again
+'''
+one edit away
+pale ple -- true
+pales pale -- true
+pale bale -- true
+pale bake -- false
+
+if s.len == t.len
+    is_one_replace_away()
+else
+    is_one_insert_away()
+
+def is_one_replace_away(s, t)
+    count places different.
+    return count == 1
+
+s,t:
+qple
+qpale
+^   j
+def is_one_insert_away(s, t)
+    if t.len < s.len
+        s, t = t, s
+    assert len.s = len.t - 1
+
+    loop until si not equal tj
+    inrement j
+
+    check si = tj till the end (end = is same for both)
+'''
+def oneaway(s, t):
+    if len(s) == len(t):
+        return is_one_replace_away(s, t)
+    return is_one_insert_away(s, t)
+
+def is_one_insert_away(s, t):
+    if len(t) < len(s):
+        s, t = t, s
+    if len(s) != len(t) - 1:
+        return False
+
+    pos = 0
+    for k in range(len(s)):
+        if s[k] != t[k]:
+            pos = k
+            break
+    else:
+        return True #ple and pleq
+    n = len(s)
+    for k in range(pos, n):
+        if s[k] != t[k+1]:
+            return False
+    return True # no other difference other than at pos found
+
+def is_one_replace_away(s, t):
+    count = 0
+    for cha, chb in zip(s, t):
+        if cha != chb:
+            count += 1
+    return count <= 1
+
+s, t = 'pale', 'ple' #-- true
+print(oneaway(s, t))
+s, t = 'pales', 'pale' #-- true
+print(oneaway(s, t))
+s, t = 'pale', 'bale' #-- true
+print(oneaway(s, t))
+s, t = 'pale', 'bake' #-- false
+print(oneaway(s, t))
+
+
+
