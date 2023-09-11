303fd588d90e20504e808e6e8bcf306827a42c40
Soji Omiwade <omiwade@gmail.com>
2023-09-11 12:19:03 -0700

    dud, too rushed and hence too complicated and so couldn't finish

--- needle_in_haystack.py
+++ needle_in_haystack.py
@@ -41,4 +41,50 @@ class Solution:
                 return i-m+1
             hsval %= mfac
         return -1
-        
+       
+
+#DUD. rushed? terrible!
+'''
+function int[] grep(string haystack, string needle)
+haystack = "aaabcdddbbddddabcdefghi"
+needle = "abc"
+[2,14]
+aaabcddd bbddddabcdefghi
+01234567 8901234
+  ^.   m
+m = haystack.length
+n = needle.length
+could use brute force in O(mn)
+could use a hash for O(m+n): compare sliding window of haystack with hash(abc)
+
+needlehash = hash(needle)
+num = num(haystack[:n-1])
+for lh_idx in [n-1:m+1] #n=3,m=8 -- 2:8
+    hash(haystack[lh_idx-n-1:lh_idx+1]) # k=7 -- [7-4:7] -- 3:7
+    if hashes are equal
+        res.append(lh_idx-n-1)
+print(res)
+'''
+from typing import List
+
+def num(pattern: str) -> int:
+
+def hash(pattern: str) -> int: 
+    ...
+# 256 -- 56 -- 567
+def adjust(hashval, nextdigit) -> int:
+    ...
+def grep(haystack: str, needle: str) -> List[int]:
+    base = 26
+    m, n = len(haystack), len(needle)
+    needlehash = hash(needle)
+    haystackhash = hash(haystack)
+    for lh_idx in [n-1:m+1] #n=3,m=8 -- 2:8
+        haystackhash = adjust()
+        hash(haystack[lh_idx-n-1:lh_idx+1]) # k=7 -- [7-4:7] -- 3:7
+        if hashes are equal
+            res.append(lh_idx-n-1)
+    print(res)
+    
+haystack, needle = 'abc', 'aaabcdddbbddddabcdefghi'
+grep(haystack, needle) #[2,14] 
