80ba5b6cc24906128dc758bf6738133793d2472b
Soji Omiwade <omiwade@gmail.com>
2023-09-23 15:43:12 -0700

    BFS approach

--- invert_tree.py
+++ invert_tree.py
@@ -16,3 +16,19 @@ class Solution:
         return root
 
 print(Solution().invertTree(None))
+
+
+# add BFS approach
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        return root        
+
