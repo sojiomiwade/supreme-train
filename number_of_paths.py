a7bfdfa966c6075fa999e615aa40451a3351e5c1
Soji Omiwade <omiwade@gmail.com>
2023-10-03 09:17:08 -0700

    memoizing constant time operations may seem simpler, but it bloats the code; remove it.

--- number_of_paths.py
+++ number_of_paths.py
@@ -1,13 +1,11 @@
 def num_of_paths_to_dest(n):
   def helper(row, col):
-    if (row,col) in memo:
-      return memo[row,col]
     if (row, col) == (n-1, n-1):
-      memo[row,col] = 1
       return 1
     if row > col or col > n-1:
-      memo[row,col] = 0
       return 0    
+    if (row,col) in memo:
+      return memo[row,col]
     memo[row,col] = helper(row + 1, col) + helper(row, col + 1)  
     return memo[row,col]
   memo = {}
