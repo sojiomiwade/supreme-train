3b299a2c7cb36f60ae2a436f3d59b60272e88c96
Soji Omiwade <omiwade@gmail.com>
2023-10-11 15:34:55 -0700

    still trying to get it working

---
+++ weave_back2back_linked_lists.py
@@ -0,0 +1,85 @@
+'''
+Given a linked list node, a1 such that
+a1 -> a2 -> ... -> an -> b1 -> b2 -> ... -> bn
+Weave this linked list so that it becomes
+a1 -> b1 -> a2 -> b2 -> ... -> an -> bn 
+'''
+from __future__ import annotations
+from typing import Optional
+
+class Node:
+    def __init__(
+        self, 
+        val: Optional[int]=None,
+        nxt: Optional[Node]=None,
+        ) -> None:
+        self.val = val
+        self.nxt = nxt
+
+    def print_list(self):
+        node = self
+        while node:
+            print(node.val, end=', ')
+            node = node.nxt
+        print()
+
+    '''
+    2 insertafter y
+    2->x
+    2 -> y -> x
+
+     = y,x
+    '''
+    def insertafter(self, node: Node):
+        self.nxt, node.nxt = node, self.nxt
+
+    '''
+    1 2
+        f
+      s
+    '''
+    def get_other_head(self) -> Node:
+        pslow = pfast = self
+        while pfast:
+            pfast = pfast.nxt
+            assert pfast
+            pfast = pfast.nxt
+            assert pslow
+            pslow = pslow.nxt
+        assert pslow
+        return pslow
+    '''
+    1 2 3 4 5 6 x
+    a c   b d 
+    
+    1 4 2 3 4...   5 6 x  
+        a c        b d
+
+    1 4 2 5 3 4 6 x
+            a c b d
+
+    1 4 2 5 3 6 4 x
+                a b  
+    '''
+    def weave(self) -> None:
+        otherhead = self.get_first_tail()
+        # print(otherhead.val) # 4 
+        a, b = head, otherhead
+        while a and b:
+            c, d = a.nxt, b.nxt
+            a.nxt, b.nxt = b, c
+            a, b = c, d
+        
+
+def init_list(n: int) -> Node:
+    dhead = Node()
+    for i in range(n, 0, -1):
+        dhead.insertafter(Node(i))
+    assert dhead.nxt
+    return dhead.nxt
+
+head = init_list(6)
+head.print_list() # 1 2 3 4 5 6
+head.weave()
+head.print_list() # 1 4 2 5 3 6
+
