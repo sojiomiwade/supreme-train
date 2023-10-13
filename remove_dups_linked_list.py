23b70c0c275e884ece60653db784c978fc22fa72
Soji Omiwade <omiwade@gmail.com>
2023-10-13 14:13:44 -0700

    remove dups now works for hashtable; working on constant space impl

--- remove_dups_linked_list.py
+++ remove_dups_linked_list.py
@@ -17,7 +17,7 @@ dt.next.next = newnode
 dt.next = newnode
 '''
 from __future__ import annotations
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
         
 class ListNode:
@@ -27,43 +27,84 @@ class ListNode:
         ) -> None:
         self.val = val
         self.next = next
-
-    def delete_next(self) -> ListNode:
-        delnode = self.next
-        assert delnode
-        self.next = delnode.next
-        return delnode
         
 class SLL:
     def __init__(self) -> None:
-        self.tail = ListNode()
-        self.head = ListNode()
+        self.head = ListNode('head')
+        self.tail = ListNode('tail')
         self.tail.next, self.head.next = self.head, self.tail
         
+    # h <-> t
+    #    5
+    # head next
     def append_to_tail(self, val: int) -> ListNode:
         last = self.tail.next
         assert last
-        last.next = ListNode(val)
-        self.tail.next = last.next
-        return last.next
+        newnode = ListNode(val)
+        last.next = newnode
+        newnode.next = self.tail
+        self.tail.next = newnode
+        return newnode
 
-    def as_list(self, node: ListNode) -> List[int]:
-        res, curr = [], node
+    def as_list(self) -> List[int]:
+        res, curr = [], self.head.next
         while curr is not self.tail:
             assert curr
             res += [curr.val]
             curr = curr.next
         return res
 
-    def remove_dups(self, node: Optional[ListNode]) -> None:
-        have = set()
-        curr = node
-        while node is not dta:
-            if node in have:
-                self.delete_next(prev)
-            node = node.next
+    def delete_next(self, node: ListNode) -> ListNode:
+        delnode = node.next
+        assert delnode and delnode is not self.tail
+        node.next = delnode.next
+        return delnode
 
-ListNode()
-# for x in range(1,10):
+    # 5 1 2 3 4 5
+    # d
+    def remove_dups(self) -> None:
+        have = set()
+        prev = self.head
+        curr = prev.next
+        while curr is not self.tail:
+            assert curr and prev
+            if curr.val in have:
+                self.delete_next(prev)
+            else:
+                prev = prev.next
+            have |= {curr.val}
+            curr = curr.next
+
+
+    def remove_dups2(self) -> None:
+        def find_node_by_val(val: int, prevstart: Optional[ListNode]) -> Optional[ListNode]:
+            assert prevstart
+            while prevstart.next is not self.tail:
+                if prevstart.next == val:
+                    return prevstart
+                prevstart = prevstart.next
+                assert prevstart
+            return None
+
+        prevstart = self.head
+        start = curr = self.head.next
+        while curr is not self.tail:
+            assert curr and curr.val is not None
+            prevstart = find_node_by_val(curr.val, prevstart)
+            if prevstart:
+                assert start
+                self.delete_next(prevstart)
+                found = startnode.next
+            else:
+                curr = curr.next
+
+from itertools import chain
+sll = SLL()
+for x in chain(range(5), range(5)):
+    sll.append_to_tail(x+1)
+print(sll.as_list())
+sll.remove_dups()
+print(sll.as_list())
+
 # todo remove the default spec of None for Node. it doesn't help much as there aren't a hundred places having Node(None, None)
 # and it makes clear what you are passing
