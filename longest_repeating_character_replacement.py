91ad7ab23a9672a490081a8aa24b816182911d3f
Soji Omiwade <omiwade@gmail.com>
2023-10-17 11:21:56 -0700

    rushed -> anxiety

--- longest_repeating_character_replacement.py
+++ longest_repeating_character_replacement.py
@@ -37,3 +37,65 @@ class Solution:
                 l += 1
         return len(s) - l
 
+
+
+
+'''
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+Example 1:
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+Example 2:
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+There may exists other ways to achieve this answer too.
+ 
+
+Constraints:
+1 <= s.length <= 105
+s consists of only uppercase English letters.
+0 <= k <= s.length
+
+k = 1
+AABABBA 
+^
+l
+  r
+therest has to be <= k
+as long as
+therest = r-l+1 - maxf <= k
+ maxf +   r-l+1
+get a longest length by advancing right, and k replacing. at a point you will violate: 
+once this is not the case move to the right
+'''
+from typing import Counter
+
+
+def kreplace(s: str, k: int) -> int:
+    left = maxf = 0
+    freq = Counter()
+    for right in range(len(s)):
+        freq[s[right]] += 1
+        if freq[s[right]] > maxf:
+            maxf = freq[s[right]]
+        if right - left + 1 - maxf > k: # 4 - 2 > 1
+            freq[s[left]] -= 1
+            left += 1
+    return len(s) - left 
+
+'''
+   r
+ABAB
+  l
+A:2 B:1 maxf:2
+'''
+s, k = 'ABAB', 2
+print(kreplace(s, k)) # 4
+
+
