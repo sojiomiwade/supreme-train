32d1a6db2c8788eb48042277113e038469389ae8
Soji Omiwade <omiwade@gmail.com>
2023-10-17 11:40:05 -0700

    ok

--- bracket_match.py
+++ bracket_match.py
@@ -69,3 +69,43 @@ print(bracketMatch(text)) # 0
 text = '())('
 print(bracketMatch(text)) # 2
 
+
+
+
+'''
+A string of brackets is considered correctly matched if every opening bracket in the string can be paired up with a later closing bracket, and vice versa. For instance, “(())()” is correctly matched, whereas “)(“ and “((” aren’t. For instance, “((” could become correctly matched by adding two closing brackets at the end, so you’d return 2.
+Given a string that consists of brackets, write a function bracketMatch that takes a bracket string as an input and returns the minimum number of brackets you’d need to add to the input in order to make it correctly matched.
+
+input:  text = “(()”
+output: 1
+
+input:  text = “(())”
+output: 0
+
+input:  text = “())(”
+output: 2
+
+())))(
+    ^
+
+())( => 2
+
+m:0 w:1
+if at zero(balanced), can never get matched if right is seen: add it to res
+but at start of iter, if left > 0, and see a right just decrement left and move on
+at the end return may_balance_left + will_never_balance_right
+'''
+
+def bracket_match(s: str) -> int:
+    may_balance_left = will_never_balance_right = 0
+    for ch in s:
+        if ch == ")":
+            if may_balance_left > 0:
+                may_balance_left -= 1
+            else:
+                will_never_balance_right += 1
+        else:
+            may_balance_left += 1
+    return may_balance_left + will_never_balance_right
+
+print(bracket_match('())(')) # 2
