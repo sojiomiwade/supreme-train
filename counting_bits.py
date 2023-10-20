8883da1e518f2bcf7d3c1ac8e99a400285138fc0
Soji Omiwade <omiwade@gmail.com>
2023-10-19 22:30:45 -0700

    ok

--- counting_bits.py
+++ counting_bits.py
@@ -41,3 +41,25 @@ class Solution:
         for i in range(1, n + 1):
             res[i] = res[i >> 1] + i % 2
         return res
+
+
+
+'''
+took a while (and was sleepy)
+but did it myself in < 30m
+'''
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        '''
+        3
+        0112
+        0123
+        '''
+        res = [0 for _ in range(n+1)]
+        res[0] = 0
+        for x in range(1,n+1):
+            if x % 2 == 1:
+                res[x] = res[x - 1] + 1
+            else:
+                res[x] = res[x // 2]
+        return res
