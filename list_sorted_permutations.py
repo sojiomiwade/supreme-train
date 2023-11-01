509924ae444eeef778a9dcc62240f62c0e3a21fd
Soji Omiwade <omiwade@gmail.com>
2023-11-01 09:09:06 -0700

    lebowit ok

--- list_sorted_permutations.py
+++ list_sorted_permutations.py
@@ -31,3 +31,44 @@ def list_sorted(k: int) -> None:
     helper([])
 
 list_sorted(2) # a, b, c,d,e
+'''
+print all strings of length k where the characters are in sorted order.
+
+generate each string of length k, then check if it is sorted
+'''
+def print_sorted_strings(k: int) -> None:
+    '''
+    aaaa
+    aaab
+    prefix method
+    abcd,
+        bcd,a
+
+        acd,b
+        abc,d
+    
+    4,.
+        3,a
+           2,aa 
+        3,b
+            2,ba
+            2,bb
+        3,c
+        ...
+    '''
+    def _sorted(s: str) -> bool:
+        for i in range(1, len(s)-1):
+            if ord(s[i-1]) > ord(s[i]):
+                return False
+            return True
+
+    def _print_sorted_strings(rem: int, prefix: str) -> None:
+        if rem == 0:
+            if _sorted(prefix):
+                print(prefix)
+            return
+        for i in range(26):
+            _print_sorted_strings(rem - 1, prefix + chr(i+ord('a')))
+    _print_sorted_strings(k, '')
+
+print_sorted_strings(5) #aaaa, aaab, ...
