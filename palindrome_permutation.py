c8ad6747cef58466d79e9f04ac415012472262ff
Soji Omiwade <omiwade@gmail.com>
2023-11-01 09:28:49 -0700

    lebowit ok; missed asking about case

--- palindrome_permutation.py
+++ palindrome_permutation.py
@@ -71,3 +71,51 @@ print(is_pal_perm(s)) # false
 s = 'coTa cot'
 print(is_pal_perm(s)) # true
 
+
+
+
+'''
+Given a string write a method to decide if it is a permutation of a palindrome
+abcd - f
+aabaa - or any of its permutations - t
+^
+1 character can have odd freq
+all others must have even number of freq
+hash table of freq, and check above conditions
+can use a bitmap
+aaaa
+1010 <-- 0 means even
+then only 1 bit can be set in the map
+aabaac
+
+0000110 -> false
+0000101
+-------
+0000100
+-------
+can check if only 1 bit is set with
+n & (n-1) == 0  implies 1 or 0 bits set
+note 0 & -1 == 0
+
+
+'''
+
+def ispalperm(s: str) -> bool:
+    vec = 0
+    for ch in s:
+        pos = ord(ch) - ord('a')
+        vec ^= 1 << pos
+    return vec & (vec-1) == 0
+    
+s='aaaa'
+print(ispalperm(s)) # t
+s='aaaabbb'
+print(ispalperm(s)) # t
+s='aaaab'
+print(ispalperm(s)) # t
+s='aabaa'
+print(ispalperm(s)) # t
+s='aaaabc'
+print(ispalperm(s)) # f
+s='aaqraa'
+print(ispalperm(s)) # f
