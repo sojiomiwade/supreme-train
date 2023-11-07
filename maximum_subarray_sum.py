4ea1486f5ffb35f528d95913e469ebf9fb8281a3
Soji Omiwade <omiwade@gmail.com>
2023-11-07 07:44:03 -0800

    w and t not so good

--- maximum_subarray_sum.py
+++ maximum_subarray_sum.py
@@ -44,3 +44,32 @@ class Solution:
             rs = max(nums[i], nums[i] + rs)
             maxsum = max(maxsum, rs)
         return maxsum
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        '''
+        -2,1,-3,4,-1,2,1,-5,4
+         ^
+        -2
+        -2
+
+        -2,1,-2,4, 3,5,6, 1,5
+        bs=6
+
+        [5,4,-1,7,8]
+        5 9 8 15 23
+        bs=9
+
+        -2,1,-3,4
+        c=-2
+        bs=1
+        '''
+        # DP style start
+        return maxSubArray(nums[])
+        # DP style end
+
+        bs = curr = nums[0]
+        for i in range(1, len(nums)):
+            curr = max(nums[i], nums[i]+curr)
+            bs = max(bs, curr)
+        return bs
+    
