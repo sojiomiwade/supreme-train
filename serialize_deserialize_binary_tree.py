bb23187538ed458da50a1d61ac1404d55c1b561e
Soji Omiwade <omiwade@gmail.com>
2023-11-10 22:25:00 -0800

    lebowit ignored some especially wt

---
+++ serialize_deserialize_binary_tree.py
@@ -0,0 +1,61 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+'''
+               8
+             /   \
+            2     5
+           /       \
+          3         8
+        823### 5#8##
+        8
+      2.   5
+    3.       8     
+'''
+class Codec:
+
+    def serialize(self, root):
+        def preorder(root):
+            if not root:
+                prel.append('#')
+            else:
+                prel.append(str(root.val))
+                preorder(root.left)
+                preorder(root.right)
+        prel = []
+        preorder(root)
+        return ' '.join(prel)
+
+    def deserialize(self, data):
+        '''
+               8
+                 \
+                  5
+                   \
+                    8
+        8#5#8##
+                    8
+                n       5
+                      n   8
+                         n.  n
+        '''
+        def _deser():
+            val = next(vals)
+            if val == '#':
+                return None
+            root = TreeNode(int(val))
+            root.left = _deser()
+            root.right = _deser()
+            return root
+        vals = iter(data.split())
+        return _deser()
+
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
