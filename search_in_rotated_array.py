e5ec92b1b38b7a167414165263ed89996b25af70
Soji Omiwade <omiwade@gmail.com>
2023-11-11 09:46:56 -0800

    missed inclusivity for range because lebowit missed ebowit

--- search_in_rotated_array.py
+++ search_in_rotated_array.py
@@ -8,4 +8,35 @@ func call(lo, hi, x)
 
   if slo<=x<=shi
     return call(slo, shi,x)
-  return call(ulo, uhi,x)
+  return call(ulo, uhi,x)'''
+x = 2
+5 6 0 1 | 2 3 4
+2 3 | 4 
+2 | 3
+slo,shi: 01
+ul, uhi: 22
+look in sorted half for the element; use that to pick the half that has the element. 
+lg n time + constant space 
+'''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        lo,hi=0,len(nums)-1
+        while lo < hi:
+            mi = lo + (hi-lo)//2
+            slo,shi = lo,mi 
+            ulo,uhi=mi+1,hi
+            if nums[lo]>nums[mi]: 
+                slo,shi=mi+1,hi
+                ulo,uhi=lo,mi
+            if nums[slo]<=target<=nums[shi]:
+                lo,hi=slo,shi
+            else:
+                lo,hi=ulo,uhi
+        return -1 if nums[lo]!=target else lo
+'''
+[4,5,6,7,0,1,2]
+ 0 1 2 3 4 5 6
+0 6
+4 6 0,1,2
+6 6
+'''
