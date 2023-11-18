abb1205791a6aa9ed17765406a9cc38f8d7d7c43
Soji Omiwade <omiwade@gmail.com>
2023-11-18 15:54:16 -0800

    thanks to lookup, actually dont need vis

--- clone_graph.py
+++ clone_graph.py
@@ -49,4 +49,57 @@ class Solution:
                 vis.add(nb)
                 _clonegraph(nb,nbc)
 
+        return nc"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+1 - 2
+|   |  
+3 - 4
+
+1 - 2
+|   |
+3 - 4
+
+can use DFS. 
+clone the curr node. then
+seems the parent is going to be passed so that 
+
+2--4
+
+
+1---2
+|   |
+4---3
+clookup={1:1c,2:2c,}
+1<--2
+^   ^
+    v 
+4<->3
+cg(2,2c)
+
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        def _clonegraph(u,uc):
+            for v in u.neighbors:
+                if v not in clookup:
+                    clookup[v]=Node(v.val)
+                    _clonegraph(v,clookup[v])
+                uc.neighbors.append(clookup[v])
+
+        if not node:
+            return None
+        nc=Node(node.val)
+        clookup={node:nc}
+        for nb in node.neighbors:
+            if nb not in clookup:
+                clookup[nb]=Node(nb.val)
+                _clonegraph(nb,clookup[nb])
+            nc.neighbors.append(clookup[nb])
         return nc
