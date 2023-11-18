3729a78a0a50da140dd032416f6087921356d64e
Soji Omiwade <omiwade@gmail.com>
2023-11-18 15:32:39 -0800

    t missing so took much longer

---
+++ clone_graph.py
@@ -0,0 +1,52 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+1 - 2
+|   |  
+3 - 4
+
+1 - 2
+|   |
+3 - 4
+
+can use DFS. 
+clone the curr node. then
+seems the parent is going to be passed so that 
+
+2--4
+
+1--3
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        def _clonegraph(u,uc):
+            for v in u.neighbors:
+                if v not in clookup:
+                    clookup[v]=Node(v.val)
+                vc=clookup[v]
+                uc.neighbors.append(vc)
+                if v not in vis:
+                    vis.add(v)
+                    _clonegraph(v,vc)
+
+        if not node:
+            return None
+        vis={node}
+        nc=Node(node.val)
+        clookup={node:nc}
+        for nb in node.neighbors:
+            if nb not in clookup:
+                clookup[nb]=Node(nb.val)
+            nbc=clookup[nb]
+            nc.neighbors.append(nbc)
+            if nb not in vis:
+                vis.add(nb)
+                _clonegraph(nb,nbc)
+
+        return nc
