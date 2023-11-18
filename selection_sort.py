528d09c161f868e8acfbd5959eb05e6ecb021853
Soji Omiwade <omiwade@gmail.com>
2023-11-17 16:53:49 -0800

    lebcowit ok

--- selection_sort.py
+++ selection_sort.py
@@ -33,3 +33,37 @@ for _ in range(5):
     n = 10
     arr = [randrange(n) for _ in range(n)]
     assert sorted(arr) == ssort(arr)
+'''
+implement selection sort
+we haave an array a
+let l be the exclusive boundary of sorted elements
+then l=0
+then loop n-1 times (l==0..n-2) starting from l always finding the idx with the smallest value
+do a swap with l, then increment l
+
+  0 1 2 5 3
+    l   c         
+
+complexity: time and space: O(n**2) and O(1)
+
+2 3 5
+  l r
+mi,mval=2,3
+'''
+
+def ss(a):
+    n=len(a)
+    for l in range(n-1):
+        mi,mval=l,a[l]
+        for r in range(l+1,n):
+            if a[r]<mval:
+                mi,mval=r,a[r]
+        a[l],a[mi]=a[mi],a[l]
+
+import random
+n=10
+a=[i for i in range(n)]
+for _ in range(n):
+    random.shuffle(a)
+    ss(a)
+    assert(a == sorted(a))
