8896aaa315f589045377a66b3b4e81da54e863bd
Soji Omiwade <omiwade@gmail.com>
2023-11-19 10:23:41 -0800

    lebcowit ok on paper

--- serialize_deserialize_binary_tree.py
+++ serialize_deserialize_binary_tree.py
@@ -58,4 +58,41 @@ class Codec:
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        def ser(root, l):
+            if root:
+                l.append(str(root.val))
+                ser(root.left,l)
+                ser(root.right,l)
+            else:
+                l.append('#')
+            return l
+
+        return ' '.join(ser(root,[]))        
+
+    def deserialize(self, data):
+        def deser():
+            r=None
+            x=q.popleft()
+            if x!='#':
+                r=TreeNode(int(x))
+                r.left=deser()
+                r.right=deser()
+            return r
+
+        q=deque(data.split())
+        return deser()        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
