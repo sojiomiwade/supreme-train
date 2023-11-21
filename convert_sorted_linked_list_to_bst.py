552d7af819d04b73c50165cd4c387827ab66ae78
Soji Omiwade <omiwade@gmail.com>
2023-11-21 14:42:20 -0800

    stressed

--- convert_sorted_linked_list_to_bst.py
+++ convert_sorted_linked_list_to_bst.py
@@ -82,3 +82,96 @@ def create_balanced_bst(head: ListNode) -> Optional[TreeNode]:
 
 head = ListNode(1, None)
 print(create_balanced_bst(head))
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+'''
+can try to enumerate all possible BST at ~2**n
+better to see if sorted list can help
+
+1 2 3 4 5 6
+ht=1,5
+                    3 123456
+        1 12                           5 456
+ 1n-null     22 2                44 4       66 6
+           2n    n2            4n.   n4
+
+get the median and make it root
+set left to l2b(head,prev)
+set right to l2b(next,tail)
+return root
+
+find-median: 
+1
+s
+f
+return prev because we need it, can derive median then
+
+1
+f
+s
+                123 (2)
+             11            33
+             
+p=null
+m=1
+mn=null
+
+ 1234
+ps
+ f
+ht=11
+p=1
+m=2
+mn=2
+             2
+           /   \
+          1     3
+                 \
+                   4
+'''
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        def l2b(h,t):
+            if not h or not t:
+                return None
+            p=mp(h,t)
+            mid=h
+            if p: mid=p.next
+            midnext=mid.next
+            if mid is t: midnext=None
+            root=TreeNode(mid.val)
+            root.left=l2b(h,p)
+            root.right=l2b(midnext,t)
+            return root
+
+        '''
+         1234
+         ps
+            f
+        '''
+        def mp(h,t): #14
+            f=s=h
+            p=None
+            while f is not t:
+                f=f.next
+                if f is t:
+                    break
+                f=f.next
+                p=s
+                s=s.next
+            return p
+
+        cur=head
+        prev=None
+        while cur:
+            prev,cur=cur,cur.next
+        return l2b(head, prev)
