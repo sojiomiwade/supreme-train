c907be79174de307109f6a6c1c18fc9e7e378621
Soji Omiwade <omiwade@gmail.com>
2023-11-28 13:44:29 -0800

    this method wont put different types in the stack, but is less intuitive

--- inorder_traversal.py
+++ inorder_traversal.py
@@ -38,4 +38,38 @@ class Solution:
                 st.append(root.right)
                 st.append(root.val)
                 st.append(root.left)
+        return res# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+'''
+           1
+        /     \
+       4       2
+        \     /  \
+         7   3    5
+
+res : 471325
+root: n
+st  : 
+
+while st has elements
+    go along left slide until until next left is null
+    root = pop from stack
+    put root into result
+    push root.right to stack if right isn't null
+
+'''
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        st,res=[],[]
+        while st or root:
+            while root:
+                st.append(root)
+                root=root.left
+            root=st.pop()
+            res.append(root.val)
+            root=root.right
         return res
