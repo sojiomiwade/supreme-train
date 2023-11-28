fae6acc36cfb4e3d544ae8660062983d4a80f28d
Soji Omiwade <omiwade@gmail.com>
2023-11-28 07:44:07 -0800

    t missing in clebcowit

--- threesum.py
+++ threesum.py
@@ -63,4 +63,45 @@ class Solution:
                     b+=1
                 else:
                     c-=1
-        return res        
+        return res        '''
+-1,0,1,2,-1,-4
+-4 0 1 2 -1 -1
+s=2 * -1 = -2
+
+1
+{}
+
+
+
+res={ [0 -1 1] []}
+
+optimizations:
+  * should remove duplicate numbers (with a set)
+  to avoid dealing with turning each tuple into a set
+  even then still need to use some kind of a sorted list
+  * unless we also sort the main list (as well as remove dups)
+  * now two sum should only go forward though
+  * once nums[si] is not negative, can break
+-4 -1 0 1 2
+have={0,-1}
+sum=1-1
+
+'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        def find_twosum(si):
+            sum=-nums[si]
+            have=set()
+            for i in range(si+1,n):
+                if sum-nums[i] in have:
+                    res.add(tuple(sorted([nums[i],sum-nums[i],-sum])))
+                have.add(nums[i])
+
+        res=set()
+        nums.sort()
+        n=len(nums)
+        for i in range(n):
+            if nums[i]>0:
+                break
+            find_twosum(i)
+        return list(res)
