20ea0e1a8d40bd4caf12f4c5006026e23509ba96
Soji Omiwade <omiwade@gmail.com>
2023-11-29 10:30:15 -0800

    lebcowit ok but took 1h to finally get longest common subsequence

--- longest_common_subsequence.py
+++ longest_common_subsequence.py
@@ -106,3 +106,73 @@ class Solution:
 
         m,n,dp=len(text1),len(text2),{}
         return _lcs(0,0)
+'''
+longest common subsequence of two strings
+aqce
+abcde
+
+ce
+bcde
+
+lcs(s,t)=
+    1+lcs(s[1:],t[1:])                if s[0]==t[0]
+    max(lcs(s[1:],t),lcs(s,t[1:]))    otherwise
+
+  a b c d e
+a 3 . . . . 
+q . 2 2 1 1
+c . 2 2 1 1
+e . 1 1 1 1
+
+a
+ab
+dp
+   a   b
+a  1   -1
+    a   b
+b   1   1
+
+0 1
+'''
+def lcs(s: str, t: str) -> str:
+    def lcs(si:int, ti:int)->int:
+        if si==m or ti==n:
+            return 0
+        if dp[si][ti] != -1: #00
+            return dp[si][ti]
+        if s[si]==t[ti]:
+            dp[si][ti]=1+lcs(si+1,ti+1)
+            return dp[si][ti]
+        dp[si][ti]=max(lcs(si,ti+1),lcs(si+1,ti))
+        return dp[si][ti]
+
+    m,n=len(s),len(t)
+    dp=[[-1 for _ in range(n)] for _ in range(m)]
+    lval=lcs(0,0)
+    res=[]
+    for i in range(m):
+        for j in range(n):
+            print(f'{dp[i][j]:3d}',end='')
+        print()
+    si=ti=0
+    while si<m and ti<n:
+        if s[si]==t[ti]:
+            res.append(s[si])
+            si+=1
+            ti+=1
+        else:
+            dpt=dps=float('-inf')
+            if si<m-1:
+                dps=dp[si+1][ti]
+            if ti<n-1:
+                dpt=dp[si][ti+1]
+            if dps<dpt:
+                ti+=1
+            else:
+                si+=1
+    return ''.join(res)
+s,t='aqce','abcde'
+print(lcs(s,t)) #ace
+
+# s,t='ace','abcde'
+# print(lcs(s,t)) #ace
