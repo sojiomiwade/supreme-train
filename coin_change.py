4a9d23f5d8abd3a82d515e2bee146548a05ba8fb
Soji Omiwade <omiwade@gmail.com>
2023-12-05 14:19:43 -0800

    tabulation

--- coin_change.py
+++ coin_change.py
@@ -23,4 +23,19 @@ class Solution:
             
         dp=[-1 for _ in range(1+amount)]
         ans=coinChange(amount)
-        return -1 if ans==float('inf') else ans
+        return -1 if ans==float('inf') else ansclass Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        COIN_MAX=float('inf')
+        dp=[COIN_MAX for _ in range(amount+1)]
+        for coin in coins:
+            if coin<=amount:
+                dp[coin]=1
+        dp[0]=0
+
+        for subamount in range(1+amount):
+            if dp[subamount]==COIN_MAX:
+                for coin in coins:
+                    if subamount-coin>0:
+                        dp[subamount]=min(dp[subamount],dp[subamount-coin])
+                dp[subamount]+=1
+        return dp[amount] if dp[amount]!=COIN_MAX else -1
