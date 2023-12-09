1f10a08ba5d02c30a819d9b26b26a0835a290629
Soji Omiwade <omiwade@gmail.com>
2023-12-09 04:58:39 -0800

    o and t had hiccups; translating english to is a range sorted was wrong

--- search_in_rotated_array.py
+++ search_in_rotated_array.py
@@ -39,4 +39,27 @@ class Solution:
 0 6
 4 6 0,1,2
 6 6
-'''
+'''class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        '''
+        3 1
+        l
+          r
+        m  
+        '''
+        l,r=0,len(nums)-1
+        while l<=r:
+            m=l+(r-l)//2
+            if nums[m]==target:
+                return m
+            if nums[l]<=nums[m]:
+                if nums[l]<=target<nums[m]:
+                    r=m-1
+                else:
+                    l=m+1
+            else:
+                if nums[m]<target<=nums[r]:
+                    l=m+1
+                else:
+                    r=m-1
+        return -1
