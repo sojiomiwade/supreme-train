50d88288cfe3c3e76e13555ec7ad80d54fc387c5
Soji Omiwade <omiwade@gmail.com>
2023-12-19 07:58:09 -0800

    lebcowit mostly ok

--- find_duplicates.py
+++ find_duplicates.py
@@ -51,3 +51,61 @@ def find_duplicates(arr1, arr2):
       b += 1
   return res
   '''
+'''
+n~m => we go for n+m 
+1 2 3 4 5 6 7
+              i
+      j
+3 6 7 8 20
+res = 3 6 7
+if elemens of idxs are the same, advance both pointers
+othewise, advance the pointer to the smaller value
+
+1 2 3
+    i
+    j
+0 2
+res = 2 
+'''
+def find_duplicates(arr1, arr2):
+  N, M = len(arr1), len(arr2)
+  i = j = 0
+  res = []
+  while i < N and j < M:
+    if arr1[i] == arr2[j]:
+      res.append(arr1[i])
+      i += 1
+      j += 1
+    elif arr1[i] < arr2[j]:
+      i += 1
+    else:
+      j += 1
+  return res
+
+# now we go for binary search on the bigger
+'''
+1 2 3 4 5 6 7
+    l
+    m    
+    h
+3 7
+k
+res = 3
+complexity = min(N,M) * lg(max(N,M))
+'''
+def find_duplicates(arr1, arr2):
+  res = []
+  if len(arr1) > len(arr2):
+    arr1, arr2 = arr2, arr1
+  for key in arr1:
+    lo, hi = 0, len(arr2)-1
+    while lo <= hi:
+      mi = lo + (hi - lo) // 2
+      if arr2[mi] == key:
+        res.append(key)
+        break
+      if arr2[mi] > key:
+        hi = mi - 1
+      else:
+        lo = mi + 1
+  return res
