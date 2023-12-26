fbdd123b35ad4eed3d4952e74274b59e5557ee45
Soji Omiwade <omiwade@gmail.com>
2023-12-26 11:25:25 -0800

    boyer-moore

--- majority_element.py
+++ majority_element.py
@@ -1,16 +1,24 @@
 '''
-2 2 1 1 1 2 2
-1 1 1 2 2 2 2
-0 1 2 3 4 5 6
-7//2 = 3
-
-
-1 1 2 2 2 2
-0 1 2 3 4 5
-6//2=3
-
+? 1 2 2 2 3 2 5
+        ^
+have,count=1,0
+2 2 2 2 1 3 5
+        ^
+you look back (and count helps with this)
+count=0 => in the past, all is balanced
+count=x => there is a majority, m, and freq(m)-freq(not m)=x 
+note that count is defined (as zero) before you start, since we have none
+return cur
 '''
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-       nums.sort()
-       return nums[(len(nums))//2] 
+        count=0
+        for i in range(len(nums)):
+            if count==0:
+                have=nums[i]
+            if nums[i]==have:
+                count+=1
+            else:
+                count-=1
+        assert 'have' in locals()
+        return have
