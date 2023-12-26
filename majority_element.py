15ffb9552045210587d39ab48211555b32c34a6b
Soji Omiwade <omiwade@gmail.com>
2023-12-26 11:59:31 -0800

    lebcowit failed since t missing

--- majority_element.py
+++ majority_element.py
@@ -1,24 +1,33 @@
 '''
-? 1 2 2 2 3 2 5
-        ^
-have,count=1,0
 2 2 2 2 1 3 5
         ^
-you look back (and count helps with this)
-count=0 => in the past, all is balanced
-count=x => there is a majority, m, and freq(m)-freq(not m)=x 
-note that count is defined (as zero) before you start, since we have none
-return cur
+153
+
+zy5x5    <--we make 00101 since {x,y,z}<5 
+approach: get the max freq, and make a number having only those bit freq equal to maxfreq
+
+when -ve, we have
+11111| 1|5xyzz5
+      32
+   0000000000|1|100001      <---wrong cuz it's positive
+ | 1111111111|1|000000
+   1111111111|
+111110
+000010
+1111
 '''
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count=0
-        for i in range(len(nums)):
-            if count==0:
-                have=nums[i]
-            if nums[i]==have:
-                count+=1
-            else:
-                count-=1
-        assert 'have' in locals()
-        return have
+        res=0
+        mask=1
+        n=len(nums)
+        for i in range(32):
+            freq=0
+            for x in nums:
+                freq+=bool(mask&x)
+            if freq>n//2:
+                res|=mask
+            mask<<=1
+        if (1<<31)&res != 0: #bit 31 is set => number is negative
+            res|=-1<<31
+        return res
