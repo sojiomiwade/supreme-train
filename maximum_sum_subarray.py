e5e934a0f056691f94b5e75b184074f51519b90b
Soji Omiwade <omiwade@gmail.com>
2023-12-27 12:22:44 -0800

    lebcowit missing certain t cases caused multiple runtime errors

--- maximum_sum_subarray.py
+++ maximum_sum_subarray.py
@@ -1,17 +1,15 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        def msa(lo,hi):
-            if lo>hi:
-                return float('-inf')
-            mi=lo+(hi-lo)//2
-            lsum=msa(lo,mi-1)
-            rsum=msa(mi+1,hi)
-            lhalf=rhalf=ml=mr=0
-            for i in range(mi-1,lo-1,-1):
-                lhalf+=nums[i]
-                ml=max(ml,lhalf)
-            for i in range(mi+1,hi+1):
-                rhalf+=nums[i]
-                mr=max(mr,rhalf)
-            return max(lsum,rsum,ml+nums[mi]+mr)
-        return msa(0,len(nums)-1)
+        l=rsum=0
+        msum=float('-inf')
+        bl=br=None
+        for i,x in enumerate(nums):
+            if rsum<0:
+                rsum=0
+                l=i
+            rsum+=x
+            if rsum>msum:
+                msum=rsum
+                bl,br=l,i
+        assert msum==sum(nums[i] for i in range(bl,br+1))
+        return msum
