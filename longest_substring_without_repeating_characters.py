de3ed14641980fe2e7ebb6ea07c5d0b1a87e5d9c
Soji Omiwade <omiwade@gmail.com>
2023-12-28 09:43:28 -0800

    use sliding window, lebcowit ok

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -1,27 +1,16 @@
-'''
-bacabcbb
- r
-l
-rl,rr
-loc = initally -1s
-    now [1 0 2]
-if a is in dictmove l to where a is, but a may be gone, so l = max(loc[a],l)
-
-         l
-               r
-s  : p w w k e w
-idx: 0 1 2 3 4 5
-loc: 0 2 2 3 4 n
-'''
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        loc=[-1 for _ in range(256)]
-        l=rl=0
-        rr=-1
+        count=[0 for _ in range(256)]
+        l=size=0
         for r in range(len(s)):
-            sr=ord(s[r])
-            l=max(l,1+loc[sr])
-            loc[sr]=r
-            if r-l>rr-rl:
-                rl,rr=l,r
-        return rr-rl+1
+            och=ord(s[r])
+            count[och]+=1
+            if count[och]==1:
+                size+=1
+            if r-l+1>size:
+                olch=ord(s[l])
+                count[olch]-=1
+                if count[olch]==0:
+                    size-=1
+                l+=1
+        return len(s)-l
