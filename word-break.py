ca8d2a182db7651ec0c5ae158570cb10ba9eea94
Soji Omiwade <omiwade@gmail.com>
2023-12-30 06:59:41 -0800

    tabulation; lebcowit ok at over 1h

--- word-break.py
+++ word-break.py
@@ -1,19 +1,11 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        def wb(k: int) -> bool:
-            if k==n:
-                return True
-            
-            if k in dp:
-                return dp[k]
-            breaks=False
-            for word in wordDict:#
-                m=len(word)
-                if n-k>=m and s[k:k+m]==word:
-                    breaks=breaks or wb(k+m)
-            dp[k]=breaks
-            return breaks
-
-        dp={}
-        n=len(s)
-        return wb(0)
+        n,m=len(s),len(wordDict)
+        dp=[False for _ in range(n+1)]
+        dp[0]=True
+        for i in range(1,n+1):
+            for j in range(m):
+                wdj=wordDict[j]
+                mj=len(wdj)
+                dp[i]=dp[i] or (s[i-mj:i]==wdj and dp[i-mj])
+        return dp[n]
