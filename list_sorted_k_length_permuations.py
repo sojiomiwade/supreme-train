86d8599f910b0d747aba35b4a62502ac841af59c
Soji Omiwade <omiwade@gmail.com>
2024-01-01 06:50:01 -0800

    lebcowit ok

--- list_sorted_k_length_permuations.py
+++ list_sorted_k_length_permuations.py
@@ -1,31 +1,37 @@
 '''
 find all k-length strings alphabetically sorted, lower case only
 
-complexity:
-time: ck * c**(k-1) * c**2
+aaaa
+aaab
+aaac
+   ^
+at each idx, pass 
+each child idx, can only do parent idx or greater   
+i.e., parent passes the start idx
 '''
 from typing import List
 
 
-def ak(lastk: int) -> List[str]:
-    def issorted(s: str) -> bool:
-        for prev, curr in zip(s, s[1:]):
-            if ord(prev) > ord(curr):
-                return False
-        return True
+def ak(k: int) -> List[str]:
+    def ak(cur_buf_idx: int, start_ascii_idx: int) -> None:
+        if cur_buf_idx==k:
+            res.append(''.join(buf))
+        else:
+            for i in range(start_ascii_idx,MAX_ASCII_IDX):
+                buf[cur_buf_idx]=chr(ord('a')+i)
+                ak(cur_buf_idx+1,i)
+    buf=['' for _ in range(k)]
+    MAX_ASCII_IDX=26
+    res=[]
+    ak(0,0)
+    return res
 
-    alpha_len = 3
-    aks = [[None], [chr(i + ord('a')) for i in range(alpha_len)]]
-    for k in range(2, lastk + 1):
-        res = set()
-        for s in aks[k-1]:
-            for j in range(len(s)):
-                for i in range(alpha_len):
-                    ch = chr(i + ord('a'))
-                    cand = s[:j] + ch + s[j:]
-                    if issorted(cand):
-                        res |= {cand}
-        aks.append(list(res))
-    return aks[-1]
-
-print(ak(3)) #
+ans=ak(3)
+assert len(ans)==len(set(ans))
+for s in ans:
+    for cha,chb in zip(s,s[1:]):
+        assert ord(cha)<=ord(chb)
+#print(ans) # [aaa,aab,...,aaz,...,zzz]
+for x in ans:
+    print(x)
+print(len(ans))
