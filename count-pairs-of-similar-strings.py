fc58fdfda2ff987213f5b309b0a710308328976c
Soji Omiwade <omiwade@gmail.com>
2024-01-03 12:23:31 -0800

    use factorial means, more efficient, but can be made asymptotically and runtime more so

--- count-pairs-of-similar-strings.py
+++ count-pairs-of-similar-strings.py
@@ -1,8 +1,23 @@
+'''
+a b a d e a
+
+abca -> {a,b,c}
+cba -> {a,b,c}
+{a,b,c}
+{b}
+
+abc: 2 => 2 choose 2 = 1
+bab: 3 => 3 choose 2 = 3
+ans = 4
+
+'''
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
+        freq=Counter()
+        for word in words:
+             freq[''.join(sorted(set(word)))]+=1
         res=0
-        for (i, worda) in enumerate(words):
-            for (j,wordb) in enumerate(words):
-                if i<j and set(worda)==set(wordb):
-                    res+=1
+        for wfreq in freq.values():
+            if wfreq>1:
+                res+=math.factorial(wfreq) // math.factorial(wfreq-2) // 2
         return res
