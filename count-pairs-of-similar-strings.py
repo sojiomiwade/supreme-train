23d0ef231138e30330dfa7da3b194121ad58de33
Soji Omiwade <omiwade@gmail.com>
2024-01-04 07:40:34 -0800

    get keey via bitmap operations

--- count-pairs-of-similar-strings.py
+++ count-pairs-of-similar-strings.py
@@ -10,13 +10,16 @@ abc: 2 => 2 choose 2 = 1
 bab: 3 => 3 choose 2 = 3
 ans = 4
 
+bbaaac
 '''
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
         freq=Counter()
         res=0
         for word in words:
-            key = ''.join(sorted(set(word)))
+            key=0
+            for ch in word:
+                key|=(1<<(ord(ch)-ord('a')))
             res+=freq[key]
             freq[key]+=1
         return res
