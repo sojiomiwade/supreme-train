fdefabbd5cd38a09722fb2339065ee3394f4f500
Soji Omiwade <omiwade@gmail.com>
2024-01-06 08:19:28 -0800

    better dictionary understanding with recursion

--- add_and_search_words.py
+++ add_and_search_words.py
@@ -1,47 +1,31 @@
 class WordDictionary:
 
     def __init__(self):
-        self.root={}
+        self.root = {}
 
     def addWord(self, word: str) -> None:
-        cur = self.root
-        for let in word:
-            if let not in cur:
-                cur[let]={}
-            cur=cur[let]
-        cur['done']='done'
-
-    def search(self, word: str) -> bool:
-        '''
-                .  wi=3, cur={d's dict} root={b:{a:{d:{done:done}}}, c:{o:{t:{done:done}}}}
-            o       b   c
-            r       a   o
-            .       d   t
-                    .   .
-        b.d and bad true
-        bod = false
-
-                    . wi=1,word=ab,cur=a dict root={a:{done:done}} 
-                a
-                done       
-        '''
-        def search(cur: Dict, wi: int) -> bool:
-            if wi==len(word):
-                if 'done' in cur:
-                    return True
-                return False
-            if word[wi]=='.':
-                found=False
-                for dictlet in cur:
-                    if dictlet != 'done':
-                        found=found or search(cur[dictlet],wi+1)
-                return found
-            if word[wi] in cur:
-                return search(cur[word[wi]],wi+1)
-            return False
-
-        return search(self.root,0)
+        cur=self.root
+        for ch in word:
+            if ch not in cur:
+                cur[ch]={}
+            cur=cur[ch]
+        cur[None]={}
         
+    def search(self, word: str) -> bool:
+        def search(cur: Dict, idx: int) -> bool:
+            if idx==len(word):
+                return None in cur
+            ch=word[idx]
+            if ch != '.':
+                if ch in cur:
+                    return search(cur[ch],idx+1)
+                return False
+            if ch=='.':
+                found=False
+                for curch in cur.values():
+                    found=found or search(curch,idx+1)
+                return found
+        return search(self.root,0)
 
 
 # Your WordDictionary object will be instantiated and called as such:
