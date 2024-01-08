304f124f1a21cf7a5341c72b856f58e50501b49a
Soji Omiwade <omiwade@gmail.com>
2024-01-07 21:53:48 -0800

    add the tree example and solution building(more important than the code)

--- largest-smaller-BST-key.py
+++ largest-smaller-BST-key.py
@@ -1,3 +1,17 @@
+'''
+          20
+        /    \
+       9     25
+     /   \     
+    5    12
+        /  \
+       11  14
+       
+approach: do a search for num, and set ans to the node in search if that node is 
+smaller than num, but bigger than ans
+if at the end of while, ans is not set, return -1
+'''
+class BST:
   def find_largest_smaller_key(self, num):
     ans=float('-inf')
     cur=self.root
