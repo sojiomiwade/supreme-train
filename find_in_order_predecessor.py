4efc94ed1d172cf45a354850bef18696f1f813be
Soji Omiwade <omiwade@gmail.com>
2024-01-07 21:49:50 -0800

    20m, moderate stress resisting jumping into code

--- find_in_order_predecessor.py
+++ find_in_order_predecessor.py
@@ -1,123 +1,13 @@
-##########################################################
-# CODE INSTRUCTIONS:                                     #
-# 1) The method findLargestSmallerKey you're asked       #
-#    to implement is located at line 30.                 #
-# 2) Use the helper code below to implement it.          #
-# 3) In a nutshell, the helper code allows you to        #
-#    to build a Binary Search Tree.                      #
-# 4) Jump to line 71 to see an example for how the       #
-#    helper code is used to test findLargestSmallerKey.  #
-##########################################################
-
-
-# A node 
-class Node:
-
-# Constructor to create a new node
-  def __init__(self, key):
-      self.key = key
-      self.left = None
-      self.right = None
-      self.parent = None
-
-# A binary search tree 
-class BinarySearchTree:
-
-  # Constructor to create a new BST
-  def __init__(self):
-      self.root = None
-
-  def find_largest_smaller_key(self, num): # 17
-    '''starting for root, just search for node with val num, until we hit null. 
-    if curr node is greater or equal num, don't alter best
-    else, best is max of best and curr key (start best of at -1)
-    
-    '''
-    def helper(curr, best): #9
-      if not curr:
-        return best
-      if curr.key >= num:
-        return helper(curr.left, best)
-      return helper(curr.right, max(best, curr.key))
-
-    curr = self.root
-    biggest_smaller = -1
-    while curr:
-      if curr.key < num:
-        biggest_smaller = max(biggest_smaller, curr.key)
-        curr = curr.right
-      else:
-        curr = curr.left
-    return biggest_smaller
-  # Given a binary search tree and a number, inserts a
-  # new node with the given number in the correct place
-  # in the tree. Returns the new root pointer which the
-  # caller should then use(the standard trick to avoid
-  # using reference parameters)
-  def insert(self, key):
-
-      # 1) If tree is empty, create the root
-      if (self.root is None):
-          self.root = Node(key)
-          return
-
-      # 2) Otherwise, create a node with the key
-      #    and traverse down the tree to find where to
-      #    to insert the new node
-      currentNode = self.root
-      newNode = Node(key)
-
-      while(currentNode is not None):
-        if(key < currentNode.key):
-          if(currentNode.left is None):
-            currentNode.left = newNode
-            newNode.parent = currentNode
-            break
-          else:
-            currentNode = currentNode.left
-        else:
-          if(currentNode.right is None):
-            currentNode.right = newNode
-            newNode.parent = currentNode
-            break
-          else:
-            currentNode = currentNode.right
-
-######################################### 
-# Driver program to test above function #
-#########################################
-
-bst  = BinarySearchTree()
- 
-# Create the tree given in the above diagram 
-bst.insert(20)
-bst.insert(9);
-bst.insert(25);
-bst.insert(5);
-bst.insert(12);
-bst.insert(11);  
-bst.insert(14);    
-
-result = bst.find_largest_smaller_key(9) # 5
-print ("Largest smaller number is %d " %(result))
-result = bst.find_largest_smaller_key(17) # 14
-print ("Largest smaller number is %d " %(result))
-result = bst.find_largest_smaller_key(4) # -1
-print ("Largest smaller number is %d " %(result))
-result = bst.find_largest_smaller_key(25) # 20
-print ("Largest smaller number is %d " %(result))
-
-'''
-time 9:31 -- 9:53 = 22
-
-'''
   def find_largest_smaller_key(self, num):
-    curr = self.root
-    res = float('-inf')
-    while curr:
-      if num <= curr.key:
-        curr = curr.left
+    ans=float('-inf')
+    cur=self.root
+    while cur:
+      if cur.key<num:
+        ans=max(ans,cur.key)
+      if num>cur.key:
+        cur=cur.right
       else:
-        res = max(res, curr.key)
-        curr = curr.right
-    return res
+        cur=cur.left
+    if ans==float('-inf'):
+      return -1
+    return ans
