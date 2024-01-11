d788f24768319f9e75f3974d0aa5479309513c0e
Soji Omiwade <omiwade@gmail.com>
2024-01-11 08:27:37 -0800

    the only relevant change here is that we can just say j in range(i+1,n) instead of all j, j!=i

--- threesum.py
+++ threesum.py
@@ -1,19 +1,28 @@
 '''
+x        0 0                  x
+i j k cannot be equal
+iterate on each i, then do a 2sum with a j which is greater than i
+arr[i], -arr[i] is what the 2sum must equal
+
+-1,0,1,2,-1,-4
+ i   j
+ z : 1-1=0
+ lookup : {0:1,}
+
+ 1 ... 2 ... 3 
+ i     j
 '''
-from typing import List
-
-
-def threesum(arr: List[int]) -> bool:
-    for i,x in enumerate(arr):
-        compslookup={}
-        for j in range(len(arr)):
-            diff=-x-arr[j]
-            if j!=i and (diff in compslookup) and (compslookup[diff] not in (i,j)):
-                return True
-            compslookup[arr[j]]=j
-    return False
-
-arr=[-6,3,3,]
-print(threesum(arr)) # true
-arr=[3,-6]
-print(threesum(arr)) # false
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res=set()
+        for i,x in enumerate(nums):
+            if nums[i]>0:
+                break
+            lookup={}
+            for j in range(i+1,len(nums)):
+                z=-x-nums[j]
+                if z in lookup:
+                    res.add(tuple(sorted([nums[i],nums[j],nums[lookup[z]]])))
+                lookup[nums[j]]=j
+        return list(res)
