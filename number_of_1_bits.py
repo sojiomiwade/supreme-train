ea76edee2f19a5324acde27b3df3b0cec4a51ce7
Soji Omiwade <omiwade@gmail.com>
2024-01-13 15:12:34 -0800

    remove lowest bit with n&=n-1 as a faster way

--- number_of_1_bits.py
+++ number_of_1_bits.py
@@ -1,8 +1,22 @@
+'''
+11101
+11100
+11000      11011
+1
+'''
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        mask = 1
-        res = 0
-        for _ in range(32):
-            res += (n & mask) != 0
-            mask <<= 1
-        return res
+        #2nd:
+        #if n>0 incremnt count; use n&(n-1)to drop the lowest bit, 
+        ans=0
+        while n:
+            ans+=1
+            n&=n-1
+        return ans
+        #first:
+        # mask=1
+        # ans=0
+        # for _ in range(32):
+        #     ans+=(n&mask)!=0
+        #     mask<<=1
+        # return ans
