22736895778a958e60c5083a9695bdf640b58d23
Soji Omiwade <omiwade@gmail.com>
2024-01-13 15:33:35 -0800

    20m calm and clear thinking eventually :-)

--- array_product_except_self.py
+++ array_product_except_self.py
@@ -1,59 +1,33 @@
-'''
-6      2      3       4
-|234   6|34    62|4    623|
-
-ans init to array of 1s
-for i in [1:n]
-    ans[i] = ans[i-1] * nums[i-1]
-for i in [n-2:-1]
-    ans[i] *= nums[i+1]
-'''
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [1 for _ in range(n)]
-        accum = 1
-        for i in range(n):
-            ans[i] *= accum
-            accum *= nums[i]
-        accum = 1
-        for i in range(n-1,-1,-1):
-            ans[i] *= accum
-            accum *= nums[i]
-        return ans
+       '''
+       use 2 loops,
+       ans starts as 1
+       left: dump the aggregate in the cell, aggregate the product, 
+       right: same
+       5       2     3.    4
+       1       5.    52.   523
+       1|234  5|34  52|4.  523|1
+       going forward .. i in (0,n)
+       agg=1
+       loop 
+        answer[i]=agg  
+        agg*=nums[i] : 5
 
-'''
-1   2   3   4
-234 134 124 123
-1   1   12  123
-234 34  4   1
-
-
-going left
-in answer[i] i in [1..n), put aggleft, then set aggleft to  nums[i]*aggleft
-going right, similar: [n-1,-1)
-in answer[i], a aggright, then aggregate nums[i] into aggright
-=> aggright is set to 
-nums: 4 5 6
-        ^
-ans:  165 46 45
-ar: 654
-al: 456
-exp:  56 46 45
-initialize first to 1
-
-'''
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        answer = [1] * n
-        aggleft = nums[0]
-        for i in range(1, n):
-            answer[i] = aggleft
-            aggleft *= nums[i]
-        aggright = nums[-1]
-        for i in range(n-2,-1,-1):
-            answer[i] *= aggright
-            aggright *= nums[i]
-        return answer
-        
+       coming back .. i in [n-1,1]
+       agg=1
+       loop
+        answer[i]*=agg
+        agg*=nums[i] : 34
+       '''
+       n=len(nums)
+       answer=[1 for _ in range(n)]
+       agg=1
+       for i in range(n):
+           answer[i]=agg  
+           agg*=nums[i]
+       agg=1
+       for i in range(n-1,-1,-1):
+           answer[i]*=agg  
+           agg*=nums[i]
+       return answer
