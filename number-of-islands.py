add91fad7b7b4acfd52bf6c6dcb0f6250eb4ad06
Soji Omiwade <omiwade@gmail.com>
2024-01-16 08:25:44 -0800

    nicely fast, no errors

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,23 +1,28 @@
+'''
+1 1
+0 0
+'''
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        '''
-        run through grid and count number of 1s, 
-        DFS at each 1 and set it to #
-        '''
-        def _numislands(r: int, c: int) -> None:
-            if not 0<=r<m or not 0<=c<n or grid[r][c] != '1':
+        def try2visit(r: int, c: int) -> None:
+            if (r,c) in visited or not (0<=r<m and 0<=c<n) or grid[r][c]=='0':
                 return
-            grid[r][c] = '#'
-            _numislands(r+1, c)
-            _numislands(r-1, c)
-            _numislands(r, c+1)
-            _numislands(r, c-1)
-
-        res = 0
-        m, n = len(grid), len(grid[0])
+            visited.add((r,c))
+            try2visit(r+1,c)
+            try2visit(r-1,c)
+            try2visit(r,c+1)
+            try2visit(r,c-1)
+        '''
+        can go for DFS, on each of the mn cells, and count
+        that cell if it hasn't been visited
+        recursive call: try2visit on neighboring cells
+        '''
+        m,n=len(grid),len(grid[0])
+        visited=set()
+        ans=0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':
-                    res += 1
-                    _numislands(i, j)
-        return res
+                if grid[i][j]=='1' and (i,j) not in visited:
+                    ans+=1
+                    try2visit(i,j)
+        return ans
