daad2e8f98252c3848132c57e7da13bde157df8e
Soji Omiwade <omiwade@gmail.com>
2024-01-16 10:51:04 -0800

    17m moderately calm

--- palindrome_permutation.py
+++ palindrome_permutation.py
@@ -1,86 +1,31 @@
 '''
-toca cot
-space is insignificant
-case is insignificant
-above is true: itself is a palindrome
-algorithm: sort the string, then for each unique char, no more than one should have odd count.
+given a string, find if it is a permutation of a palindrome. assume ascii
 
-aaabb
-l
-r
-toca cott
-accoottt
-   ^
+hhhha
+first: hashmap counter every key should have even count; one can have odd count. time is n
+2nd: could use 256 bit vector. then at the end only one key can have a set bit. time still n, space O(256). ignore the space
+3rd: sort it and count neighbors. come back here since it's n lg n to start
+
+ahhhh
+f t t     t
+0 1 2 ... 8 ...
+ube=t
 '''
-def is_pal_perm(s: str) -> bool:
-    slist = sorted(s.upper())
-    left = 0
-    already_have = False
-    while left < len(slist):
-        count = 0
-        while count + left < len(slist) and slist[left] == slist[count + left]:
-            count += 1
-        if slist[left] != ' ' and count % 2 == 1:
-            if already_have:
+def ispalperm(s: str) -> bool:
+    isbal=[True for _ in range(256)]
+    for x in s:
+        isbal[ord(x)]=not isbal[ord(x)]
+    unbalanced_exists=False
+    for x in s:
+        if not isbal[ord(x)]:
+            if unbalanced_exists:
                 return False
-            already_have = True
-        left += count
+            unbalanced_exists=True
     return True
 
-s = 'toca cot'
-print(is_pal_perm(s)) # true
-s = 'cota cot'
-print(is_pal_perm(s)) # true
-s = 'cota cott'
-print(is_pal_perm(s)) # false
-s = 'coTa cot'
-print(is_pal_perm(s)) # true
-
-
-
-
-'''
-Given a string write a method to decide if it is a permutation of a palindrome
-abcd - f
-aabaa - or any of its permutations - t
-^
-1 character can have odd freq
-all others must have even number of freq
-hash table of freq, and check above conditions
-can use a bitmap
-aaaa
-1010 <-- 0 means even
-then only 1 bit can be set in the map
-aabaac
-
-0000110 -> false
-0000101
--------
-0000100
--------
-can check if only 1 bit is set with
-n & (n-1) == 0  implies 1 or 0 bits set
-note 0 & -1 == 0
-
-
-'''
-
-def ispalperm(s: str) -> bool:
-    vec = 0
-    for ch in s:
-        pos = ord(ch) - ord('a')
-        vec ^= 1 << pos
-    return vec & (vec-1) == 0
-    
-s='aaaa'
-print(ispalperm(s)) # t
-s='aaaabbb'
-print(ispalperm(s)) # t
-s='aaaab'
-print(ispalperm(s)) # t
-s='aabaa'
-print(ispalperm(s)) # t
-s='aaaabc'
-print(ispalperm(s)) # f
-s='aaqraa'
-print(ispalperm(s)) # f
+print(ispalperm('ahhhh')) # t
+print(ispalperm('ahhh')) # f
+print(ispalperm('noon')) # t
+print(ispalperm('noon1122')) # t
+print(ispalperm('noon11223')) # t
+print(ispalperm('noon112234')) # f
