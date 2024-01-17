9ea104da45aa29376649e4cd407dd676e9c8e7a3
Soji Omiwade <omiwade@gmail.com>
2024-01-17 10:23:05 -0800

    still some examples and walkthrough errors

--- sales_path.py
+++ sales_path.py
@@ -1,87 +1,38 @@
 '''
-Sales Path
-The car manufacturer Honda holds their distribution system in the form of a tree (not necessarily binary). The root is the company itself, and every node in the tree represents a car distributor that receives cars from the parent node and ships them to its children nodes. The leaf nodes are car dealerships that sell cars direct to consumers. In addition, every node holds an integer that is the cost of shipping a car to it.
-
-Take for example the tree below:
-                           0
-                        /  |  \
-                      /    |    \
-                     5     3      6
-                    /    /  \    /  \ 
-                   4    2    0  1    5
-                       /    /
-                      1    10
-                       \
-                        1
-func mincost(node)
-    return node.cost + mincost amongs each child
-A path from Honda’s factory to a car dealership, which is a path from the root to a leaf in the tree, is called a Sales Path. The cost of a Sales Path is the sum of the costs for every node in the path. For example, in the tree above one Sales Path is 0→3→0→10, and its cost is 13 (0+3+0+10).
-
-Honda wishes to find the minimal Sales Path cost in its distribution tree. Given a node rootNode, write a function getCheapestCost that calculates the minimal Sales Path cost in the tree.
-
-Implement your function in the most efficient manner and analyze its time and space complexities.
-
-For example:
-
-Given the rootNode of the tree in diagram above
-
-Your function would return:
-
-7 since it’s the minimal Sales Path cost (there are actually two Sales Paths in the tree whose cost is 7: 0→6→1 and 0→3→2→1→1)
-
-Constraints:
-
-[time limit] 5000ms
-
-[input] Node rootNode
-
-0 ≤ rootNode.cost ≤ 100000
-[output] integer
-
-                           0
-                        /  |  \
-                      /    |    \
-                     5     3      6
-                    /    /  \    /  \ 
-                   4    2    0  1    5
-                       /    /
-                      1    10
-                       \
-                        1
-
-gcc(6)
-    gcc(1)
-
-    gcc(5)
+need to consider every path and return the mincost path
+time,space=O(V+E),O(V+E) via DFS for example
+if we find better replace the current
+cost[i] = mycost+mincost of my children
+if root:
+  update mincost
 '''
-from __future__ import annotations
-from typing import List, Optional
+def get_cheapest_cost(rootNode):
+  minchildcost=float('inf') if rootNode.children else 0
+  for chnode in rootNode.children:
+    minchildcost=min(minchildcost,get_cheapest_cost(chnode))
+  return rootNode.cost+minchildcost
+  
+########################################## 
+# Use the helper code below to implement #
+# and test your function above           #
+##########################################
 
-class TreeNode:
-    MAX_COST = 0
+# A node 
+class Node:
 
-    def __init__(
-            self, cost: int, 
-            children_costs: Optional[List[int]] = None,
-            ) -> None:
-        TreeNode.MAX_COST = max(TreeNode.MAX_COST, cost)
+  # Constructor to create a new node
+  def __init__(self, cost):
+    self.cost = cost
+    self.children = []
+    self.parent = None
 
-        self.cost = cost
-        if children_costs:
-            self.children = \
-            [TreeNode(childcost) for childcost in children_costs]
-        else:
-            self.children = None
+n51,n31,n61=Node(5),Node(3),Node(6)
+n42,n22,n02,n12,n52=Node(4),Node(2),Node(10),Node(1),Node(5)
+n51.children=[n42]
+n31.children=[n22,n02]
+n61.children=[n12,n52]
+children=[n51,n31,n61]
+root=Node(0)
+root.children=children
 
-def get_cheapest_cost(rootNode: TreeNode) -> int:
-    if not rootNode:
-        raise ValueError('no root node')
-    if not rootNode.children:
-        return rootNode.cost
-
-    min_child_cost = TreeNode.MAX_COST
-    for node in rootNode.children:
-        min_child_cost = min(min_child_cost, get_cheapest_cost(node))
-    return rootNode.cost + min_child_cost
-
-print(get_cheapest_cost(TreeNode(6, [1,5]))) # 7
+print(get_cheapest_cost(root)) #3
