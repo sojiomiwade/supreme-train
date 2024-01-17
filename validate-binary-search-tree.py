62dc7d34fabf9eebf0533f7ca61493fe0621f12a
Soji Omiwade <omiwade@gmail.com>
2024-01-17 15:33:24 -0800

    now done with solution based on tree height

--- validate-binary-search-tree.py
+++ validate-binary-search-tree.py
@@ -13,18 +13,36 @@ otherwise keep going
           /
         1
         lis=[-inf,]
+50
+   \
+    60 (50,inf)
+      \
+         70 (70,inf)
+      /(50,70)      \ 
+     63             90
+     isvalid(node.left,lb,node.val)
+     isvalid(node.right,node.val,ub)
 '''
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def isvalid(node):
+        def isvalid(node, lb: int, ub: int):
             if not node:
                 return True
-            ans=isvalid(node.left)
-            if lis[-1]>=node.val:
-                return False
-            lis.append(node.val)
-            ans=ans and isvalid(node.right)
-            return ans
+            ans=lb<node.val<ub
+            ans=ans and isvalid(node.left,lb,node.val)
+            return ans and isvalid(node.right,node.val,ub)
 
-        lis=[float('-inf')]
-        return isvalid(root)
+        INTMIN,INTMAX=-2**31-1,2**31
+        return isvalid(root,INTMIN,INTMAX)
+        # def isvalid(node):
+        #     if not node:
+        #         return True
+        #     ans=isvalid(node.left)
+        #     if lis[-1]>=node.val:
+        #         return False
+        #     lis.append(node.val)
+        #     ans=ans and isvalid(node.right)
+        #     return ans
+
+        # lis=[float('-inf')]
+        # return isvalid(root)
