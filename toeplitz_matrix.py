4be9ba7c094ed6926f9eba14833bde577992cf92
Soji Omiwade <omiwade@gmail.com>
2024-01-18 09:44:46 -0800

    2m; still even thought about approach before jumping in

--- toeplitz_matrix.py
+++ toeplitz_matrix.py
@@ -1,29 +1,9 @@
-#just need to check 1 pair of diagonals, for all diagonals. easy peezy.
-
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        m, n = len(matrix), len(matrix[0])
-        vert = ((i,0) for i in range(m))
-        horiz = ((0,j) for j in range(n))
-        for (i,j) in itertools.chain(horiz, vert):
-            while i+1 < m and j+1 < n:
-                if matrix[i][j] != matrix[i+1][j+1]:
-                    return False
-                i += 1
-                j += 1
-        return True
-
-'''
-1 2 3 4
-a b c d
-e f g h
-'''
-class Solution:
-    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        m, n = len(matrix), len(matrix[0])
-        for i in range(m - 1):
-            for j in range(n - 1):
-                if matrix[i][j] != matrix[i + 1][j + 1]:
+        # compare matrix[i][j] to i+1 j+1
+        m,n=len(matrix),len(matrix[0])
+        for i in range(m-1):
+            for j in range(n-1):
+                if matrix[i][j]!=matrix[i+1][j+1]:
                     return False
         return True
-
