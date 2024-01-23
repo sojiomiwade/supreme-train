0c1258ecd83adee929f282805c43b1328c6c2597
Soji Omiwade <omiwade@gmail.com>
2024-01-23 10:39:55 -0800

    walkthrough missing

--- house-robber-ii.py
+++ house-robber-ii.py
@@ -1,65 +1,58 @@
-#rob(i)= nums[i]+rob(i+2) OR rob(i+1)
-#pass back inf if (i)first_is_there and (ii)i==n-1
-# 1 2 3 4
-#    fu2   fu1   
-# 0  0     1    2   3  4
 '''
-5 7 3
+2 3 2
+|      \
+2 3     3 2
 
-rob[1]=max(nums[1],nums[2])
-if n==1:
-    return 1
-if n==2
-    retrun max(nums[0],nums[1])
-fu2=False
-fu1=true if nums[0]>nums[1] else false
-rob[0]=nums[0]
+9 2 3 8 x x
+ans=9
+rob(0,end=n-2)
+rob(1,end=n-1)
+rob(i)=max(nums[i]+rob(i+2),rob(i+1))
+    if > end, return 0
 
-repeat for all i in 2..n-1
-    numsi=0 if i==n-1 and fu2. otherwise, it's just nums[i]
-    prof_with_i=numsi+rob[i-2]
-    prof_without_i=rob[i-1]
-    rob[i]=max(prof_with_i,prof_without_i)
-    cur=fu2 if prof_with_i>prof_without_i else fu1
-    fu2,fu1=fu1,cur
-
-does the cur robbery i make involve the first house
-fu      t   f   t   f  f
-rob     0   0   2   3   (0+2,3)
-nums            2   3   2
-hidx            0   1   2
-
-fu      t   f   
-rob     0   0   
-nums            1   2   3
-hidx            0   1   2
-ans should be 3
+0 1 2
+2 3 9
+|      \
+2 3      3 9
+0
+|     \
+1      2
+|   \
+2    3
+| \  
+3  4
+max(max(2,3),  max(3,9))
 '''
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        def rob(hidx: int, end: int) -> int:
+            if hidx>end:
+                return 0
+            if (hidx,end) in dp:
+                return dp[hidx,end]
+            dp[hidx,end]=max(nums[hidx]+rob(hidx+2,end),rob(hidx+1,end))
+            return dp[hidx,end]
         n=len(nums)
-        fu2,fu1=True,False
-        rob=rob1=rob2=0
-        for i in range(n):
-            if i==n-1 and fu2:
-                rob_with_i=max(nums[i],rob2)
-            else:
-                rob_with_i=nums[i]+rob2
-            rob=max(rob_with_i,rob1)
-            cur=fu2 if rob_with_i>rob1 else fu1
-            rob2,rob1=rob1,rob
-            fu2,fu1=fu1,cur
-        return rob
+        if n==1:
+            return nums[0]
+        # dp={}
+        # return max(rob(0,end=n-2),rob(1,end=n-1))
+        '''
+              s   e
+              0 1 2  3     
+              9 2 3  8
+        0  0  9 9 12 .       
+           r2 r1 a     
 
-    # def rob(hasfirst: bool, hidx) -> int:
-    #     if hidx>n-1 or (hidx==n-1 and hasfirst):
-    #         return 0
-    #     if (hasfirst,hidx) in dp:
-    #         return dp[hasfirst,hidx]
-    #     dp[hasfirst,hidx]=max(rob(hasfirst,hidx+2)+nums[hidx],rob(hasfirst,hidx+1))
-    #     return dp[hasfirst,hidx]
-
-    # n=len(nums)
-    # dp={}
-    # return max(rob(True,2)+nums[0],rob(False,1))
-    
+            1   2   3
+         0  0   .   .
+         r1 r2  a
+        '''
+        def rob(start: int, end: int) -> int:
+            r2=r1=0
+            ans=None
+            for i in range(start,1+end):
+                ans=max(r1,r2+nums[i])
+                r2,r1=r1,ans
+            return ans
+        return max(rob(0,n-2),rob(1,n-1))
