1d45e014c49c8fdd38191cb069390d9ff86ae106
Soji Omiwade <omiwade@gmail.com>
2024-01-24 07:45:55 -0800

    37m, lacked confidence, dwelt on finishing in as min time as possible, and almost did not even finish

--- clone_graph.py
+++ clone_graph.py
@@ -4,140 +4,33 @@ class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-
-1 - 2
-|   |  
-3 - 4
-
-1 - 2
-|   |
-3 - 4
-
-can use DFS. 
-clone the curr node. then
-seems the parent is going to be passed so that 
-
-2--4
-
-1--3
 """
+'''
+def cg(node)
+    if node
+        clonenode=Node()
+        for all children of node
+            clonenode.neighbors.append(cg(child))
+
+how to handle already created nodes?
+everyone does their pointing
+clookup={1:c1,2:c2}
+c1 -- c2
+|      |
+        c3
+'''
 
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        def _clonegraph(u,uc):
-            for v in u.neighbors:
-                if v not in clookup:
-                    clookup[v]=Node(v.val)
-                vc=clookup[v]
-                uc.neighbors.append(vc)
-                if v not in vis:
-                    vis.add(v)
-                    _clonegraph(v,vc)
-
-        if not node:
-            return None
-        vis={node}
-        nc=Node(node.val)
-        clookup={node:nc}
-        for nb in node.neighbors:
-            if nb not in clookup:
-                clookup[nb]=Node(nb.val)
-            nbc=clookup[nb]
-            nc.neighbors.append(nbc)
-            if nb not in vis:
-                vis.add(nb)
-                _clonegraph(nb,nbc)
-
-        return nc"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-
-1 - 2
-|   |  
-3 - 4
-
-1 - 2
-|   |
-3 - 4
-
-can use DFS. 
-clone the curr node. then
-seems the parent is going to be passed so that 
-
-2--4
-
-
-1---2
-|   |
-4---3
-clookup={1:1c,2:2c,}
-1<--2
-^   ^
-    v 
-4<->3
-cg(2,2c)
-
-"""
-
-from typing import Optional
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        def _clonegraph(u,uc):
-            for v in u.neighbors:
-                if v not in clookup:
-                    clookup[v]=Node(v.val)
-                    _clonegraph(v,clookup[v])
-                uc.neighbors.append(clookup[v])
-
-        if not node:
-            return None
-        nc=Node(node.val)
-        clookup={node:nc}
-        for nb in node.neighbors:
-            if nb not in clookup:
-                clookup[nb]=Node(nb.val)
-                _clonegraph(nb,clookup[nb])
-            nc.neighbors.append(clookup[nb])
-        return nc"""
-1 2
-3 4
-"""
-
-from typing import Optional
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        def _clonegraph(u):
-            uc=clookup[u]=Node(u.val)
-            for v in u.neighbors:
-                if v not in clookup:
-                    _clonegraph(v)
-                uc.neighbors.append(clookup[v])
-            return uc
+        def clonegraph(node):
+            if node in clookup:
+                return clookup[node]
+            cnode=Node(node.val)
+            clookup[node]=cnode
+            for nb in node.neighbors:
+                cnb=clonegraph(nb)
+                cnode.neighbors.append(cnb)
+            return cnode
         clookup={}
-        return _clonegraph(node) if node else None"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-
-
-"""
-
-from typing import Optional
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        q=deque([node]) if node else deque([])
-        cl={node: Node(node.val)} if node else {}
-        while q:
-            u=q.popleft()
-            for v in u.neighbors:
-                if v not in cl:
-                    q.append(v)
-                    cl[v]=Node(v.val)
-                cl[v].neighbors.append(cl[u])
-        return cl[node] if node else None
+        return clonegraph(node) if node else None
