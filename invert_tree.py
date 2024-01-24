e7d735d2e7ef87ca0d76edcff0057e23c91fe319
Soji Omiwade <omiwade@gmail.com>
2024-01-24 08:09:50 -0800

    3lines-->1line since invert-tree returns the root

--- invert_tree.py
+++ invert_tree.py
@@ -7,7 +7,5 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if root:
-            root.left,root.right=root.right,root.left
-            self.invertTree(root.left)
-            self.invertTree(root.right)
+            root.left,root.right=self.invertTree(root.right),self.invertTree(root.left)
         return root
