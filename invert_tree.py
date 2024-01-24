bde66866df45838356eb584b41c684f26519b805
Soji Omiwade <omiwade@gmail.com>
2024-01-24 08:07:56 -0800

    3m,swap left and right, then call recursively on both approach

--- invert_tree.py
+++ invert_tree.py
@@ -1,41 +1,3 @@
-This is pretty simple recursively speaking: swap the left and right 
-reference, then recurse to both. looks like you could also get away
-with recursing to both, then swapping the references...verified this!
-
-from typing import Optional
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return None
-        root.left, root.right = root.right, root.left
-        self.invertTree(root.left)
-        self.invertTree(root.right)
-        return root
-
-print(Solution().invertTree(None))
-
-
-# add BFS approach
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return None
-        q = deque([root])
-        while q:
-            node = q.popleft()
-            node.left, node.right = node.right, node.left
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-        return root        
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -44,12 +6,8 @@ print(Solution().invertTree(None))
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        q=deque([root]) if root else deque()
-        while q:
-            v=q.popleft()
-            v.left,v.right=v.right,v.left
-            if v.left:
-                q.append(v.left)
-            if v.right:
-                q.append(v.right)
+        if root:
+            root.left,root.right=root.right,root.left
+            self.invertTree(root.left)
+            self.invertTree(root.right)
         return root
