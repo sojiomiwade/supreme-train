06d4d99846ff58f81bae021a7078de5267431b68
Soji Omiwade <omiwade@gmail.com>
2024-01-24 08:50:11 -0800

    11m, but rushing caused overlook codewalkthrough and basic test

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -1,45 +1,32 @@
-'''
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-11
- 73
- 58
----
-171
-
-new list, head = res = Node(null) 
-get element from l1 and l2 and carry if possible # move l1 and l2 accordingly
-res.next = new Node(l1val + l2val + carry // 10)
-carry = above operand % 10
-if not we are done
-return head.next
-'''
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 '''
-create new list on the fly, stop
+handle carry! 
+    1
+  3 4
+  8 '0'
+h 1 5  
+  c
+ans should be 1 5
 '''
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = l3 = ListNode()
-        carry = 0
+        cur=head=ListNode()
+        carry=0
         while l1 or l2 or carry:
-            l1val = l2val = 0
+            l1val=0
             if l1:
-                l1val = l1.val
-                l1 = l1.next
+                l1val=l1.val 
+                l1=l1.next
+            l2val=0
             if l2:
-                l2val = l2.val
-                l2 = l2.next
-            trunc = (carry + l1val + l2val) % 10
-            l3.next = ListNode(trunc)
-            l3 = l3.next
-            carry = (carry + l1val + l2val) // 10
+                l2val=l2.val 
+                l2=l2.next
+            digit=(l1val+l2val+carry)%10
+            carry=(l1val+l2val+carry)//10
+            cur.next=ListNode(digit)
+            cur=cur.next
         return head.next
