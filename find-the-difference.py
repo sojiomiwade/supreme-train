c7202cb3ffee2024f7a576383a8850abc394b0c3
Soji Omiwade <omiwade@gmail.com>
2024-01-26 07:31:47 -0800

    19m, and missed the ability to xor the two strings

--- find-the-difference.py
+++ find-the-difference.py
@@ -1,127 +1,30 @@
 '''
-9:19 - 9:28 = 9 mins
-t = random_shuffle(s) + x
-s = abbcdd
-t = abbcddd
-s_ch_count
-t_ch_count
-for each ch in t_ch_count, if it is different in s_ch_count, return it
-time: build maps: m + n, space: m + n
-a
-aa
-schcount[0] = 1
-tchcount[0] = 2
+first: ss: can use one such set: then iterate on t until you find an element not in ss. time, space: O(n), O(26)
+can use an integer instead as the set for t. time, space: O(n), O(1)
+
+s : a
+t : aa
+tcounter : a:1
+
 '''
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sch_count = [0]*26
-        tch_count = [0]*26
-        for ch in t:
-            tch_count[ord(ch)-ord('a')] += 1
-        for ch in s:
-            sch_count[ord(ch)-ord('a')] += 1
-        for ch in t:
-            if tch_count[ord(ch)-ord('a')] != sch_count[ord(ch)-ord('a')]:
-                return ch
-        return None
+        tcounter = Counter(t)
+        for el in s:
+            tcounter[el] -= 1
+        for let, count in tcounter.items():
+            if count == 1:
+                return let
+        assert Exception('no added element')
+        
+        
 
-
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        svec = tvec = 0
-        for ch in s: #abcd: 
-            mask = 1 << (ord(ch) - ord('a'))
-            svec ^= mask
-        for ch in t:
-            mask = 1 << (ord(ch) - ord('a'))
-            tvec ^= mask
-        resvec = svec ^ tvec
-        for chidx in range(26):
-            char_is_set = resvec & 1 #1011 & 1
-            if char_is_set:
-                return chr(chidx + 97)
-            resvec >>= 1
-        return None
-           
-
-# the no sweat bit manipulation way of doing this
-'''
-fish
-fiysh
-sum of si and ti. at the end just add t_last
-
-by xor
-s, t
-res ^= ch
-then return result
-'''
-from itertools import chain
-def find_the_difference(s: str, t: str) -> str:
-    res = 0
-    for sch, tch in zip(s, t):
-        res += ord(tch) - ord(sch)
-    return chr(ord(t[-1]) + res)
-
-def find_the_difference_xor(s: str, t: str) -> str:
-    res = 0
-    for ch in chain(s, t):
-        res ^= ord(ch)
-    return chr(res)
-
-find_the_difference = find_the_difference_xor
-
-s, t = 'fish', 'fishy'
-print(find_the_difference(s, t)) #y
-s, t = '', 'a'
-print(find_the_difference(s, t)) #a
-s, t = 'fish', 'shiyf'
-print(find_the_difference(s, t)) #y
- 
-'''
-1 2 3 = x
-1 2 3 5 = 5 + x
-1+2+3+5 - (1-2-3)
-a
-aa
-tot=2*97-2*97=0
-'''
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        tot=0
-        for x,y in zip(s,t):
-            tot+=ord(y)-ord(x)
-            tot%=26
-        tot+=(ord(t[-1])-ord('a'))
-        tot%=26
-        return chr(tot+ord('a'))
-
-            '''
-can be done with bit toggle
-
-abc
-abca
-
-abc
-abcd
-
- ab
-x01
-y10
-v10
- 
-0010
-0001
-'''
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        v=0
-        for x,y in zip(s,t):
-            mx=1<<(ord(x)-ord('a'))
-            my=1<<(ord(y)-ord('a'))
-            v^= mx ^ my
-        v^=1<<(ord(t[-1])-ord('a'))
-        m=1
-        for i in range(26):
-            if v&m != 0:
-                return chr(i+ord('a'))
-            m<<=1
+        # vector = 0
+        # for el in s:
+        #     mask = 1 << (ord(el) - ord('a'))
+        #     vector |= mask
+        # for el in t:
+        #     mask = 1 << (ord(el) - ord('a'))
+        #     if vector & mask == 0:
+        #         return el
+        # assert Exception('no added element')
