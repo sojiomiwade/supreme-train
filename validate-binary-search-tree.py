d66f672a47c48fea14c972b0b1770ffd3a068072
Soji Omiwade <omiwade@gmail.com>
2024-01-28 10:48:12 -0800

    5m, iteratively validate, calm down a tad man, anyway, i knew the approach fresh from in-order traversal that i just did

--- validate-binary-search-tree.py
+++ validate-binary-search-tree.py
@@ -4,45 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-'''
-first: do an inorder, and validate. time, space = n, n
-better: as we inorder can we just check the cur element against the last?
-perhaps pass the last to inorder. can return false immediately when last>=cur.
-otherwise keep going
-            1
-          /
-        1
-        lis=[-inf,]
-50
-   \
-    60 (50,inf)
-      \
-         70 (70,inf)
-      /(50,70)      \ 
-     63             90
-     isvalid(node.left,lb,node.val)
-     isvalid(node.right,node.val,ub)
-'''
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def isvalid(node, lb: int, ub: int):
-            if not node:
-                return True
-            ans=lb<node.val<ub
-            ans=ans and isvalid(node.left,lb,node.val)
-            return ans and isvalid(node.right,node.val,ub)
-
-        INTMIN,INTMAX=-2**31-1,2**31
-        return isvalid(root,INTMIN,INTMAX)
-        # def isvalid(node):
-        #     if not node:
-        #         return True
-        #     ans=isvalid(node.left)
-        #     if lis[-1]>=node.val:
-        #         return False
-        #     lis.append(node.val)
-        #     ans=ans and isvalid(node.right)
-        #     return ans
-
-        # lis=[float('-inf')]
-        # return isvalid(root)
+        prev=float('-inf')
+        st=[]
+        while st or root:
+            if root:
+                st.append(root)
+                root=root.left
+            else:
+                next=st.pop()
+                if prev<next.val:
+                    prev=next.val
+                else:
+                    return False
+                root=next.right
+        return True
