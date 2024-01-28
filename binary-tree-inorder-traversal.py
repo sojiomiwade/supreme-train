26199589a87ba524be2f6ec642f18fba0475173e
Soji Omiwade <omiwade@gmail.com>
2024-01-28 10:36:29 -0800

    27m, need more problem-solving: how to iterate tree iteratively should be known

--- binary-tree-inorder-traversal.py
+++ binary-tree-inorder-traversal.py
@@ -5,71 +5,51 @@
 #         self.left = left
 #         self.right = right
 '''
-                 2
-                / \
-               3   4
-              /   /
-             5   6
-53264
-
-st=[]
-(4)(6)
-53264
-
-1
-  \
-   2
-  /
-3
-exp: 132
-st : [n(2)3
-root : 2
-res : [132]
-'''
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        st=[root]
-        res=[]
-        while st:
-            root=st.pop()
-            if type(root) is int:
-                res.append(root)
-            elif root:
-                st.append(root.right)
-                st.append(root.val)
-                st.append(root.left)
-        return res# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-'''
+first: recursive: append  left, then root.val, then right
+    time,space: O(n),O(h+n)=O(n) since n>=h
+second: iterative:
            1
         /     \
-       4       2
-        \     /  \
-         7   3    5
+       2       3
+      / \     / \
+     .   4   5   .
+              \
+               6 
+    ans=[2,4,1,5,6,3]
+    st=[3 6] 
+    root=
+    next=(6)
+    ans=[2 4 1 5 6]
 
-res : 471325
-root: n
-st  : 
-
-while st has elements
-    go along left slide until until next left is null
-    root = pop from stack
-    put root into result
-    push root.right to stack if right isn't null
-
+    st=[]
+    while st or root
+        if root
+            st.append(root)
+            root=root.left
+        else:
+            next=st.pop()
+            ans.append(next.val)
+            root=next.right
 '''
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        st,res=[],[]
+        st,ans=[],[]
         while st or root:
-            while root:
+            if root:
                 st.append(root)
                 root=root.left
-            root=st.pop()
-            res.append(root.val)
-            root=root.right
-        return res
+            else:
+                next=st.pop()
+                ans.append(next.val)
+                root=next.right
+        return ans
+
+        # def inorder(root):
+        #     if root:
+        #         inorder(root.left)
+        #         ans.append(root.val)
+        #         inorder(root.right)
+
+        # ans=[]
+        # inorder(root)    
+        # return ans
