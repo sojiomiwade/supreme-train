8d18287b307509743135b4291958b6ba130c6920
Soji Omiwade <omiwade@gmail.com>
2024-01-30 11:46:21 -0800

    5m lebcowit ok, jumped in a little early into code

--- hamming-distance.py
+++ hamming-distance.py
@@ -1,79 +1,22 @@
 '''
-0001
-0100
- ^ ^
- stopwatch: 8:15 - 8:33 = 18m
- confirmations: x, y is non-neg int
- examples:
- method: xor each, and count the number of 1s in end result
- mask = 1
- res = x ^ y
-rescount = 0
- for i = 0 to 31
-    if res & mask != 0:
-        rescount ++ 
-    mask << 1
+0 0 0 1
+0 1 0 0
+  ^   ^
+order matters, can't just get the next bit
+use power of 2 instead of mask to get the next one
+and do this 32 times
+cur bit of x is x & 1
+advance x is x>>1
 
-analytics:
-O(1)
-
-0001
-0100
-0101
 '''
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        mask = 1
-        res = x ^ y
-        rescount = 0
-        for _ in range(31):
-            if (res & mask) != 0:
-                rescount += 1 
-            mask <<= 1
-        return rescount
-        
-
-#refresh
-'''
-0101
-0110
-0011 <-- xy
-
-xord = x^y 
-sum_ += xord & 1 and shift xord and do this 31 more times
-time: O(1), space: O(1)
-'''
-class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        res = 0
-        xy = x ^ y
-        while xy:
-            res += xy & 1
-            xy >>= 1
-        return res
-
-x, y = 5, 6
-print(Solution().hammingDistance(x, y)) # 2
-'''
-keep shifting and getting the xors of both at the end of the numbers
-wait. just xor the two, and count the 1s
-time: O(1) since 32 times
-space: O(1) since only make one new number and no additional data structures
-1001
-   1
-3210
-----
-   1
-
-0123
-1001
-'''
-class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        xy=x^y
-        mask=1
         count=0
-        for _ in range(32):
-            count+=bool(xy&mask)
-            mask<<=1
+        while x or y:
+            if x&1!=y&1:
+                count+=1
+            x>>=1
+            y>>=1
         return count
+
+        
