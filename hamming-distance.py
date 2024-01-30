e5d7e469105e5c279fd10b8e882f7abf9e1ae2a8
Soji Omiwade <omiwade@gmail.com>
2024-01-30 11:50:10 -0800

    5m lebcowit ok, used x&=x-1

--- hamming-distance.py
+++ hamming-distance.py
@@ -1,22 +1,23 @@
 '''
 0 0 0 1
 0 1 0 0
-  ^   ^
+0 1 0 1
 order matters, can't just get the next bit
 use power of 2 instead of mask to get the next one
 and do this 32 times
 cur bit of x is x & 1
 advance x is x>>1
-
+0
+1
+1
 '''
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
+        z=x^y
         count=0
-        while x or y:
-            if x&1!=y&1:
-                count+=1
-            x>>=1
-            y>>=1
+        while z:
+            count+=1
+            z&=z-1
         return count
 
         
