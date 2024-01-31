6c465aa3b75ffb49344a404a4b9a717a34d11b5e
Soji Omiwade <omiwade@gmail.com>
2024-01-31 07:39:11 -0800

    should rehash approach couple times to be sure of the algorithms without pseudocode, should know how to strip punctuation

--- word_count_engine.py
+++ word_count_engine.py
@@ -1,68 +1,48 @@
+'''
+input:  document = "Practice makes perfect. you'll only
+                    get Perfect by practice. just practice!"
+
+output: [ ["practice", "3"], ["perfect", "2"],
+          ["makes", "1"], ["youll", "1"], ["only", "1"], 
+          ["get", "1"], ["by", "1"], ["just", "1"] ]
+
+can just lower the whole thing and also remove punctuation
+sort by frequency (descending).
+when 2 are the same sort by ordinal in original string
+can get ordinal using a count.
+for each word
+  if word not in ordlookup
+    ordlookup[word]=count, 
+    count ++
+'''
 from collections import Counter
-'''
-1. somehow remove all punctuation
-doc.split() = ['practice', ..., "you'll"]
-a. for each letter in a word if it doesn't fall in ascii remove it
 
-2. build dictionary: {practice: [3,idx]}
 
-2. use key=function(x)
-practice 3, 0
-makes 1, 1
-perfect 2, 3
-youll 1
+def remove_punc(s):
+  return ''.join([ch for ch in s if ch.isalnum() or ch==' ']).split()
 
-(w,wf,)
-sort(key=lambda (w,wf,oi): (wf,oi,w))
-3, practice < 1 makes but
-1 makes
+def get_ord(s):
+  ordlookup={}
+  count=0
+  for word in s:
+    if word not in ordlookup:
+      ordlookup[word]=count
+      count+=1
+  return ordlookup
 
-b' a a
-b a a
-
-f={a:2, b:1}
-oil={a:1, b:0}
-'''
 def word_count_engine(document):
-  cd=[]
-  for w in document.split():
-    w=w.lower()
-    cw=[]
-    for let in w:
-      if ord('a')<=ord(let)<=ord('z'):
-        cw.append(let)
-    cd.append(''.join(cw))
-  
-  freq=Counter()
-  oil={}
-  oi=0
-  for w in cd:
-    freq[w]+=1
-    if w not in oil:
-      oil[w]=oi
-      oi+=1
-  
-  freql=list(freq.items())
-  #print(freql)
-  interm=sorted(freql,key=lambda (w,wf): (-wf,oil[w]))
-  return [[w,str(wf)] for w,wf in interm]
-
-document="b' a a"
-print(word_count_engine(document))
+  doc=remove_punc(document.lower())
+  ordlookup=get_ord(doc)
+  wcount=Counter(doc)
+  wtups=list(wcount.items()) # (a,2) (b,3)
+  wtups.sort(key=lambda x: (-x[1],ordlookup[x[0]]))
+  return [[word,str(freq)] for (word,freq) in wtups]
 
 document = "Practice makes perfect. you'll only get Perfect by practice. just practice!"
 print(word_count_engine(document))
-from collections import Counter
-from itertools import count
-def word_count_engine(document):
-  doc=document.split()
-  for i,w in enumerate(doc):
-    doc[i]= ''.join(x for x in w.lower() if ord('a')<=ord(x)<=ord('z'))   
-  freq=Counter(doc)
-  res={}
-  counter=count()
-  for w in doc:
-    if w not in res:
-      res[w]=[freq[w],next(counter)]   
-  res_tup=sorted(res.items(),key=lambda t: (-t[1][0],t[1][1]))
-  return [[w,str(f)] for (w,(f,_)) in res_tup]
+
+'''
+output: [ ["practice", "3"], ["perfect", "2"],
+          ["makes", "1"], ["youll", "1"], ["only", "1"], 
+          ["get", "1"], ["by", "1"], ["just", "1"] ]
+'''
