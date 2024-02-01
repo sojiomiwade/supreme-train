a6fea6b147e62c22c9560c67a93702418c9bd54e
Soji Omiwade <omiwade@gmail.com>
2024-02-01 11:16:35 -0800

    add BFS, no major mistake, examples were used

--- network-delay-time.py
+++ network-delay-time.py
@@ -20,15 +20,36 @@ spath[k]=
 '''
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        ans=defaultdict(lambda :float('inf'))
+        #now use BFS
+        #do not further process if a node cannot be made cheaper
+        #if can be, make it cheaper, and then add the corresponding neighbors
+        #if edges trigger adding other edges, complexity is E**2
+        friend=defaultdict(dict)
+        #{2:{0:1,1:2},3:{}}
+        for u,v,w in times:
+            friend[u][v]=w
         q=deque([(0,k)])
-        friends,cost=defaultdict(list),{}
-        for (u,v,cuv) in times:
-            friends[u].append((v,cuv))
+        ans=defaultdict(lambda :float('inf'))
         while q:
-            cx,nx=q.popleft()
-            if cx<ans[nx]:
-                ans[nx]=cx
-                for ny,cxy in friends[nx]:
-                    q.append((cx+cxy,ny))
+            cu,u=q.popleft()
+            if cu<ans[u]:
+                ans[u]=cu
+                for v,cuv in friend[u].items():
+                    q.append((cuv+cu,v))
         return max(ans.values()) if len(ans)==n else -1
+
+
+
+
+        # ans=defaultdict(lambda :float('inf'))
+        # q=deque([(0,k)])
+        # friends,cost=defaultdict(list),{}
+        # for (u,v,cuv) in times:
+        #     friends[u].append((v,cuv))
+        # while q:
+        #     cx,nx=q.popleft()
+        #     if cx<ans[nx]:
+        #         ans[nx]=cx
+        #         for ny,cxy in friends[nx]:
+        #             q.append((cx+cxy,ny))
+        # return max(ans.values()) if len(ans)==n else -1
