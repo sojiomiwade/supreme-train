bf76b4295c9854975437eb055891ff1edcae68fa
Soji Omiwade <omiwade@gmail.com>
2024-02-01 10:01:58 -0800

    28m, didnot consider the general case: graph can have cycles

---
+++ network-delay-time.py
@@ -0,0 +1,36 @@
+'''
+shortest path from node k to all nodes, return the maximum
+can use queue or BFS:
+queue starts with (0,2)
+11 13 
+{2:0 1:1}
+
+(9,4) -- 4 got added by say 2
+but later (2,4) gets added by 3, and damages are fixed
+cost: 
+14 .5 .5
+10 1 1 1 1 
+cost is just all the edges considered: O(V + E lg E)
+spath[k]=
+
+(2,3) (4,x)
+   -  3 - - - 
+ /
+1-x----------2
+'''
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        ans=defaultdict(lambda :float('inf'))
+        q=deque([(0,k)])
+        friends,cost=defaultdict(list),{}
+        for (u,v,cuv) in times:
+            friends[u].append(v)
+            cost[u,v]=cuv
+        while q:
+            cx,nx=q.popleft()
+            for ny in friends[nx]:
+                trycost=cx+cost[nx,ny]
+                if trycost<ans[ny]:
+                    q.append((trycost,ny))
+            ans[nx]=min(ans[nx],cx)
+        return max(ans.values()) if len(ans)==n else -1
