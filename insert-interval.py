ee5ae65ca719bac758711c3de2cee7717839a385
Soji Omiwade <omiwade@gmail.com>
2024-02-02 12:17:42 -0800

    break them into merge,left,right

--- insert-interval.py
+++ insert-interval.py
@@ -1,61 +1,67 @@
 '''
 
----
-    ---
-         -
-            --------
-      -------             <--new
-    ---------------      <-- all 3 above replaced with this
-find all the intervals i have intersection with and mark them 
-tomerge=[a,b,c,new]
-new2 is just minstart+maxend
-ans=[everything except tomerge + new2]
-put intervals into a set. then can now remove a tuple in O(1) time
-tomerge must be a set, and this is the only set
-ans can be a list
-            -
-            44
-             -
-             55
-                      1-----5
-                       4------9
-            ea
+if merge: => left+right!=ints
+    then get merged
+return left + merged + right
+merged=(min(ints[len(left)][0],s),  max(ints[-len(right)-1][1],e))
 
-123456789
--
-  ---
-   ------ new
+s--e                                
+left=ints
+right=[]
+or could have it the other way. 
+in these cases, can still do
+left+merge+right
+
+left and right can be collected based on s & e
+
+a and b can be (inf,-inf) 
+i=len(left) 
+j=-len(right)-1
+
+a: first overlapping interval 
+b: last overlapping interval
+merge is just min(a.left,s),max(b.right,e)
+merge
+if 
+then re
+          --- ----  - - -----    -- -- ----          
+                                  s-------e
+                        s-------e 
+    s-------e
+s---e
+                                            s---e
+
+          --- ----  - - -----    -- -- ----          
+    s-------e
+left,right=[],[all except first]
+[[1,3],[6,9]]
+[2,5]
+output : [[1,9]]
+exp :    [[1,5],[6,9]]
+
+23456789
+--- ----
+---
+
+  ------6
+     2-----
+overlaps=>max(ba,bb)-min(ea,eb)>=0
+     
+    be b
+    a---5 6
+        c----d
+    c----d
+c----d
+(2,5),(5,7)
+overlaps= 5 == or  5
 '''
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        def overlaps(inta,intb) -> bool:
-            ba,ea=inta
-            bb,eb=intb
-            # return bb<=ba<=eb or ba<=bb<=ea 
-            #     ------
-            #        -------
-            return max(bb,ba)<=min(ea,eb)
-
-        def merge(inta,intb):
-            ba,ea=inta
-            bb,eb=intb
-            return [min(bb,ba),max(ea,eb)]
-
-
-        n=len(intervals)
-        ans=[]
-        for idx,interval in enumerate(intervals):
-            if interval[1]<newInterval[0]:
-                ans.append(interval)
-            elif overlaps(interval,newInterval):
-                newInterval=merge(interval,newInterval)
-            else:
-                break
-        else:
-            idx=n # new is to the right of everything
-        
-        ans.append(newInterval)
-
-        for sidx in range(idx,n):
-            ans.append(intervals[sidx])
-        return ans
+        s,e=newInterval
+        left=[[cs,ce] for (cs,ce) in intervals if ce<s]
+        right=[[cs,ce]for (cs,ce) in intervals if e<cs]
+        merged=[[s,e]]
+        if left+right!=intervals:
+            merged=[[min(intervals[len(left)][0],s),
+                max(intervals[-len(right)-1][1],e)]]
+        return left+merged+right
