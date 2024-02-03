c8e86b44b3678cfa56add86ba2c5c4b2a49327ea
Soji Omiwade <omiwade@gmail.com>
2024-02-02 16:06:35 -0800

    got lost in the testing phase

--- minimum-size-subarray-sum.py
+++ minimum-size-subarray-sum.py
@@ -1,39 +1,35 @@
 '''
-2,3,1,2,4,3,2 2
-        l
-          r
-7
-ans=2
-when sum is too big, increment l until it's too small => while loop on l
-but do check at l or r if we get the sum. 
-2 4 3
-  l
-    r
-cur,ans=7,2
-target=7
+>=target
+could do it in O(n**2) by considering all l,r l<r
 
-1,2,3,4,5
-l
-r
-cur,ans=0,0
-target=7
+cursum=0
+for r in [0..n-1]
+    cursum+=nums[r]
+    while cursum >=target 
+        L,R=l,r if l,r is smaller
+        cursum-=nums[l]
+        l+=1
+return R-L+1 if L is not -inf else 0
+1 3 -7 5 1 -3 4 . . .
+       L      R
+
+target = 2
+1 3 -7
+     l
+     r
+cs=0
+L,R=1,1
 '''
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        left,n=0,len(nums)
-        ans=1+n
-        cur=0
-        for right in range(n):
-            cur+=nums[right]
-            while cur>=target:
-                ans=min(ans,right-left+1)
-                cur-=nums[left]
-                left+=1
-        if ans==1+n:
-            return 0
-        return ansquote> 2 1 -4 4 7
-quote> 2 3 -1 4 11
-quote> l resets when prev is rejected
-quote> when nums[i] is negative or nums[i] or , update minsize_maxl and minsize_maxr with l and r-1
-quote> prev is rejected when nums[i-1] is <=0 
-quote> minsize_maxr and minsize_maxl are changed only if cursum is bigger than maxsum
+        L,R=float('-inf'),float('inf')
+        cursum,n,l=0,len(nums),0
+        for r in range(n):
+            cursum+=nums[r]
+            while cursum>=target: 
+                if r-l<R-L:
+                    L,R=l,r
+                cursum-=nums[l]
+                l+=1
+        return R-L+1 if L != float('-inf') else 0
+        
