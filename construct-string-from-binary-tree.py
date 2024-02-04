463df2751569a2a2d7758bee7b064e23fb08814f
Soji Omiwade <omiwade@gmail.com>
2024-02-04 11:14:04 -0800

    17m, walkthrough: instead of returning ans, called the tree2str func, which has already being called for root children. walkthrough: missed self. on call

--- construct-string-from-binary-tree.py
+++ construct-string-from-binary-tree.py
@@ -5,23 +5,33 @@
 #         self.left = left
 #         self.right = right
 '''
-1(2()(4))(3)
-[1,(2()(4))()]
-idea: if there is nothing in right, omit the parentheses. otherwise dont
+               1
+            /     \ 
+           2       3
+1(2)(3)
+
+               1
+            /     \ 
+           2       3
+1()(3)
+
+               1
+            /     \ 
+           2       
+1(2)
+
+
+example 1: 1(2(4))(3)
+example 2: 1(2()(4))(3)
 '''
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
-        def tree2str(root):
-            if root:
-                res.append(str(root.val))
-                if root.left or root.right:
-                    res.append('(')
-                    tree2str(root.left)
-                    res.append(')')
-                    if root.right:
-                        res.append('(')
-                        tree2str(root.right)
-                        res.append(')')
-        res=[]
-        tree2str(root)
-        return ''.join(res)
+        if not root:
+            return ''
+        ans=str(root.val)
+        if not root.left and not root.right:
+            return ans
+        ans+=f'({self.tree2str(root.left)})'
+        if root.right:
+            ans+=f'({self.tree2str(root.right)})'
+        return ans
