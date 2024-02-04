b51fa81814ab699b6074ba104f8a27e2af0c55ce
Soji Omiwade <omiwade@gmail.com>
2024-02-04 10:40:34 -0800

    overlooked prunning with continue if remleaps==0

--- cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops.py
@@ -17,13 +17,40 @@ class Solution:
         f,L,INF=defaultdict(dict),k+1,float('inf')
         for u,v,w in flights:
             f[u][v]=w
-        q=deque([(0,src,L)])
-        ans=defaultdict(lambda :INF)
-        while q:
-            cu,u,remleaps=q.popleft()
-            if cu<ans[u]:
-                ans[u]=cu
-                if remleaps>0:
-                    for v,cuv in f[u].items():
-                        q.append((cu+cuv,v,remleaps-1))
-        return ans[dst] if ans[dst] != INF else -1
+        heap=[(0,src,L)]
+        '''
+        key question in heap: can ans[x] get changed?
+        yes, only to be made more expensive since cheaper
+        routes had prior claimed it was cheaper, but now
+        it must be made more expensive
+        so remleaps is the only admission control
+        so to 
+        a --- b
+        |\__  |
+        |   \ |
+        c --- d
+        cheapest routes must be pruned =>
+        '''
+        minremleaps=defaultdict(lambda :-INF)
+        while heap:
+            cu,u,remleaps=heapq.heappop(heap)
+            if u==dst:
+                return cu
+            if remleaps==0:
+                continue
+            if remleaps>=minremleaps[u]:
+                minremleaps[u]=remleaps
+                for v,cuv in f[u].items():
+                    heapq.heappush(heap,(cu+cuv,v,remleaps-1))
+        return -1 # once i hit the destination i am done
+
+        # q=deque([(0,src,L)])
+        # ans=defaultdict(lambda :INF)
+        # while q:
+        #     cu,u,remleaps=q.popleft()
+        #     if cu<ans[u]:
+        #         ans[u]=cu
+        #         if remleaps>0:
+        #             for v,cuv in f[u].items():
+        #                 q.append((cu+cuv,v,remleaps-1))
+        # return ans[dst] if ans[dst] != INF else -1
