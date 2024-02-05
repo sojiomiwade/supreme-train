3784cc4d01d8cf96385edf38c6906c4876327938
Soji Omiwade <omiwade@gmail.com>
2024-02-04 22:06:29 -0800

    listen/example: did not use both; used dict instead of set for visited at first

--- keys-and-rooms.py
+++ keys-and-rooms.py
@@ -1,15 +1,20 @@
+'''
+0 1 2 3 ... n
+if len(visited)==n return true, false otherwise
+DFS (or BFS) starting from 0, and just visit
+'''
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        '''
-        0 -> 1 -> 2 -> 3
-        v = {0 1}
-        '''
-        def canVisitAllRooms(room):
+        def visit(room):
+            if room in visited:
+                return
+            visited.add(room)
             for oroom in rooms[room]:
-                if oroom not in visited:
-                    visited.add(oroom)
-                    canVisitAllRooms(oroom)
-
-        visited = set([0])
-        canVisitAllRooms(0)
-        return len(visited) == len(rooms)
+                visit(oroom)
+        # 0 -- 1
+        #  \   /     
+        #.   2
+        # v={0,1,2s}
+        visited,n=set(),len(rooms)
+        visit(0)
+        return len(visited)==n
