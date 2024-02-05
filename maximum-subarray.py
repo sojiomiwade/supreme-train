18c7b9b03807517a529717741417bbc33230c095
Soji Omiwade <omiwade@gmail.com>
2024-02-05 14:50:58 -0800

    17m, testing: did not run a full example, include the array indexes defining the maximum subarray

--- maximum-subarray.py
+++ maximum-subarray.py
@@ -1,15 +1,27 @@
+'''
+       L    R
+2 5 -8 4 5 -3 4 -1
+2 7 -1 4 9 7 11 10 
+       l
+                r
+find L,R with maximum sum
+don't forget max sum subarray problem: if newsum with me is greater
+than me, take me otherwise take new sum. at this point reset l
+update L,R always
+
+'''
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        l=rsum=0
-        msum=float('-inf')
-        bl=br=None
-        for i,x in enumerate(nums):
-            if rsum<0:
-                rsum=0
-                l=i
-            rsum+=x
-            if rsum>msum:
-                msum=rsum
-                bl,br=l,i
-        assert msum==sum(nums[i] for i in range(bl,br+1))
-        return msum
+        INF=float('inf')
+        l=L=R=None
+        LRsum=-INF
+        n,cursum=len(nums),0
+        for r in range(n):
+            if cursum<=0:
+                l=r
+                cursum=0
+            cursum+=nums[r]
+            if cursum>LRsum:
+                L,R=l,r
+                LRsum=cursum
+        return sum(nums[i] for i in range(L,R+1))
