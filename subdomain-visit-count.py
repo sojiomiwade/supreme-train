3212285152fa4c578288e76fe1e5cb542e77b14b
Soji Omiwade <omiwade@gmail.com>
2024-02-05 22:59:42 -0800

    20m, but lee215 solution of finding the . and incrementing count from there (after at first doing the whole string) is more extensible for many dots!

--- subdomain-visit-count.py
+++ subdomain-visit-count.py
@@ -1,21 +1,27 @@
 '''
-discuss.leetcode.com
-discuss  leetcode  com
-com leetcode discuss
+cpdomains = [
+    "900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
 
-com.leetcode
-[com,leetcode,discuss]
+for each dom, split it tokens, then make a deque out of it
+for each token
+    count[token+rest-of-tokens]+=rep
+    tokens.popleft(0)
+then output [count key-of-count]
 '''
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        dcount=Counter()
-        for countplusdomain in cpdomains:
-            count,cpdomain=countplusdomain.split()
-            cur=[]
-            for subdomain in reversed(cpdomain.split('.')):
-                cur.append(subdomain)
-                dcount[tuple(cur)]+=int(count)
-        res=[]
-        for rdomain,count in dcount.items():
-            res.append(str(count) + ' ' + '.'.join(reversed(rdomain)))
-        return res
+        count=Counter()
+        for cpdomain in cpdomains:
+            rep,doms=cpdomain.split()
+            irep=int(rep)
+            doms_l=cpdomain.split('.')
+            if len(doms_l)==3:
+                a,b,c=doms_l
+                count[doms]+=irep
+                count[f'{b}.{c}']+=irep
+                count[c]+=irep
+            else:
+                b,c=doms_l
+                count[doms]+=irep
+                count[c]+=irep
+        return [f'{countval} {irep}' for irep,countval in count.items()]
