bd151bde6966524002889238f669a00ef14ad9df
Soji Omiwade <omiwade@gmail.com>
2024-02-09 07:43:21 -0800

    use bisect for n lg n time

--- longest-increasing-subsequence.py
+++ longest-increasing-subsequence.py
@@ -1,9 +1,28 @@
+'''
+10 9  2  5  3  7  25 18
+10 25
+9 25
+2 5 7 25
+2 3 7 25
+2 3 7 25
+
+2, 6, 8, 3, 4, 5, 1 2 6 6
+-        -  -  -
+[2 3 8]
+bisect left, so existing number is overwritten
+if ans == n, then append
+2 3
+max=3
+'''
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n=len(nums)
-        dp=[1 for _ in range(n)]
-        for right in range(n):
-            for left in range(right):
-                if nums[right]>nums[left]:
-                    dp[right]=max(dp[right],1+dp[left])
-        return max(dp)
+        ans=[]
+        for num in nums:
+            idx=bisect.bisect_left(ans,num)
+            if idx==len(ans):
+                ans.append(num)
+            else:
+                ans[idx]=num
+        return len(ans)
+
