2214a67643a6483a28f2ddf84c93231b448d9bf7
Soji Omiwade <omiwade@gmail.com>
2024-02-11 15:31:01 -0800

    13m, testing is a must to success

---
+++ find-pivot-index.py
@@ -0,0 +1,25 @@
+'''
+1 7 3 6 5 6
+          x
+suml is 0 and sumr is the sum of all except the 1st. idx starts at 0
+check if suml and sumr equal. return the idx
+if idx+1==n return -1
+adjust suml to include idx, and sumr to exclude idx+1
+advance idx go back to check
+
+3 2 5
+  i
+suml sumr : 5 5
+'''
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n=len(nums)
+        suml,sumr=0,sum(nums)-nums[0]
+        idx=0
+        while suml!=sumr:
+            if idx+1==n:
+                return -1
+            suml+=nums[idx]
+            sumr-=nums[idx+1]
+            idx+=1
+        return idx
