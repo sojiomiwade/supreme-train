a70f2a9f8d020a80e2abefb836d03271bb3f4d65
Soji Omiwade <omiwade@gmail.com>
2024-02-11 09:11:45 -0800

    pays to really understand the problem and your solution

--- cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops.py
@@ -1,55 +1,45 @@
-'''
-s --> m --> d
-L : the number of leaps allowed. k = 1 => L = 2, 
-leaps[src]=L
-can use BFS or heap(dijkstra)
-BFS: with src starting in queue as (cost,src,remleaps), 
-    any dequeue must satisfy this to add children
-    if is dst, update cost as needed and be done
-    otherwise, 
-        1. remleaps>0
-        2. cost is cheaper than cost at the node
-        3. then go ahead and add downstreams
-time,space: O(V + E**2): every edge's addition can trigger E edges to be re-added
-'''
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        f,L,INF=defaultdict(dict),k+1,float('inf')
-        for u,v,w in flights:
-            f[u][v]=w
-        heap=[(0,src,L)]
         '''
-        key question in heap: can ans[x] get changed?
-        yes, only to be made more expensive since cheaper
-        routes had prior claimed it was cheaper, but now
-        it must be made more expensive
-        so remleaps is the only admission control
-        so to 
-        a --- b
-        |\__  |
-        |   \ |
-        c --- d
-        cheapest routes must be pruned =>
+        if a (ucost,u,rem_hops) is removed, we will check if rem_hops is strictly less 
+        than  the last there
+        if it does, it could (i.e., not guaranteed to) win: the last didn't make it!
+        criteria to allow: if rem_hops > last_rem_lookup[u]
+        now if lrl[u] starts 0, and we hit a node with 0 rem_hops, we cannot advance!
+        now, we can freely change the cost (increase it because the last one couldn't)
+        now set the cost[u] (hashmap) to 5 from 3
+        what do we start each rem_hops to!
+            the 1st node must always make it, no matter. 
+            could use -inf. could also use 0. since if you come to a node
+            and you only have 0, then you can't move forward
+            src b c dst
+             3  2 1 0
+                rem_hops : 2
+        we don't have to exhaust the heap, but if we do, there's no way to get to 
+        dst in k stops
+
+        complexity: up to k times we run a 
+            heapsearch: E (lg E)
+        => k E lg E ? 
+        break it down to BFS
+        initialization: E (the number of nodes is the number of connections)
+        loop: think BFS but we do it k times:
+             k E lg E
         '''
-        minremleaps=defaultdict(lambda :-INF)
+        f=defaultdict(dict)
+        for u,v,cuv in flights:
+            f[u][v]=cuv
+        lrl=defaultdict(int)
+        MAXHOPS=k+1
+        heap=[(0,MAXHOPS,src)]
+        cost={}
         while heap:
-            cu,u,remleaps=heapq.heappop(heap)
+            (cu,remhops,u)=heapq.heappop(heap)
             if u==dst:
                 return cu
-            if remleaps==0 or remleaps<minremleaps[u]:
-                continue
-            minremleaps[u]=remleaps
-            for v,cuv in f[u].items():
-                heapq.heappush(heap,(cu+cuv,v,remleaps-1))
-        return -1 # once i hit the destination i am done
-
-        # q=deque([(0,src,L)])
-        # ans=defaultdict(lambda :INF)
-        # while q:
-        #     cu,u,remleaps=q.popleft()
-        #     if cu<ans[u]:
-        #         ans[u]=cu
-        #         if remleaps>0:
-        #             for v,cuv in f[u].items():
-        #                 q.append((cu+cuv,v,remleaps-1))
-        # return ans[dst] if ans[dst] != INF else -1
+            if remhops>lrl[u]:
+                cost[u]=cu
+                lrl[u]=remhops
+                for v,cuv in f[u].items():
+                    heapq.heappush(heap,(cu+cuv,remhops-1,v))
+        return -1
