443f8a7cb849133f73a0b1a9c4a65ef56392cf52
Soji Omiwade <omiwade@gmail.com>
2024-02-12 15:55:59 -0800

    11m moore voting

--- majority-element.py
+++ majority-element.py
@@ -1,33 +1,30 @@
 '''
-2 2 2 2 1 3 5
-        ^
-153
+2 2
+2/2 = 1
 
-zy5x5    <--we make 00101 since {x,y,z}<5 
-approach: get the max freq, and make a number having only those bit freq equal to maxfreq
+2 2 3 4 5
+        c
+ans=2
+count=2
 
-when -ve, we have
-11111| 1|5xyzz5
-      32
-   0000000000|1|100001      <---wrong cuz it's positive
- | 1111111111|1|000000
-   1111111111|
-111110
-000010
-1111
+    count=
+    seen
+count starts at 0
+
+3/2 = 1
+c
+repeat for each element
+    if count is zero [no majority eleme]
+        set ans to cur
+    count += 1 if cur is ans else -1
+return ans
 '''
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        res=0
-        mask=1
-        n=len(nums)
-        for i in range(32):
-            freq=0
-            for x in nums:
-                freq+=bool(mask&x)
-            if freq>n//2:
-                res|=mask
-            mask<<=1
-        if (1<<31)&res != 0: #bit 31 is set => number is negative
-            res|=-1<<31
-        return res
+        count=0
+        ans=None
+        for cur in nums:
+            if not count:
+                ans=cur
+            count+=1 if cur==ans else -1
+        return ans
