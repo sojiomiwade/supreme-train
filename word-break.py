aee39f1cfa3e1a0593e80d36d80510869718ef6c
Soji Omiwade <omiwade@gmail.com>
2024-02-17 06:51:43 -0800

    add tabulation and try to derive the runtime complexities(did so on tabulation by code) but the recursive evades me

--- word-break.py
+++ word-break.py
@@ -15,22 +15,58 @@ applepenapple
       -imagine taking a p p l e, but if we had apple we already did it
         when e gets there!
       wb at i is the aggregate of using each word if possible
+
+      dp
+      0123456789012
+      applepenapple
+      fffftfftfffft
+
+      dp[i]=dp[i-m] and word==s[i-m:i] for any word
+
+W=wm
+memoization complexity: n/m * ?mW = nW
+
+W + (W+1) + ... W+n/m = nW/m
+<------n/m---->
+2 3 4 5 
+
+            /    \
+           / \   / \  
+tabulation complexity: nW
+01234567
+     
+leetcode
+fff?ffff
+   i
+s[4-4:4]==word
+dp[4]
 '''
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        def wb(i: int) -> bool:
-            if i==n:
-                return True
-            if i in dp:
-                return dp[i]
+        n=len(s)
+        dp=[False]*(n+1)
+        dp[0]=True
+        for i in range(1,1+n):
             for word in wordDict:
                 m=len(word)
-                if s[i:i+m]==word and wb(i+m):
+                if i-m>=0 and dp[i-m] and word==s[i-m:i]:
                     dp[i]=True
-                    return True
-            dp[i]=False
-            return False
+                    break
+        return dp[n]
 
-        dp={}
-        n=len(s)
-        return wb(0)
+        # def wb(i: int) -> bool:
+        #     if i==n:
+        #         return True
+        #     if i in dp:
+        #         return dp[i]
+        #     for word in wordDict:
+        #         m=len(word)
+        #         if s[i:i+m]==word and wb(i+m):
+        #             dp[i]=True
+        #             return True
+        #     dp[i]=False
+        #     return False
+
+        # dp={}
+        # n=len(s)
+        # return wb(0)
