e40789f9f11bfe40ed1050eae2272ef8dd8859f1
Soji Omiwade <omiwade@gmail.com>
2024-02-19 15:46:18 -0800

    ''' create a mapping for each word abca: abc cba: abc ... now loop (i and j) when i and j have same mappings increment count time,space: O((wn)**2),O(nw)
    ab          0
    abb         1
    abba        2
    abbbba      3
    .
    3+2+1=6
    i :    0
    ans :  0 1 3 6 10
    freq : 1 2 3 4
    so two things going on:
        1. can use a 32-bit int to manage the set of chars (which you don't have to do) as set suffices but adds to the space complexity
        2. analytically, everytime you add a new token (that has x others sharing the map
            ), then the answer should increment by the number x (as this new one marries each of the existing x)
    
    so ans is always ans+=count[mapped], and count[mapped] just always increments by 1

--- count-pairs-of-similar-strings.py
+++ count-pairs-of-similar-strings.py
@@ -1,25 +1,34 @@
 '''
-a b a d e a
+create a mapping for each word
+abca: abc
+cba: abc
+...
+now loop (i and j) when i and j have same mappings increment count
+time,space: O((wn)**2),O(nw)
 
-abca -> {a,b,c}
-cba -> {a,b,c}
-{a,b,c}
-{b}
+ab          0
+abb         1
+abba        2
+abbbba      3
+.
+3+2+1=6     
+i :    0
+ans :  0 1 3 6 10 
+freq : 1 2 3 4
+so two things going on: 
+    1. can use a 32-bit int to manage the set of chars (which you don't have to do) as set suffices but adds to the space complexity
+    2. analytically, everytime you add a new token (that has x others sharing the map
+        ), then the answer should increment by the number x (as this new one marries each of the existing x)
 
-abc: 2 => 2 choose 2 = 1
-bab: 3 => 3 choose 2 = 3
-ans = 4
-
-bbaaac
+so ans is always ans+=count[mapped], and count[mapped] just always increments by 1
 '''
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        freq=Counter()
-        res=0
-        for word in words:
-            key=0
-            for ch in word:
-                key|=(1<<(ord(ch)-ord('a')))
-            res+=freq[key]
-            freq[key]+=1
-        return res
+        unique={word:set(word) for word in words}
+        n=len(words)
+        count=0
+        for i in range(n):
+            for j in range(i+1,n):
+                if unique[words[i]]==unique[words[j]]:
+                    count+=1
+        return count
