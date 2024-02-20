ad884be2a33ad4d4ed4a296e21bfa9009547ee6b
Soji Omiwade <omiwade@gmail.com>
2024-02-20 06:37:56 -0800

    17m, my own permutation way

--- list_sorted_k_length_permuations.py
+++ list_sorted_k_length_permuations.py
@@ -1,27 +1,37 @@
+'''
+List all k-length permutations that are sorted (lowercase only)
+k=3
+abd
+abc
+aaa
+no! --> bba
+-----
+0.. k
 
-def filter_for_sorted(slist: List[str]) -> List[str]:
-    res=[]
-    for s in slist:
-        for i in range(1, len(s)):
-            if s[i-1] > s[i]:
-                break
+k=2
+aux [aa]
+printed: aa,ab,ac,ad,ae
+'''
+from typing import List
+
+
+def list_sorted_k_perm(k: int) -> None:
+    def sorted(arr: List[str]) -> bool:
+        for i in range(1,len(arr)):
+            if arr[i]<arr[i-1]:
+                return False
+        return True
+
+    def listsorted(aux: List[str]):
+        if len(aux)==k:
+            if sorted(aux):
+                print(''.join(aux))
         else:
-            res.append(s)
-    return res
+            for i in range(3):
+                listsorted(aux+[chr(ord_a+i)])
 
-def get_k_len_sorted_strings(k: int) -> List[str]:
-    dp=[[] for _ in range(1+k)]
-    dp[0]=['']
-    for i in range(k + 1):
-        for s in dp[i - 1]: # a b
-            for ksi in range(26):
-                ch=chr(ksi + 97)
-                dp[i].append(s+ch)
-        dp[i]=filter_for_sorted(dp[i])
-    return dp[k]
+    ord_a=ord('a')
+    listsorted([])
 
-# print(get_k_len_sorted_strings(2))
-
-ans=get_k_len_sorted_strings(3)
-assert len(ans)==len(set(ans))
-print(len(ans))
+list_sorted_k_perm(3)
+# list_sorted_k_perm(4)
