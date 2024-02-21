c9cf49e11b980cef18303d33768f4ff3215482c2
Soji Omiwade <omiwade@gmail.com>
2024-02-21 07:43:31 -0800

    21m, iterative merge 2 sorted lists

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -3,15 +3,36 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+ch
+
+        l2  
+ 1   8  95 100
+ 
+ 58  93 94
+           l1
+         c
+
+
+       l2
+h-1  2 4
+  V /| |
+  1  3-4
+       c  l1
+if l1 > l2, then swap l1 and l2
+cur points to l1, l1 advances to its next
+cur advances to its next
+at the end cur can just point to l1 or l2
+'''
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        def merge(list1,list2):
-            if not list1:
-                return list2
-            if not list2:
-                return list1
-            if list1.val>list2.val:
-                list1,list2=list2,list1
-            list1.next=merge(list1.next,list2)
-            return list1
-        return merge(list1,list2)
+        l1,l2=list1,list2
+        head=cur=ListNode(0)
+        while l1 and l2:
+            if l1.val>l2.val:
+                l1,l2=l2,l1
+            cur.next=l1
+            cur=cur.next
+            l1=l1.next
+        cur.next=l2 or l1
+        return head.next
