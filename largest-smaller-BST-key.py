88c9f12a9033f8d590c98e6f68a3958b491547b2
Soji Omiwade <omiwade@gmail.com>
2024-02-22 08:18:48 -0800

    10m, actually not difficult at all to operationalize with realization that you only update ans when you go down the right, no max or min needed

--- largest-smaller-BST-key.py
+++ largest-smaller-BST-key.py
@@ -1,27 +1,94 @@
-'''
-          20
-        /    \
-       9     25
-     /   \     
-    5    12
-        /  \
-       11  14
-       
-approach: do a search for num, and set ans to the node in search if that node is 
-smaller than num, but bigger than ans
-if at the end of while, ans is not set, return -1
-'''
-class BST:
+##########################################################
+# CODE INSTRUCTIONS:                                     #
+# 1) The method findLargestSmallerKey you're asked       #
+#    to implement is located at line 30.                 #
+# 2) Use the helper code below to implement it.          #
+# 3) In a nutshell, the helper code allows you to        #
+#    to build a Binary Search Tree.                      #
+# 4) Jump to line 71 to see an example for how the       #
+#    helper code is used to test findLargestSmallerKey.  #
+##########################################################
+
+
+# A node 
+class Node:
+
+# Constructor to create a new node
+  def __init__(self, key):
+      self.key = key
+      self.left = None
+      self.right = None
+      self.parent = None
+
+# A binary search tree 
+class BinarySearchTree:
+
+  # Constructor to create a new BST
+  def __init__(self):
+      self.root = None
+
   def find_largest_smaller_key(self, num):
-    ans=float('-inf')
-    cur=self.root
-    while cur:
-      if cur.key<num:
-        ans=max(ans,cur.key)
-      if num>cur.key:
-        cur=cur.right
+    ans=-1
+    root=self.root
+    while root:
+      if root.key<num:
+        ans=root.key
+        root=root.right
       else:
-        cur=cur.left
-    if ans==float('-inf'):
-      return -1
-    return ans
+        root=root.left
+    return ans    
+    
+
+  # Given a binary search tree and a number, inserts a
+  # new node with the given number in the correct place
+  # in the tree. Returns the new root pointer which the
+  # caller should then use(the standard trick to avoid
+  # using reference parameters)
+  def insert(self, key):
+
+      # 1) If tree is empty, create the root
+      if (self.root is None):
+          self.root = Node(key)
+          return
+
+      # 2) Otherwise, create a node with the key
+      #    and traverse down the tree to find where to
+      #    to insert the new node
+      currentNode = self.root
+      newNode = Node(key)
+
+      while(currentNode is not None):
+        if(key < currentNode.key):
+          if(currentNode.left is None):
+            currentNode.left = newNode
+            newNode.parent = currentNode
+            break
+          else:
+            currentNode = currentNode.left
+        else:
+          if(currentNode.right is None):
+            currentNode.right = newNode
+            newNode.parent = currentNode
+            break
+          else:
+            currentNode = currentNode.right
+
+######################################### 
+# Driver program to test above function #
+#########################################
+
+bst  = BinarySearchTree()
+ 
+# Create the tree given in the above diagram 
+bst.insert(20)
+bst.insert(9);
+bst.insert(25);
+bst.insert(5);
+bst.insert(12);
+bst.insert(11);  
+bst.insert(14);    
+
+for key in (20,9,25,5,17):
+  result = bst.find_largest_smaller_key(key)
+  print (" %d " %(result))
+# 14,5,20,-1,14
