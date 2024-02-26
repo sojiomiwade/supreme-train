da5f4f39947272cb1928ea3d7ef06c8a98c77c72
Soji Omiwade <omiwade@gmail.com>
2024-02-26 10:14:03 -0800

    important to ensure you read the question carefully: 38m

--- 3sum.py
+++ 3sum.py
@@ -1,28 +1,33 @@
 '''
-x        0 0                  x
-i j k cannot be equal
-iterate on each i, then do a 2sum with a j which is greater than i
-arr[i], -arr[i] is what the 2sum must equal
+-1 0 1
+     t
+target 0
+have 0
 
--1,0,1,2,-1,-4
- i   j
- z : 1-1=0
- lookup : {0:1,}
-
- 1 ... 2 ... 3 
- i     j
+0 0 0
+  t
+-1 0 0 1 1
 '''
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        def twosum(idx: int):
+            ans,target=set(),-nums[idx]
+            have=set()
+            for tidx in range(idx+1,n):
+                if target-nums[tidx] in have:
+                    ans.add(tuple(sorted([-target,target-nums[tidx],nums[tidx]])))
+                have.add(nums[tidx])
+            return list(ans)
+
+        ans,n=[],len(nums)
         nums.sort()
-        res=set()
-        for i,x in enumerate(nums):
-            if nums[i]>0:
+        prev=None
+        for idx,x in enumerate(nums):
+            if x>0:
                 break
-            lookup={}
-            for j in range(i+1,len(nums)):
-                z=-x-nums[j]
-                if z in lookup:
-                    res.add(tuple(sorted([nums[i],nums[j],nums[lookup[z]]])))
-                lookup[nums[j]]=j
-        return list(res)
+            if prev is None or prev!=x:
+                print(idx)
+                ans.extend(twosum(idx))
+                prev=x
+        return ans
+        
