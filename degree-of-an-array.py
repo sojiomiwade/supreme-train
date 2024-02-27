e64267926ff401ad70547c6f3a0f3f60ff1c265f
Soji Omiwade <omiwade@gmail.com>
2024-02-26 16:10:50 -0800

    22m, good lebcowit

--- degree-of-an-array.py
+++ degree-of-an-array.py
@@ -1,23 +1,39 @@
 '''
-0123
-1221
-  i
-idx,num : 31
-first : 10 21
-count : 12 22
-deg,res: 2,2
-3-0+1
+1 0 0 0 0 1,2,2,3,,4
+^ ^     
+    i
+ans=None
+maxdeg=-inf
+loop the array on
+    update deg[cur] and first[cur]
+    if deg[cur] is more than maxdeg
+        update ans to i-first[cur]
+        maxdeg=deg of cur
+    elif deg[cur] == maxdeg
+        ans=min(ans,i-first[cur])
+
+maxdeg
+also now update maxdeg to max(deg[cur],maxdeg)
+if current element has highest degree so far, then update ans to i-first[cur]
+also need to track deg[cur]
+0 1 2 3
+1 0 1 0
+      i
+deg 02 12 
+first 01 10
+maxdeg 2
+ans 3
 '''
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        first,count={},Counter()
-        deg=res=0
-        for idx,num in enumerate(nums):
-            first.setdefault(num,idx)
-            count[num]+=1
-            if count[num]>deg:
-                deg=count[num]
-                res=idx-first[num]+1
-            elif count[num]==deg:
-                res=min(res,idx-first[num]+1)
-        return res
+        ans,maxdeg=None,float('-inf')
+        deg,first=Counter(),{}
+        for i,cur in enumerate(nums):
+            deg[cur]+=1
+            first.setdefault(cur,i)
+            if deg[cur]>maxdeg:
+                maxdeg=deg[cur]
+                ans=i-first[cur]+1
+            elif deg[cur]==maxdeg:
+                ans=min(ans,i-first[cur]+1)
+        return ans
