5fab9bdf4a6db2861aaab0d83045efface197fd5
Soji Omiwade <omiwade@gmail.com>
2024-02-27 13:47:19 -0800

    42m, need to use a better example

--- largest-rectangle-in-histogram.py
+++ largest-rectangle-in-histogram.py
@@ -1,33 +1,49 @@
 '''
-2 5
-    r
-[]   nondec
-5 prev
-5 ans
-5*(2-1)
-2*(2-0)
+0 2--
+1 3---
+2 5-----
+3 2--
+4 0
+index
 
---
--
---
-2 1 2
+02 13 25 | 32
+           i
+go forward as long as the rectangles grow adding idx,height to stack
+when we hit something smaller (chi, chh) checkpoint, compute some results
+result is max of itself or (chi-topi)*toph
+pop stack
+push (i-popcount,chh)
+at the end, can just add zero to array to flush it of residue result
 
+0 2--
+1 3---
+2 1-
+3 2--
+4 0
+ans 4
+st [  ]
+prevh 1
+i,curh (30)
+topi,toph 01
+
+area is more than it should be
 '''
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        nondec,ans,n=[],float('-inf'),len(heights)
-        for r in range(n):
-            width=1
-            idx=r
-            while nondec and nondec[-1][0]>heights[r]:
-                h,idx=nondec.pop()
-                ans=max(ans,h*(r-idx))
-            nondec.append((heights[r],idx))
-
-        r=n
-        while nondec:
-            h,idx=nondec.pop()
-            ans=max(ans,h*(r-idx))
+        ans=0
+        st=[]
+        heights.append(0)
+        prevh=0
+        for i,curh in enumerate(heights):
+            if curh>=prevh:
+                st.append((i,curh))
+            else:
+                topi=i
+                while st and st[-1][1]>=curh:
+                    topi,toph=st.pop()
+                    ans=max(ans,(i-topi)*toph)
+                st.append((topi,curh))
+            prevh=curh
+        heights.pop()
         return ans
 
-            
