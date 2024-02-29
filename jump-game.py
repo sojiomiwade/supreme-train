2252121cf5dd3eb7d4b4ea30d1598c51f808e19e
Soji Omiwade <omiwade@gmail.com>
2024-02-29 14:42:44 -0800

    quick practice

--- jump-game.py
+++ jump-game.py
@@ -1,27 +1,21 @@
 '''
-once i have done an index, no need to do it again in the future => dp array
-at curidx, can go multiple places (number of which is in that slot)
-once idx >= n, we are done
-oidx in range(1,4)
-canjump(0+1...3)
-canjump(1 + 1..2)
-canjump(2 + [1..1])
-canjump(3 + )
+
+arr 2 3 1 1 4
+idx 0 1 2 3 4
+            ^
+mi 4
+update maxidx as you go to max of itself and i+arr[i]
+if at any idx maxidx<curidx return False
+if idx==n-1 return True
+       6
+maxidx 5
+
 '''
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        def canjump(idx: int) -> bool:
-            if idx>=n-1:
-                return True
-            if dp[idx] is not None:
-                return dp[idx]
-            for oidx in range(1,1+nums[idx]):
-                if canjump(idx+oidx):
-                    dp[idx]=True
-                    return dp[idx]
-            dp[idx]=False
-            return dp[idx]
-
-        n=len(nums)
-        dp=[None for _ in range(n)]
-        return canjump(0)
+        mi=0
+        for i,x in enumerate(nums):
+            if mi<i:
+                return False
+            mi=max(mi,i+nums[i])
+        return True
