3c5b6e88615723d8e3e5f4a54bf6300725c7653a
Soji Omiwade <omiwade@gmail.com>
2024-02-29 09:46:56 -0800

    remove the answer, and add the question

--- is-rotation-via-substring.py
+++ is-rotation-via-substring.py
@@ -1,21 +1,6 @@
 '''
-wat|erbottle -- xy
-erbottle|wat -- yxyx
-substring -- 
+Given a string s and t, return true if t is a rotation of s, using only the method substring.
+A string t is a rotation of s if
+1. s and t have the same length.
+2. for some k>=0, t[k:]+t[:k] equals s. In other words, cut some portion to the end, and pad it on front
 '''
-def isrotation_via_substr(s1: str, s2: str) -> bool:
-    return s1 in 2*s2
-
-s1='waterbottle'
-s2='erbottlewat'
-print(isrotation_via_substr(s1,s2)) # true
-s1='fish'
-s2='hsif'
-print(isrotation_via_substr(s1,s2)) # false
-s1='ooooo'
-s2='ooooo'
-print(isrotation_via_substr(s1,s2)) # true
-s1='waterbotle'
-s2='erbottlewat'
-print(isrotation_via_substr(s1,s2)) # false
-
