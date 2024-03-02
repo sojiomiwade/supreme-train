c4f6fb9fe2da54ad54bdc7e91742cbc4522ade5c
Soji Omiwade <omiwade@gmail.com>
2024-03-02 14:57:55 -0800

    9m, lebcowit ok

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,28 +1,24 @@
 '''
-1 1
-0 0
+from driver visit all cells: if any cell is unvisited and it is a '1'
+    increment a count, and DFS there to visit the corresponding component
+return count
 '''
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def try2visit(r: int, c: int) -> None:
-            if (r,c) in visited or not (0<=r<m and 0<=c<n) or grid[r][c]=='0':
-                return
-            visited.add((r,c))
-            try2visit(r+1,c)
-            try2visit(r-1,c)
-            try2visit(r,c+1)
-            try2visit(r,c-1)
-        '''
-        can go for DFS, on each of the mn cells, and count
-        that cell if it hasn't been visited
-        recursive call: try2visit on neighboring cells
-        '''
+        def comp_visit(r:int, c:int) -> None:
+            if 0<=r<m and 0<=c<n and (r,c) not in visited and grid[r][c]=='1':
+                visited.add((r,c))
+                comp_visit(r+1,c)
+                comp_visit(r-1,c)
+                comp_visit(r,c+1)
+                comp_visit(r,c-1)
+
         m,n=len(grid),len(grid[0])
+        count=0
         visited=set()
-        ans=0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]=='1' and (i,j) not in visited:
-                    ans+=1
-                    try2visit(i,j)
-        return ans
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c]=='1' and (r,c) not in visited:
+                    count+=1
+                    comp_visit(r,c)
+        return count
