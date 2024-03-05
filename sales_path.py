b5507c000e7a016eba3425386c4a9ac8c3bc43ef
Soji Omiwade <omiwade@gmail.com>
2024-03-05 10:45:11 -0800

    11m including manual own tests

--- sales_path.py
+++ sales_path.py
@@ -1,38 +1,38 @@
 '''
-need to consider every path and return the mincost path
-time,space=O(V+E),O(V+E) via DFS for example
-if we find better replace the current
-cost[i] = mycost+mincost of my children
-if root:
-  update mincost
+can bottom up this
+a node's mincost is the minchild cost, and do this recursively
 '''
 def get_cheapest_cost(rootNode):
-  minchildcost=float('inf') if rootNode.children else 0
-  for chnode in rootNode.children:
-    minchildcost=min(minchildcost,get_cheapest_cost(chnode))
-  return rootNode.cost+minchildcost
-  
+  def gcc(root):
+    if not root:
+      return 0
+    mincost=MAXCOST
+    for child in root.children:
+      mincost=min(gcc(child),mincost)
+    return root.cost+(0 if mincost==MAXCOST else mincost)
+  MAXCOST=1+100000
+  return gcc(rootNode)
 ########################################## 
 # Use the helper code below to implement #
 # and test your function above           #
 ##########################################
 
+
 # A node 
 class Node:
-
   # Constructor to create a new node
   def __init__(self, cost):
     self.cost = cost
     self.children = []
     self.parent = None
-
-n51,n31,n61=Node(5),Node(3),Node(6)
-n42,n22,n02,n12,n52=Node(4),Node(2),Node(10),Node(1),Node(5)
-n51.children=[n42]
-n31.children=[n22,n02]
-n61.children=[n12,n52]
-children=[n51,n31,n61]
+ 
 root=Node(0)
-root.children=children
+five=Node(5)
+three=Node(3)
+two=Node(2)
+two.children=[Node(2)]
+three.children=[two,Node(10)]
+six=Node(6)
+root.children=[five,three,six]
 
-print(get_cheapest_cost(root)) #3
+print(get_cheapest_cost(root)) # 3
