71fc1cbcb22783bc66740ff759cfdfb34aae5e6b
Soji Omiwade <omiwade@gmail.com>
2024-03-05 10:50:22 -0800

    3m including testing in head

--- absolute_value_sort.py
+++ absolute_value_sort.py
@@ -1,12 +1,7 @@
-"""
-2 ->2,1
--2->2,0
--7->7,0
-0 ->0,0
-"""
-
 def absSort(arr):
-  return sorted(arr,key=lambda x: (abs(x),x>0))
-
-arr = [2, -7, -2, -2, 0]
-print(absSort(arr))
+  """
+  @param arr: int[]
+  @return: int[]
+  """
+  arr.sort(key=lambda x: (abs(x),x))
+  return arr
