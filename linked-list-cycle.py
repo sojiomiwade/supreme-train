2172e5c26bc201cc4fb1b690edafc86978c44d59
Soji Omiwade <omiwade@gmail.com>
2024-03-06 08:35:06 -0800

    slowed down a little to see i could do it simpler

--- linked-list-cycle.py
+++ linked-list-cycle.py
@@ -4,21 +4,23 @@
 #         self.val = x
 #         self.next = None
 '''
-0 1 2 3 4 5 6 7 8 9
-                s
-                f
-    p
+  -----
+  V   ^
+3 2 0 4
+      s
+      f
 
-0 1
-s   
-f      
+-----
+1 2 3
+s    
+f  
 '''
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = fast = head
+        slow=fast=head
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow is fast:
+            fast=fast.next.next
+            slow=slow.next
+            if fast is slow:
                 return True
         return False
