084b92c5efeba6e3da615de0e8da253f324e7186
Soji Omiwade <omiwade@gmail.com>
2024-03-07 08:04:51 -0800

    22m, via counting sort, whew not easy

--- relative-sort-array.py
+++ relative-sort-array.py
@@ -1,20 +1,27 @@
 '''
-arr1 3 2 1
-arr2 1 2
-count {11 21 31}
-ans [1 2 3]
-expected 1 2 3
+exp  8 8 9 1 1 2 
+get count of 8, then count of 9
+then for sorting the rest. run through count and update arr1
+
+arr2 8 9
+arr1 9 2 8 1 8 1
+arr1 8 8 9 1 1 2
+count 0 2 1 0 0 0 0 0 0 0
+idx  0 1 2 3 4 5
+                 ^
 '''
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        arr1.sort()
-        count=Counter(arr1)
-        arr2set=set(arr2)
-        ans=[]
-        for el in arr2:
-            for _ in range(count[el]):
-                ans.append(el)
+        MAX=1001
+        count=[0]*MAX
         for el in arr1:
-            if el not in arr2set:
-                ans.append(el)
-        return ans
+            count[el]+=1
+        idx=0
+        for el in arr2:
+            arr1[idx:idx+count[el]]=count[el]*[el]
+            idx+=count[el]
+            count[el]=0
+        for el in range(MAX):
+            arr1[idx:idx+count[el]]=count[el]*[el]
+            idx+=count[el]
+        return arr1
