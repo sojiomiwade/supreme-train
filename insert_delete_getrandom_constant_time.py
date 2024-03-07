04fce6be84e981fd4e521e7d178e9ca0bbb83378
Soji Omiwade <omiwade@gmail.com>
2024-03-07 11:11:08 -0800

    20m, lebcowit not bad at all

--- insert_delete_getrandom_constant_time.py
+++ insert_delete_getrandom_constant_time.py
@@ -1,46 +1,49 @@
+'''
+delete could just move the last element into the deleted slot
+    must modify maps going both ways
+random uses the rl to get element value
+so insert is now a simple task
+'''
 class RandomizedSet:
 
     def __init__(self):
-        self.ll,self.rll={},{}
+        self.vall,self.idxl={},{}
 
-    '''
-    ll, rll
-    insert: if not exists: add to ll, add reverse to rll
-    remove: put last in 3's place in both lookup. 7 takes on 3's idx
-    ie ll[7]=ll[3], del last in ll, del last in rll, rll[ll[7]]=7
-    1 3 2 4 7
-    0 1 2 3 4 <-- can do a random on these and reverse lookup back
-
-      v     lv
-    1 2 4 7
-    0 2 3 1 <-- can do a random on these and reverse lookup back
-            
-    0 1 2 3 <-- can do a random on these and reverse lookup back
-    1 7 2 4
-    ^     ^
-    '''
     def insert(self, val: int) -> bool:
-        ll,rll=self.ll,self.rll
-        if val in ll: return False
-        ll[val]=len(ll)
-        rll[len(rll)]=val
+        if val in self.idxl:
+            return False
+        count=len(self.vall)
+        self.vall[count]=val
+        self.idxl[val]=count
         return True
 
+    '''
+    2 8 4 5
+    0 1 2 3
+    2 8 4 5
+    0 1 2 3
+    idxl {51 --81--}
+    vall {15 --35--}
+    '''
     def remove(self, val: int) -> bool:
-        ll,rll=self.ll,self.rll
-        if val not in ll: return False
-        count=len(ll) #5
-        lastval=rll[count-1] #7
-        ll[lastval]=ll[val]
-        rll[ll[lastval]]=lastval
-        del ll[val]
-        del rll[count-1]
+        if val not in self.idxl:
+            return False
+        idx=self.idxl[val]               # 1
+        lastidx=len(self.vall)-1    # 3 
+        lastval=self.vall[lastidx]       # 5
+        self.idxl[lastval]=idx           #  
+        self.vall[idx]=lastval
+
+        # prune the lookups
+        del self.vall[lastidx]
+        del self.idxl[val]
         return True
 
     def getRandom(self) -> int:
-        ll,rll=self.ll,self.rll
-        count=len(ll)
-        return rll[random.randrange(count)]
+        count=len(self.vall)
+        idx=random.randrange(count)
+        return self.vall[idx]
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
