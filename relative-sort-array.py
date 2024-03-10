0303c20908a81475b1edabfbdaf026cb43515819
Soji Omiwade <omiwade@gmail.com>
2024-03-10 08:57:52 -0700

    recalling the approach

--- relative-sort-array.py
+++ relative-sort-array.py
@@ -1,27 +1,14 @@
 '''
-exp  8 8 9 1 1 2 
-get count of 8, then count of 9
-then for sorting the rest. run through count and update arr1
+arr1: 2 5 4 2 3 4, arr2 4 2
+ans: 4 4 2 2 5 3
+count:
+idx 0 1 2 3 4 5 ...
+cnt     2 1 2 1
 
-arr2 8 9
-arr1 9 2 8 1 8 1
-arr1 8 8 9 1 1 2
-count 0 2 1 0 0 0 0 0 0 0
-idx  0 1 2 3 4 5
-                 ^
+loop through arr2, and put each element in ans, and decrment count
+now loop through arr1, and for each occurfencd of that element in cout
+put it in ans
 '''
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        MAX=1001
-        count=[0]*MAX
-        for el in arr1:
-            count[el]+=1
-        idx=0
-        for el in arr2:
-            arr1[idx:idx+count[el]]=count[el]*[el]
-            idx+=count[el]
-            count[el]=0
-        for el in range(MAX):
-            arr1[idx:idx+count[el]]=count[el]*[el]
-            idx+=count[el]
-        return arr1
+        
