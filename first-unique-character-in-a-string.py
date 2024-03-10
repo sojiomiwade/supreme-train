44d1316de8dc2641491ce8b24cd57fe81223476d
Soji Omiwade <omiwade@gmail.com>
2024-03-10 09:51:26 -0700

    ~20m, not so easy when you use bitvec, lebcowit ok. likewse testing

--- first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string.py
@@ -1,16 +1,26 @@
 '''
-have to go through whole string no matter
 leetcode
-l1 e3 t1 c1 o1 d1
-l->(1,l)
-0,1,2,3
-first: counter
-then loop again and take the first one with counter 0
+use two integers:
+first: first one flags if first time seen
+again: 2nd flags oonly if first is seen
+finally loop through s and pick element that has first but not second
+aabb
+   ^
+mask   01
+first  11
+second 11
 '''
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        count=Counter(s)
-        for i,x in enumerate(s):
-            if count[x]==1:
+        first=second=0
+        for ch in s:
+            mask=1<<ord(ch)-ord('a')
+            if first&mask==0:
+                first|=mask
+            else:
+                second|=mask
+        for i,ch in enumerate(s):
+            mask=1<<ord(ch)-ord('a')
+            if first&mask!=0 and second&mask==0:
                 return i
         return -1
