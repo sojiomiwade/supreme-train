98b90c6ae8eb5f693eb48d16bab37a0409c95cfa
Soji Omiwade <omiwade@gmail.com>
2024-03-11 14:59:52 -0700

    21m, not elegant, but works

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,44 +1,38 @@
 '''
-       26
-a-z -> 97-123
-A-Z -> 65-91
-65+?=97->32
-abc
+come from the left and right
+valid left/right is alphanum
+a man, a plan, a canal: panama
+l
+                              r
+return false anytime ch of l and r are not the same
+once l and r cross, return true
+
+97 --> 
+
+a,a
+  l
+ r
 '''
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        def lower(ch: str) -> str:
-            ordch=ord(ch)
-            assert isalphanum(ch)
-            if ORD_HIGHER_A<=ordch<=ORD_HIGHER_Z:
-                return chr(ordch+SEP)
-            return ch
-
-        def isalphanum(ch: str) -> bool:
-            ordch=ord(ch)
-            if ord('0')<=ordch<=ord('9'):
-                return True
-            if ORD_HIGHER_A<=ordch<=ORD_HIGHER_Z or ORD_LOWER_A<=ordch<=ORD_LOWER_Z:
-                return True
-            return False
-
-        SEP=32
-        ORD_LOWER_A,ORD_LOWER_Z,ORD_HIGHER_A,ORD_HIGHER_Z=(
-            97,97+25,65,65+25
-        )
-        left,right=0,len(s)-1
-        while left<right:
-            while left<right and not isalphanum(s[left]):
+        n=len(s)
+        left,right=0,n-1
+        while left<=right:
+            while left<=right:
+                ch=ord(s[left])
+                if ord('0')<=ch<=ord('9') or ord('a')<=ch<=ord('z') or ord('A')<=ch<=ord('Z'):
+                  break
                 left+=1
-            while left<right and not isalphanum(s[right]):
+            while left<=right:
+                ch=ord(s[right])
+                if ord('0')<=ch<=ord('9') or ord('a')<=ch<=ord('z') or ord('A')<=ch<=ord('Z'):
+                  break
                 right-=1
-            if left>=right:
-                return True
-            sleft=lower(s[left])
-            sright=lower(s[right])
-            if sleft!=sright:
-                print(f'{(left,right)}:{(sleft,sright)}:{(s[left],s[right])}')
+            if left>right or s[left].lower()==s[right].lower():
+                left+=1
+                right-=1
+            else:
                 return False
-            left+=1
-            right-=1
         return True
+
+
