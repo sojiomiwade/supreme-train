0502509b8cdb6db8277b0b014f6ba04f6a4f16c7
Soji Omiwade <omiwade@gmail.com>
2024-03-11 10:24:56 -0700

    no nlgn sorting

--- pairs-with-specific-difference.py
+++ pairs-with-specific-difference.py
@@ -1,31 +1,22 @@
 '''
-first: consider all pairs x!=y: x-y=k. time,space:n**2,1
-better:
-k=1
-1 0 -> [1,0] 
-0 1 -> [1,0]
-x - y = k  or y - x = k
-x-k (add (x,y)) or x+k (add (y,x)) we already have
+x-y=k
+x, look for x-k in arr
+*also* look for y+k in arr
 
-ans should be [[1,0]]
-arr=[1 0] k=1
-     x
-have:{1 }
-ans:[[1,0]]
-
-{2}
-=>
+arr 2 1 3 0 
+k 1
+1 2
+0 1
+2 3
+-1 0
+ans [ 3,2 2,1 1,0]
 '''
 def find_pairs_with_given_difference(arr, k):
-  have={}
   ans=[]
-  for i,x in enumerate(arr):
-    if x-k in have:
-      ans.append([x,x-k])
-    if x+k in have:
-      ans.append([x+k,x])
-    have[x]=i
-  ans.sort(key=lambda tup: have[tup[1]])
+  xval={}
+  for x in arr:
+    xval[x-k]=x
+  for y in arr:
+    if y in xval:
+      ans.append([xval[y],y])
   return ans
-arr,k=[0, -1, -2, 2, 1],1
-print(find_pairs_with_given_difference(arr, k))
