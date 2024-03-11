a489c44c0639550d646f0fc40268dd97a5e97b61
Soji Omiwade <omiwade@gmail.com>
2024-03-11 09:45:00 -0700

    8m,lebcowit all the way

--- first-bad-version.py
+++ first-bad-version.py
@@ -1,18 +1,18 @@
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 '''
-bin search
-1 2   3 4 5 6 7
-f t   f t t t t
-ans=-1
-if not bad, move lo to mi+1
-else ans=mi, then move hi to mi-1
-return ans
-0 1 2
+
+1 2 3 4 5 6 7
+f f f f t t t
+must squeeze the range to zero and remember the last bad version
+if good version, lo gets mid + 1. otherwise hi gets mid -1
+1 2 3 4
+t t t t
+ans 1
 '''
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        ans=-1
+        ans=None
         lo,hi=1,n
         while lo<=hi:
             mi=lo+(hi-lo)//2
@@ -21,5 +21,6 @@ class Solution:
                 hi=mi-1
             else:
                 lo=mi+1
-        assert ans!=-1
+        assert ans is not None
         return ans
+        
