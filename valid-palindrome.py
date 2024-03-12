d7b0e890978d1027a2035450d43598256dc77ff0
Soji Omiwade <omiwade@gmail.com>
2024-03-12 07:53:06 -0700

    use isalnum and lower and less than

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,38 +1,19 @@
 '''
-come from the left and right
-valid left/right is alphanum
-a man, a plan, a canal: panama
+race a car
 l
-                              r
-return false anytime ch of l and r are not the same
-once l and r cross, return true
-
-97 --> 
-
-a,a
-  l
- r
+         r
 '''
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        n=len(s)
-        left,right=0,n-1
-        while left<=right:
-            while left<=right:
-                ch=ord(s[left])
-                if ord('0')<=ch<=ord('9') or ord('a')<=ch<=ord('z') or ord('A')<=ch<=ord('Z'):
-                  break
+        left,right=0,len(s)-1
+        while left<right:
+            if not s[left].isalnum():
                 left+=1
-            while left<=right:
-                ch=ord(s[right])
-                if ord('0')<=ch<=ord('9') or ord('a')<=ch<=ord('z') or ord('A')<=ch<=ord('Z'):
-                  break
-                right-=1
-            if left>right or s[left].lower()==s[right].lower():
-                left+=1
+            elif left<right and not s[right].isalnum():
                 right-=1
             else:
-                return False
+                if left<right and s[left].lower()!=s[right].lower():
+                    return False
+                left+=1
+                right-=1
         return True
-
-
