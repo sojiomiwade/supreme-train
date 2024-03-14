69ae38f804bed3879dc0c20dd55682458382c800
Soji Omiwade <omiwade@gmail.com>
2024-03-14 15:37:19 -0700

    12m where i bet on no e2e test but focused on unit tests

--- word-search.py
+++ word-search.py
@@ -1,36 +1,28 @@
 '''
-DFS from every cell
-T(n)=4*T(n-1) + 1
-=4*(4*T(n-2)+1)+1
-=4*(4*(4(T(n-3)+1))+1) + 1
-=4**2
-=4**n +   +4**3 ..4**2.4 + 1
-=4**n + 4**n
+there is a visited we need to backtrack out of
+time O(mn * 4**(mn))
 '''
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def exist(r: int, c: int, idx: int) -> bool:
-            if idx==len(word):
+        def exist(r: int, c: int, idx: int):
+            if idx==W:
                 return True
-            if (r,c) in visited:
-                return False
-            if not 0<=r<m or not 0<=c<n:
-                return False
-            if word[idx]!=board[r][c]:
+            if (r,c) in visited or not(0<=r<m and 0<=c<n) or word[idx]!=board[r][c]:
                 return False
             visited.add((r,c))
-            doesexist=(
-                exist(r+1,c,idx+1) or
-                exist(r-1,c,idx+1) or
-                exist(r,c+1,idx+1) or
-                exist(r,c-1,idx+1))
+            ans=False
+            idx+=1
+            if exist(r-1,c,idx) or exist(r+1,c,idx) or exist(r,c-1,idx) or exist(r,c+1,idx):
+                return True
             visited.remove((r,c))
-            return doesexist
+            return False
 
-        m,n=len(board),len(board[0])
         visited=set()
+        m,n=len(board),len(board[0])
+        W=len(word)
         for r in range(m):
             for c in range(n):
+                assert not visited
                 if exist(r,c,0):
                     return True
         return False
