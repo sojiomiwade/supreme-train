5f7fe2ed0474a82f355bbc1649d174badca61bac
Soji Omiwade <omiwade@gmail.com>
2024-03-14 16:45:43 -0700

    solution outline done, done with this till next cycle!

--- draw_lines_on_canvas.py
+++ draw_lines_on_canvas.py
@@ -1,11 +1,23 @@
 '''
 in a canvas, draw a line from a point x to a point y
+DON'T LOOK BELOW. IT'S GOT A SOLUTION OUTLINE
 
-x
- x
-   x
-    xxxxy
+
+
+
+deltax is 1 or -1 depending on if startx < or > finishx
+likewise deltay
+curx,cury=startx,starty
+arr[curx][cury]=.
+loop until we are at finish y and finishx as follows
+    if curx is already at finishx, do nothing to it
+    otherwise increment it with deltax
+    likewise cury
+    arr[curx][cury]=.
+return arr
 '''
+
+
 from typing import Tuple
 
 
