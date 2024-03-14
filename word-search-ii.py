492071bf7b8dadbdc8a5d2ebe1fb3a224345ac88
Soji Omiwade <omiwade@gmail.com>
2024-03-14 16:10:18 -0700

    27m, nice! unit tests vs e2e test

--- word-search-ii.py
+++ word-search-ii.py
@@ -1,49 +1,47 @@
 '''
-W=len(words)
-DFS: W(4**n)
-walk a trie (of words) along with each cell as a start
-T(n)=4**n
+complexity without a trie K*mn*4**(mn)
+complexity with a trie
+how?
+build a trie from words
+then at each mxn cell, 
+    follow it, until you can get to end of trie.
+    add that word if you can. 
+    try to get to the end of the trie
 '''
+import typing
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        def build_trie():
+        def buildtrie(words: List[str]) -> typing.Dict:
             root={}
             for word in words:
                 cur=root
-                for let in word:
-                    if let not in cur:
-                        cur[let]={}
-                    cur=cur[let]
-                cur['.']=word
+                for ch in word:
+                    if ch not in cur:
+                        cur[ch]={}
+                    cur=cur[ch]
+                cur[None]=word
             return root
-        '''
-        oabn
-        otae
-        ahkr
-        aflv
-        '''
-        def findWords(r,c,root):
-            if not 0<=r<m or not 0<=c<n:
-                return
-            if (r,c) in visited:
-                return
-            ch=board[r][c]
-            if ch not in root:
-                return
-            if '.' in root[ch]:
-                res.add(root[ch]['.'])
-            visited.add((r,c))
-            findWords(r+1,c,root[ch])
-            findWords(r-1,c,root[ch])
-            findWords(r,c+1,root[ch])
-            findWords(r,c-1,root[ch])
-            visited.remove((r,c))
-                    
-        m,n,W=len(board),len(board[0]),len(words)
-        words,res=set(words),set()
-        root=build_trie()
+
+        def findWords(r: int, c: int, root: typing.Dict):
+            if None in root:
+                ans.add(root[None])
+            if 0<=r<m and 0<=c<n and (r,c) not in visited:
+                brc=board[r][c]
+                if brc in root:
+                    visited.add((r,c))
+                    child=root[brc]
+                    findWords(r+1,c,child) 
+                    findWords(r-1,c,child)
+                    findWords(r,c+1,child)
+                    findWords(r,c-1,child)
+                    visited.remove((r,c))
+
         visited=set()
+        m,n=len(board),len(board[0])
+        trie=buildtrie(words)
+        ans=set()
         for r in range(m):
             for c in range(n):
-                findWords(r,c,root)
-        return list(res)
+                assert not visited
+                findWords(r,c,trie)
+        return list(ans)
