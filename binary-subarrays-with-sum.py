65aa1cba22f7570cad35523a92f294aeac06cbbe
Soji Omiwade <omiwade@gmail.com>
2024-03-14 15:18:34 -0700

    finally get the hashmap approach: increment the count if when we take goal away from prefix sum, the result is a key in counter. because then it tells us how many permutations of goal we have, which is count[result]

--- binary-subarrays-with-sum.py
+++ binary-subarrays-with-sum.py
@@ -1,11 +1,72 @@
+'''
+        count=Counter({0:1})
+        ans=0
+        prefix=0
+        for x in nums:
+            prefix+=x
+            ans+=count[prefix-goal]
+            count[prefix]+=1
+        return ans
+1 1 0 0 0 
+1 1 0 0 0 1 1
+        x
+p 3
+ans 2 <--- c[p-2]
+c {(01) (11) 22 31}
+
+1 + 2
+1 1 0 0 1
+        x
+1 0 1 0 1 goal 2
+        x
+
+1 1 0 0 1
+
+p 3
+ans 2 + c[1] <-- c[p-g]
+c {01 12 22}
+
+
+p
+
+  s
+
+0 0 0 0 0
+
+000000 | goal 0
+  i
+
+ans 3
+c {03}
+
+
+i
+res 
+c {0:1, 1:2}
+
+10101
+ 1
+-----
+psum 3
+res 2 + c[3-2]
+   i
+c {0:1,1:2,2:2,3:1}
+psum 2
+res 2
+2 0 0 2 1 2<--- 6
+    x
+p 4
+ans 2
+count {01 22 41 61}
+ans+=count[prefix-goal]
+'''
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        n=len(nums)
-        count=0
-        for i in range(n):
-            prefix=0
-            for j in range(i,n):
-                prefix+=nums[j]
-                if prefix==goal:
-                    count+=1
-        return count
+        count=Counter({0:1})
+        ans=0
+        prefix=0
+        for x in nums:
+            prefix+=x
+            ans+=count[prefix-goal]
+            count[prefix]+=1
+        return ans
