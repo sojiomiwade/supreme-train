43bd60109d7d7e09d599b836a19d0bd6c492ae76
Soji Omiwade <omiwade@gmail.com>
2024-03-15 10:57:58 -0700

    32m, shoulda tested! so many mistakes

--- house-robber-ii.py
+++ house-robber-ii.py
@@ -1,58 +1,49 @@
 '''
+1 | 2 3 2 | 5
+solve it like 5 is not there
+solve it like 1 is not there
+3+5=8
+
+1 2 3 2 5 <-- even odd length of array doesn't matter 
+           0
+        /     \
+       2       1
+     /   \    / \
+    x     y  3   2
+what is 'it'?
+rob
+rob(i)=nums[i]+rob(i+2) or rob(i+1)
+now can do this 
+
 2 3 2
-|      \
-2 3     3 2
+i
+rob(0 1)
+dp {}
+            0
+        /1+     \3
+       2         1 
+     /   \      / \0
+    4     3 (0)3   2
 
-9 2 3 8 x x
-ans=9
-rob(0,end=n-2)
-rob(1,end=n-1)
-rob(i)=max(nums[i]+rob(i+2),rob(i+1))
-    if > end, return 0
+rob(1 2)
+dp {}
 
-0 1 2
-2 3 9
-|      \
-2 3      3 9
-0
-|     \
-1      2
-|   \
-2    3
-| \  
-3  4
-max(max(2,3),  max(3,9))
 '''
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def rob(hidx: int, end: int) -> int:
-            if hidx>end:
+        def rob(idx: int, last: int):
+            if idx>last:
                 return 0
-            if (hidx,end) in dp:
-                return dp[hidx,end]
-            dp[hidx,end]=max(nums[hidx]+rob(hidx+2,end),rob(hidx+1,end))
-            return dp[hidx,end]
-        n=len(nums)
-        if n==1:
+            if idx in dp:
+                return dp[idx]
+            dp[idx]=max(nums[idx]+rob(idx+2,last),rob(idx+1,last))
+            return dp[idx]
+        
+        if len(nums)==1:
             return nums[0]
-        # dp={}
-        # return max(rob(0,end=n-2),rob(1,end=n-1))
-        '''
-              s   e
-              0 1 2  3     
-              9 2 3  8
-        0  0  9 9 12 .       
-           r2 r1 a     
-
-            1   2   3
-         0  0   .   .
-         r1 r2  a
-        '''
-        def rob(start: int, end: int) -> int:
-            r2=r1=0
-            ans=None
-            for i in range(start,1+end):
-                ans=max(r1,r2+nums[i])
-                r2,r1=r1,ans
-            return ans
-        return max(rob(0,n-2),rob(1,n-1))
+        dp={}
+        n=len(nums)
+        max1=rob(0,last=n-2)
+        dp={}
+        max2=rob(1,last=n-1)
+        return max(max1,max2)
