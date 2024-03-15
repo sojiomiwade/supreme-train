e0ff98a9d3181bd5a6dbbf283efef2c2ce0289d9
Soji Omiwade <omiwade@gmail.com>
2024-03-15 07:43:31 -0700

    11m, at least walkthrough / run some test, corner case test would have caught the run problem

--- find-peak-element.py
+++ find-peak-element.py
@@ -1,25 +1,24 @@
+'''
+1 2 1 3 5 6 4
+  p       p      
+use binary search with invariant that if we don't have a peak
+then we going where the peak is guarantees we will have an ans
+'''
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        '''
-        1,2,1,3,5,6,4
-        1 2 3 1
-            l h
-            m
-        if cur element isn't peak, then go binsearch in direction of any larger element
-        '''
         n=len(nums)
         lo,hi=0,n-1
-        while lo<=hi:
+        INTMIN=-2**31
+        while lo<hi:
             mi=lo+(hi-lo)//2
-            nums_mil=nums_mir=float('-inf')
-            if mi>0:
-                nums_mil=nums[mi-1]
-            if mi<n-1:
-                nums_mir=nums[mi+1]
-            if nums[mi]>max(nums_mil,nums_mir):
+            lval=nums[mi-1] if mi>0 else INTMIN
+            rval=nums[mi+1] if mi<n-1 else INTMIN
+            mval=nums[mi]
+            if mval>max(lval,rval):
                 return mi
-            if nums_mil>nums[mi]:
+            if lval>rval:
                 hi=mi-1
             else:
                 lo=mi+1
-        raise Exception('peak element should exist')
+        return lo
+        
