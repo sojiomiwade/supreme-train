5ffe7ba2e7c26a8ea2ef4077532b5073dbe615a7
Soji Omiwade <omiwade@gmail.com>
2024-03-18 15:12:35 -0700

    2 pass approach, first getting the lastseen lookup, then no need to cleanup

--- remove-zero-sum-consecutive-nodes-from-linked-list.py
+++ remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -19,28 +19,33 @@ psum 0 1  0 2
      .------>
             c  
 have {0:0, 1:1, }
+two pass solution
+first get the have map, then start the prefix sum again, and 
+if you see the prefix there, go ahead and point to its next. and set yourself to that node
+otherwise cur just get cur.next
 0  1 3 2 -3 -2  5  5 -5 1
+0    4    3  1    11  6 7
 0  1 4 6  3  1  6 11  6 7
-   ----------c-->
-   n          
-0  1 2 3. 4. 5. 7  8
-                      c
-lis  d 01 02
-psum 0  0  0 
-           c
-     ------>   
-have {0:d}
+0  .------------>.------->
+             c
+list  0 0 0 n
+      c-----> 
+have -1 x 0 
+    
 '''
 class Solution:
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy=ListNode(0,head)
         have={}
-        cur=dummy=ListNode(0,head)
-        psum=0
+        psum,cur=0,head
         while cur:
             psum+=cur.val
-            node=have.get(psum,cur)
-            while psum in have:
-                have.popitem()
-            have[psum]=node
-            node.next=cur=cur.next
+            have[psum]=cur
+            cur=cur.next
+        cur,psum=dummy,0
+        while cur:
+            psum+=cur.val
+            if psum in have:
+                cur.next=have[psum].next
+            cur=cur.next
         return dummy.next
