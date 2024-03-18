1482753dfac26ac4ba66996b253db7ca0e50862c
Soji Omiwade <omiwade@gmail.com>
2024-03-18 16:08:11 -0700

    18m, lebcowit ok

--- construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,48 +4,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+#preorder holds the roots
 '''
-pre : 3   9   20   15   7
-ino : 9   3   15   20   7
-take from pre and make a root
-call buildtree on array to left in ino, and array to right of ino
                3
             /     \
-           9        [15 20 7] pre -> 20 -> (20) 
-                   /   \
-                  [15] [7]
+           9      20
+                 /  \
+               15    7 
 
-preidx=0
-there's a ilo and ihi (for in order range)
+buildtree(lo, rootloc-1) 
+passing the tree bounds makes us know when to stop 
+    building down the left and move to right
+3 9 4
+inorder  [9 3 4]
+preorder [3 9 4 ]
+inol {90 31 42}
+               3
+            /     \
+pre [9 4]
+bt (0 2)
+            3
+            / \
+         (00) (11)
 
-test
-should have
-            3
-          /   \
-         9     1
-iil={9:0,3:1,1:2}
-pre 3 9 1
-ino 9 3 1
-preidx=2
-lo,mi,hi=0 0 2
-            3
-          /   \
-         9
 '''
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def buildtree(lo: int, hi: int) -> Optional[TreeNode]:
-            nonlocal preidx
+        def buildtree(lo, hi):
             if lo>hi:
                 return None
-            root=TreeNode(preorder[preidx])
-            preidx+=1
-            mi=iilookup[root.val]
-            root.left=buildtree(lo,mi-1)
-            root.right=buildtree(mi+1,hi)
+            root=TreeNode(pre.popleft())
+            irloc=inol[root.val]
+            root.left=buildtree(lo,irloc-1)
+            root.right=buildtree(irloc+1,hi)
             return root
-
-        preidx=0
-        iilookup={x:i for i,x in enumerate(inorder)}
-        lo,hi=0,len(inorder)-1
-        return buildtree(lo,hi)
+        inol={inoval:idx for idx,inoval in enumerate(inorder)}
+        pre=collections.deque(preorder)
+        root=buildtree(0,len(inol)-1)
+        assert not pre
+        return root
+    
