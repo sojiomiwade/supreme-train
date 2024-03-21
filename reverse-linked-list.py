233786b157bce5a2a050ea7bb8a3d00d6cf829fa
Soji Omiwade <omiwade@gmail.com>
2024-03-21 09:27:36 -0700

    8m, lebcowit all the way

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -3,30 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+1 2 3 4 5 n
+prev = null
+for node not null
+    store node's next in temp
+    node sets next to prev
+    prev=node
+    set node to temp
+return prev
+n<1<2<3 n
+        h
+      p
+'''
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        '''
-        a b c d e
-                p c t  
-        next point to cur, then call rev on cur.next and its next
-        stop when cur.next is null. then return cur
-        
-        prev,cur=None, starts at null
-        then loop on cur=head until cur is none.
-            cur
-            cur.next=prev
-            prev,cur
-        return prev
-
-        n<-a  n
-           p  c
-
-        n<-a<-b<-c
-                 p  c  t 
-        '''
-        prev,cur=None,head
-        while cur:
-            temp=cur.next
-            cur.next=prev
-            prev,cur=cur,temp
+        prev=None
+        while head:
+            nextnode=head.next
+            head.next=prev
+            prev=head
+            head=nextnode
         return prev
