8f534dbf971c1235b6780afe6f23376d27bbd4f8
Soji Omiwade <omiwade@gmail.com>
2024-03-21 09:39:29 -0700

    8m, lebcowit all the way, but thought visited would be needed, but it was not

--- battleships-in-a-board.py
+++ battleships-in-a-board.py
@@ -1,11 +1,16 @@
+'''
+idea look to left and up for  visited. if so cannot count this if it is an X
+X .
+X .
+count 1
+'''
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
-        m, n = len(board), len(board[0])
-        count = 0
-        for i in range(m):
-            for j in range(n):
-                if (board[i][j] == 'X' 
-                    and (i == 0 or board[i - 1][j] == '.')
-                    and (j == 0 or board[i][j - 1] == '.')):
-                    count += 1
+        count=0
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if (board[r][c]=='X' and 
+                    (r==0 or board[r-1][c]!='X') and 
+                    (c==0 or board[r][c-1]!='X')):
+                    count+=1
         return count
