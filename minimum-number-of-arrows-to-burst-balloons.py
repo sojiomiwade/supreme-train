6ce4fe509c6d620d690fa0a3a8132380c195739f
Soji Omiwade <omiwade@gmail.com>
2024-03-21 16:41:42 -0700

    working out how to solve this

--- minimum-number-of-arrows-to-burst-balloons.py
+++ minimum-number-of-arrows-to-burst-balloons.py
@@ -1,36 +1,22 @@
-'''
-0    123456
-1     2345678
-2          789012
-            8
-            8
-3             0123456
-loop on the array
-    eat until you can't anymore.
+sprev, eprev
+count 1
+sort by the ends
+for each point scur,ecur
+  if 
+  
+   ----
+     ----
 
-0 1 2 3 
-l
-r
-count 0
-[[1,2],[3,4],[5,6],[7,8]]
-     v
-    --
-    ------
-       -----
-          ----
-     ------------
-0 12
-1   34
-2     56
-3       78
-'''
-class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort(key=lambda x: x[1])
-        count=0
-        pos=float('-inf')
-        for s,e in points:
-            if pos<s:
-                pos=e
-                count+=1
-        return count
+        -------
+----------------
+             ----
+             ----
+             -------
+     |       |
+ 
+ -----
+   -------
+                          -------
+             ---
+                    ----------      
+       -------
