7d05df7eb850fae201c9d15bd048538877837b3f
Soji Omiwade <omiwade@gmail.com>
2024-03-25 08:51:58 -0700

    using the array to check itself if we have seen some other number

--- find-all-duplicates-in-an-array.py
+++ find-all-duplicates-in-an-array.py
@@ -1,26 +1,35 @@
 '''
-0 1 2 3 4 5 6 7 8
+0 1 2 3 4 5 6 7
 4,3,2,7,8,2,3,1
-1 2 3 4 . . 7 8 
 
-[3 2]
-try to put each element in the right place
-anything that can't go, put it in ans, then advance
+0 --> 4 --> 8
+1 --> 3 --> 7    6
+<------------         
+      <-----------
+s
+f   
 
-0 1 2
-1 1 2 --> [1]
-    ^
-ans []
-1 == 
+1 2 3 4 5 6 7 8
+
+6
+5 --> 2 
+      ><
+
+keep nexting like a linked list, and if you hit a cycle, add that 
+node, and move to next thing
+0   1   2 
+-1 -1   2
+-1 -1   2
+ans [1]
+num --> nums[num-1]
 '''
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        ans=set()
-        for i in range(len(nums)):
-            while nums[i]!=i+1: # 2 3
-                if nums[nums[i]-1]==nums[i]: # n[1] n[2]
-                    ans.add(nums[i])
-                    break
-                else:
-                    nums[nums[i]-1],nums[i]=nums[i],nums[nums[i]-1] #n[1],n[2]
-        return list(ans)
+        ans=[]
+        for x in nums:
+            x=abs(x)
+            if nums[x-1]<0:
+                ans.append(x)
+            else:
+                nums[x-1]*=-1
+        return ans
