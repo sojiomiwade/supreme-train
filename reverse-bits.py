22044b5ef57ad55d45d638cd85801ea83747b714
Soji Omiwade <omiwade@gmail.com>
2024-03-26 10:38:47 -0700

    study the examples closely to understand the problem intent

--- reverse-bits.py
+++ reverse-bits.py
@@ -1,3 +1,42 @@
+'''
+48
+000001001010
+010100
+
+1010 -x
+0101
+
+011
+110 --> 111...110
+
+so if the last bit is a 1 then pad the ans with 111...
+    000000|101100
+    111111|000000
+ans|=(-1<<32) when last bit is 1
+always shift, but add one if the mask is zero
+
+input n 011 --> 110 = -6
+n 000
+ans 110
+i 0..2 1
+ans_is_neg 1
+
+000110
+111000
+   
+111|101
+
+11101 --> 11 = -3
+11110
+
+11101
+
+'''
 class Solution:
     def reverseBits(self, n: int) -> int:
-        return int('0b'+''.join(reversed(bin(n)[2:].zfill(32))),base=0)
+        ans=0
+        for _ in range(32):
+            ans<<=1
+            ans+=(n&1)
+            n>>=1
+        return ans
