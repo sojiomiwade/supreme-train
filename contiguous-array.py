b0df70057cb4b2ef7c9005164cf5b672605af63b
Soji Omiwade <omiwade@gmail.com>
2024-03-27 15:04:33 -0700

    just reviewing!

--- contiguous-array.py
+++ contiguous-array.py
@@ -1,30 +1,35 @@
 '''
- 0011
-54345
- 1100
-56765 
+    -----------------
+0   1   2   3   4   5
+1   0   0   1   0   1
+1  -1  -1   1  -1   1
+1   0  -1   0  -1   0
 
-idx  -1 0 1
-nums    1 0
-count 0 1 0
+111|3|
+stream 1 1 0 0 1 1
+prefix 1 2 1 0 1 2
 
+3 - (-1+1) + 1
 
-idx     1  2  3  4  5  6  7  8  9
-nums    0  1  1  1  1  1  0  0  1
-count  -1  1  2  3  4  5  4  3  4   
-       
-count -1 
-table {00 -11 11 23 34 45 56}
-maxlen 0
+l {0:-1}
+idx,val:
+    idx - l[val]
+
+0  1  0
+-1 0 -1
+      p
+ans 2
+il {0:-1 -1:0 }
 '''
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        maxlen=count=0
-        seen={0:-1}
-        for i in range(len(nums)):
-            count+=1 if nums[i] else -1
-            if count in seen:
-                maxlen=max(maxlen,i-seen[count])
-            else:
-                seen[count]=i
-        return maxlen
+        il=dict({0:-1})
+        prefix=ans=0
+        for idx,val in enumerate(nums):
+            prefix+=1 if val==1 else -1 
+            il.setdefault(prefix,idx)
+            ans=max(ans,idx-il[prefix])
+        return ans
+        '''
+        make transformation, and use prefix sum to get lengths
+        '''
