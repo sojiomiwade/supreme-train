42a2729b598b493254d3b4a69bc13be67010bfb7
Soji Omiwade <omiwade@gmail.com>
2024-03-28 16:11:28 -0700

    5m: trick to count 1s in a number, and xor to do the 1 counting

--- hamming-distance.py
+++ hamming-distance.py
@@ -1,23 +1,17 @@
 '''
-0 0 0 1
-0 1 0 0
-0 1 0 1
-order matters, can't just get the next bit
-use power of 2 instead of mask to get the next one
-and do this 32 times
-cur bit of x is x & 1
-advance x is x>>1
-0
-1
-1
+xor both numbers
+now use mask to count number of 1s
+can apply n&=n-1 to remove a 1 and repeat until n is zero
+0100
+0011
+0000
+count 2
 '''
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        z=x^y
+        n=x^y
         count=0
-        while z:
+        while n:
             count+=1
-            z&=z-1
+            n&=n-1
         return count
-
-        
