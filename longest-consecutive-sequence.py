5ebc6582270d734235551b18b9d272d1c4db7a20
Soji Omiwade <omiwade@gmail.com>
2024-03-28 14:46:30 -0700

    ~20m,looks like I need sorting!

--- longest-consecutive-sequence.py
+++ longest-consecutive-sequence.py
@@ -1,34 +1,25 @@
+'''
+create a graph of neighbors for each x. well no need: just make a set
+of nums
+
+now can check if num +/- 1 in set (which implies it is in graph)
+now withh a dfs on each num, we can get our ans
+need visited set
+
+visited {100 4}
+4-3-2-1
+'''
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        '''
-        put every number in a set
-        now make graph for each num, if num-1 is there, then num and num-1 are neighbors
-        now find the largest component
-        1-2 -3-4
-        \ |
-         \-5
-        v v v v 
-        100
-        400
-        '''
-        def count_seq(num: int) -> int:
-            count=0
-            visited.add(num)
-            for nb in nbs[num]:
-                if nb not in visited:
-                    count+=count_seq(nb)
-            return 1 + count
-
-        nums=set(nums)
-        nbs=defaultdict(list)
+        def visit(val: int) -> int:
+            if val not in iset or val in visited:
+                return 0
+            visited.add(val)
+            return 1+max(visit(val-1),visit(val+1))
+            
+        ans=0
+        iset=set(nums)
         visited=set()
-        for num in nums:
-            if num-1 in nums:
-                nbs[num-1].append(num)
-                nbs[num].append(num-1)
-        
-        ans=float('-inf')
-        for num in nums:
-            if num not in visited:
-                ans=max(ans,count_seq(num))
-        return ans if nums else  0
+        for x in sorted(nums):
+            ans=max(ans,visit(x))
+        return ans
