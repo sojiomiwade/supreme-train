b158dde2c59972776af587415a50eb0f2d546f64
Soji Omiwade <omiwade@gmail.com>
2024-03-29 09:44:57 -0700

    practice

--- find-all-duplicates-in-an-array.py
+++ find-all-duplicates-in-an-array.py
@@ -1,27 +1,13 @@
 '''
-0 1 2 3 4 5 6 7
-4,3,2,7,8,2,3,1
+0   1  2  3 4 5  6  7
+-4 -3 -2 -7 8 2 -3 -1
+ans [2 3]
+for a number x, set x to abs(x) if arr[x-1] is negative already, add it to ans
+otherwise, make arr[x-1] negative
 
-0 --> 4 --> 8
-1 --> 3 --> 7    6
-<------------         
-      <-----------
-s
-f   
-
-1 2 3 4 5 6 7 8
-
-6
-5 --> 2 
-      ><
-
-keep nexting like a linked list, and if you hit a cycle, add that 
-node, and move to next thing
-0   1   2 
--1 -1   2
--1 -1   2
+0  1 2
+1 -1 2
 ans [1]
-num --> nums[num-1]
 '''
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
@@ -30,6 +16,6 @@ class Solution:
             x=abs(x)
             if nums[x-1]<0:
                 ans.append(x)
-            else:
-                nums[x-1]*=-1
+            nums[x-1]*=-1
         return ans
+
