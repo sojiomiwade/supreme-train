ed404c9f8b7a821fac752ca39be8f668505b522b
Soji Omiwade <omiwade@gmail.com>
2024-03-29 11:00:51 -0700

    do it myself

--- find-the-duplicate-number.py
+++ find-the-duplicate-number.py
@@ -1,22 +1,18 @@
 '''
-1 3 4 2 2
-1 4 3
-  ^
+3 1 3 4 2
 0 1 2 3 4
 
-iterate on each number
-    if number in its place, just move to next number
-    otherwise
-        find the right number for current idx
-            if the same value is already in its place for the other
-                numbers, return that specific number
-            otherwise: do the swap, repeatedly until we get curidx right 
+slow 0  
+fast 0
 '''
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            while nums[i]!=i+1: # 3 2
-                if nums[nums[i]-1]==nums[i]: # [2] == 3
-                    return nums[i]
-                nums[nums[i]-1],nums[i]=nums[i],nums[nums[i]-1] # =3,4
-        raise Exception('duplication shouldve been found')
+        slow=fast=0
+        while not slow or slow!=fast:
+            slow=nums[slow]
+            fast=nums[nums[fast]]
+        slow=0
+        while slow!=fast:
+            slow=nums[slow]
+            fast=nums[fast]
+        return slow
