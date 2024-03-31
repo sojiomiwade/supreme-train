188eb489e2c6e54f177066abba893a17b9a12210
Soji Omiwade <omiwade@gmail.com>
2024-03-30 17:44:34 -0700

    need more practice with enumerate

--- maximum-product-subarray.py
+++ maximum-product-subarray.py
@@ -1,28 +1,23 @@
-'''
-2 3 -2   5  2  3   4   4   0 1 5
-2 6 -12           -99 -99  0 1 5
-array a: go forward aggregating, if you see 0, set that to 0                            
-array b: go backward, same thing
-return the max in a+b
-
--6  6  3  0  1 
--1  2  3  0  1
--1 -2 -6  0  1       
-'''
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        agg=1
+        ans=float('-inf')
+        for i,x in enumerate(nums):
+            if i-1>=0 and nums[i-1]==0:
+                agg=x
+            else:
+                agg*=x
+            ans=max(ans,agg)
+
         n=len(nums)
-        forward=[0 for _ in range(n)]
-        backward=[0 for _ in range(n)]
-        forward[0],backward[-1]=nums[0],nums[-1]
-        for i in range(1,n):
-            if forward[i-1]:
-                forward[i]=forward[i-1]*nums[i]
+        # . 0 .
+        # 0 1 2
+        agg=1
+        for i,x in enumerate(reversed(nums)):
+            if i-1>=0 and nums[n-i]==0:
+                agg=x
             else:
-                forward[i]=nums[i]
-            j=n-i-1
-            if backward[j+1]:
-                backward[j]=backward[j+1]*nums[j]
-            else:
-                backward[j]=nums[j]
-        return max(itertools.chain(forward,backward))
+                agg*=x
+            ans=max(ans,agg)
+
+        return ans
