317a7eecf592f040500d7191ef16e218d2f18f83
Soji Omiwade <omiwade@gmail.com>
2024-04-01 08:31:31 -0700

    didnot test

--- kth-smallest-element-in-a-bst.py
+++ kth-smallest-element-in-a-bst.py
@@ -5,46 +5,17 @@
 #         self.left = left
 #         self.right = right
 '''
-st -- do we have something to go back to
-root -- i hit a null (doesn't mean we are done)
-so loop on: 
-- if htere's root, put it in stack and search further down the left
-- otherwise, pop the stack, process the element, and set root to right
+5
 '''
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        '''
-                    3
-                 /     \ 
-                1       4
-                 \
-                  2
-          count=1
-          root=3
-        '''
-        st=[];count=1
-        while root or st:
-            if root:
-                st.append(root)
-                root=root.left
-            else:
-                node=st.pop()
-                if count==k:
-                    return node.val
-                count+=1
-                root=node.right
-        assert 0, f'{k} too big'
-        # def inorder(root):
-        #     nonlocal count
-        #     if not root:
-        #         return None
-        #     ans=inorder(root.left)
-        #     if ans:
-        #         return ans
-        #     count+=1
-        #     if count==k:
-        #         return root
-        #     return inorder(root.right)
-            
-        # count=0
-        # return inorder(root).val
+        def kthsmallest(root, idx) -> Tuple[bool, int]:
+            if not root:
+                return (False,idx)
+            found,idx=kthsmallest(root.left,idx)
+            if found:
+                return (True, idx)
+            if idx==k:
+                return (True,root.val)
+            return kthsmallest(root.right,idx+1)
+        return kthsmallest(root,1)[1]
