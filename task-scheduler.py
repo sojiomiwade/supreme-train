1331ffd8de2d36e77f43d984f4d8832a4f9f34a3
Soji Omiwade <omiwade@gmail.com>
2024-04-02 07:41:19 -0700

    greedy approach via idle count

--- task-scheduler.py
+++ task-scheduler.py
@@ -1,40 +1,10 @@
-'''
-4a 4d 3b 2c, n = 5
-objective: calculate idles
-then ans is number of tasks + idles
-
-layout a map with a at ends
-a d . . . .  a d . . . . a d . . . . a d, n = 5
-all open idles: (n-xwidth+1)*(xcount-1) 5-2
-used-up-idles --> m-(xcount*xwidth)
-idles equals: maximum of zero and difference between open and used
-
-a b c d a b, [n 1] -->
-a b a c b d --> 6
-
-a b . a b
-m 6
-xcount 2
-xwidth 2
-openidles (1-2) neg
-used
-idles
-
-a a a b b b
-
-a b . . a b . . a b , [n 3]
-count {a3 b3}
-xcount 3
-xwidth 2
-openidles (3-2)*(2)
-'''
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        m=len(tasks)
+        L=len(tasks)
         count=Counter(tasks)
-        xcount=max(count.values())
-        xwidth=sum(1 for x in count.values() if x==xcount)
-        openidles=(n-xwidth+1)*(xcount-1)
-        used=m-(xcount*xwidth)
-        idles=max(0,openidles-used)
-        return m+idles
+        mf=max(count.values())
+        mfc=sum(1 for x in count.values() if x==mf)
+        T=L-mfc
+        A=(mf-1)*(n+1)
+        I=A-T
+        return L+max(0,I)
