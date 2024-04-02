f71d7ee71369cdc5c3ec23a088181d74a02b0d63
Soji Omiwade <omiwade@gmail.com>
2024-04-02 11:49:04 -0700

    26m, via heap

--- network-delay-time.py
+++ network-delay-time.py
@@ -1,48 +1,38 @@
 '''
+need shortest path to all nodes
+when len(dist) is n, stop
+can stop when heap is empty. no heap will not be empty, 
+just track len(dist)
+track mindist
 
 
-shortest path from node k to all nodes, return the maximum
-can use BFS (queue) or heap:
-queue starts with (0,2)
-11 13 
-{2:0 1:1}
+can use BFS or heap
 
-(9,4) -- 4 got added by say 2
-but later (2,4) gets added by 3, and damages are fixed
-cost: 
-14 .5 .5
-10 1 1 1 1 
-cost is just all the edges considered: O(V + E lg E)
-spath[k]=
+for heap, if we do dist[v], it will never change again
 
-(2,3) (4,x)
-   -  3 - - - 
- /
-1-x----------2
+1st put (0,2)
+on pop, add 11 and 13
+pop 13 and add 24,
+then pop 11
+
+heap [24 94]
+nc,n 13
+dist {20 11 31 42}
 '''
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        '''
-        heap:
-        heap need not be exhausted? if so, then can use the len(ans)?
-        no cause we will never reach e, so let's exhaust heap. just 
-        deny something popped if it's too big.
-        time,space: O(E lg E), O(E)
-                a --- b
-                |  9  |     e
-                c --- d
-        '''
-        arr=[(0,k)]
-        f=defaultdict(dict)
-        ans=defaultdict(lambda :float('inf'))
+        cost=defaultdict(dict)
         for u,v,w in times:
-            f[u][v]=w
-        while arr:
-            cu,u=heapq.heappop(arr)
-            if u in ans:
-                assert ans[u]<=cu
-            else:
-                ans[u]=cu
-                for v,cuv in f[u].items():
-                    heapq.heappush(arr,(cuv+cu,v))
-        return -1 if len(ans)!=n else max(ans.values())
+            cost[u][v]=w
+
+        dist={}
+        heap=[(0,k)]
+        while heap:
+            nodecost,node=heapq.heappop(heap)
+            if node in dist:
+                continue
+            dist[node]=nodecost
+            for nb,nb_node_cost in cost[node].items():
+                heapq.heappush(heap,(nodecost+nb_node_cost,nb))
+        # print(list(dist.items()))
+        return -1 if len(dist)<n else max(dist.values())
