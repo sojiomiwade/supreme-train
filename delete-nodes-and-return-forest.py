b111c566f03d52f8bae78855c76776c3e5a299f3
Soji Omiwade <omiwade@gmail.com>
2024-04-06 16:32:53 -0700

    19m, this problem can be straightforward solved via post order traversal

--- delete-nodes-and-return-forest.py
+++ delete-nodes-and-return-forest.py
@@ -5,29 +5,32 @@
 #         self.left = left
 #         self.right = right
 '''
-exp forests=[a,f,g]
-forests=[a,]
-key here is: 
-1. if a node is a root, and not to be deleted, it can
-    be added to forest. 
-2. if a node is to be deleted return None
-               a
-            /     \
-           b      Xc
-         /   \   /   \
-        d    Xe f     g 
+best perhaps seen as postorder (children first then me)
+if children are processed, ok, so now i can set them to null
+parent sets children to null, as needed, 
+children can add themselves to forest
+1-2-3-4
 '''
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-        def delNodes(root, is_a_root):
-            if not root:
-                return None
-            delete_me=root.val in todel
-            root.left=delNodes(root.left,delete_me)
-            root.right=delNodes(root.right,delete_me)
-            if is_a_root and not delete_me:
-                forest.append(root)
-            return None if delete_me else root
-        forest,todel=[],set(to_delete)
-        delNodes(root,True)
-        return forest
+        def delNodes(root,forest):
+            if root:
+                delNodes(root.left,forest)
+                delNodes(root.right,forest)
+
+                if root.val in to_delete_set:
+                    if root.left and root.left.val not in to_delete:
+                        forest.append(root.left)
+                    if root.right and root.right.val not in to_delete:
+                        forest.append(root.right)
+
+                if root.left and root.left.val in to_delete:
+                    root.left=None
+                if root.right and root.right.val in to_delete:
+                    root.right=None
+            return forest
+        to_delete_set=set(to_delete)
+        forest=[]
+        if root and root.val not in to_delete_set:
+            forest.append(root)
+        return delNodes(root,forest) 
