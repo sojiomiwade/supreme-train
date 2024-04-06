5fabb48ffe4f54e00014c8894f77102458ab3f95
Soji Omiwade <omiwade@gmail.com>
2024-04-06 15:06:50 -0700

    faster it seems this way with one lookup

--- isomorphic-strings.py
+++ isomorphic-strings.py
@@ -8,17 +8,22 @@ sigh. can do it by mapping
 p to t
 if cur is not mapped, then map it
 otherwise see if this is the right mapping. return false if it isn't
+
+if e not there add ea
+else verify map[e] is a
+badc
+baba
+lookup {bb aa db}
 '''
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        sbrother,tbrother={},{}
+        lookup={}
         for chs,cht in zip(s,t):
-            if chs in tbrother and cht in sbrother:
-                if tbrother[chs]!=cht:
+            if chs in lookup:
+                if lookup[chs]!=cht:
                     return False
-                assert sbrother[cht]==chs
-            elif chs not in tbrother and cht not in sbrother:
-                tbrother[chs],sbrother[cht]=cht,chs
             else:
-                return False
+                if cht in lookup.values():
+                    return False
+                lookup[chs]=cht
         return True
