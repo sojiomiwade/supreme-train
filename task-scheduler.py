6e0681d656c7bc543b07ec64876a37a33f658b57
Soji Omiwade <omiwade@gmail.com>
2024-04-06 22:34:49 -0700

    heap based solution based on LC editorial

--- task-scheduler.py
+++ task-scheduler.py
@@ -1,10 +1,36 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        L=len(tasks)
-        count=Counter(tasks)
-        mf=max(count.values())
-        mfc=sum(1 for x in count.values() if x==mf)
-        T=L-mfc
-        A=(mf-1)*(n+1)
-        I=A-T
-        return L+max(0,I)
+        pq = [-f for f in Counter(tasks).values()]
+        heapq.heapify(pq)
+
+        time = 0
+        # Process tasks until the heap is empty
+        '''
+        a b c d . .
+        a b c . . .
+        a b . . . .
+        a b
+        pq [-4 -4 -2 -1] | n 5 | ans 20
+        time,cycle 0,5
+        store [3]
+        task_count 1
+        current_freq 4
+        '''
+        while pq:
+            store = []
+            task_count = 0
+            
+            # Execute tasks in each cycle
+            for _ in range(n+1):
+                if not pq: 
+                    break
+                current_freq = -heapq.heappop(pq)
+                if current_freq > 1:
+                    store.append(-(current_freq - 1))
+                task_count += 1
+            
+            for x in store:
+                heapq.heappush(pq, x)
+
+            time += task_count if not pq else n + 1
+        return time
