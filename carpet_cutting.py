2752277e8a319dccf4b083828ce309d0d839b30b
Soji Omiwade <omiwade@gmail.com>
2024-04-09 11:01:20 -0700

    ~15m, some testing mistakes testing too fast

--- carpet_cutting.py
+++ carpet_cutting.py
@@ -1,47 +1,35 @@
-'''given dimensions a, and b, return number of squares that can be cut out. smallest square dimension is 1
+'''
+carpet cutting
+given dimensions a, and b, return number of squares that can be cut out. smallest square dimension is 1
 
+             a
+    --------------------
+   |                    |
+   |                    | b
+   |                    |
+    --------------------
+a and b : 100 and 3
+ans = 100 // 7
+then set a to b
+b to a % old_b 
+repeat this until b equals 0
 
- ______________a________
-|                      |
-|                      |b
-|______________________|
+50 // 25 --> 2
+50 % 25 --> 0
 
-a x b
-a//b square carpets
-then set a,b to b,a%b
-repeat until a,b=1,1
-
-10
-3
-10,3 -> 3
-3,1 -> 3
-1,0-> 
-break when b==0
-
-1,1 -> 1
-1,0 -> break
 '''
 def count_squares(a: int, b: int) -> int:
-    assert a and b
-    if b>a:
-        a,b=b,a
-    ans=0
-    while b:
-        ans+=a//b
+    count=0
+    while b!=0:
+        count+=a//b
         a,b=b,a%b
-    return ans
+    return count
 
-a,b=10,3
-print(count_squares(a,b)) # 6
-
-a,b=9,3
-print(count_squares(a,b)) # 3
-
-a,b=12,5
-'''
-12,5 -> 2
-5,2 -> 2
-2,1 -> 2
-1,0 -> break
-'''
-print(count_squares(a,b)) # 6
+print(count_squares(50,25)) # 2
+print(count_squares(50,1)) # 50
+print(count_squares(50,23)) # 11
+# count 2 5
+# 23 50 2
+# 4 23  5
+# 3 4   1
+# 1 3   3
