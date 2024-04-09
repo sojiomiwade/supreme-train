076bf3e9feabbb33344aa09ac2711452cb675fa6
Soji Omiwade <omiwade@gmail.com>
2024-04-08 21:40:37 -0700

    9m, was not systematic,

--- maximum-subarray.py
+++ maximum-subarray.py
@@ -1,27 +1,18 @@
 '''
-       L    R
-2 5 -8 4 5 -3 4 -1
-2 7 -1 4 9 7 11 10 
-       l
-                r
-find L,R with maximum sum
-don't forget max sum subarray problem: if newsum with me is greater
-than me, take me otherwise take new sum. at this point reset l
-update L,R always
+        --------
+-2 1 -3 4 -1 2 1 -5 4
+-2 1 -2 4  3 5 6  1 5   
+        L      R
 
 '''
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        INF=float('inf')
-        l=L=R=None
-        LRsum=-INF
-        n,cursum=len(nums),0
-        for r in range(n):
-            if cursum<=0:
-                l=r
-                cursum=0
-            cursum+=nums[r]
-            if cursum>LRsum:
-                L,R=l,r
-                LRsum=cursum
-        return sum(nums[i] for i in range(L,R+1))
+        ans=nums[0]
+        prev=float('-inf')
+        for x in nums:
+            if prev<0:
+                prev=x
+            else:
+                prev+=x
+            ans=max(ans,prev)
+        return ans
