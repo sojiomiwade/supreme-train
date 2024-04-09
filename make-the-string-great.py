89d500f8eee421cce77c1aa541cffdd681cac1fe
Soji Omiwade <omiwade@gmail.com>
2024-04-09 08:08:58 -0700

    using two pointers, thanks to votrubac

--- make-the-string-great.py
+++ make-the-string-great.py
@@ -1,19 +1,17 @@
 '''
-abBAcC
-just use a stack. done!
-
-top e
-st [l ]
+leEetcode
+ p
+   i
+p goes back, so the rest of the string is shifted onto it
 '''
 class Solution:
-    def makeGood(self, s: str) -> str:
-        st=[]
-        for ch in s:
-            top=st[-1] if st else ''
-            if top.lower()==ch.lower() and top!=ch:
-                st.pop()
+    def makeGood(self, s: str, p: int = 0) -> str:
+        t=[' ']+list(s)
+        for i in range(len(t)):
+            if abs(ord(t[i])-ord(t[p-1]))==32:
+                p-=1
             else:
-                st.append(ch)
-        return ''.join(st)
-                
-        
+                t[p]=t[i]
+                p+=1
+
+        return ''.join(t[1:p])
