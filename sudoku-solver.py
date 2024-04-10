b1328d7084975ce426cf31b766bdd0d68aa91072
Soji Omiwade <omiwade@gmail.com>
2024-04-10 10:29:14 -0700

    24m, missed that valid should have been an inner inner function

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,45 +1,47 @@
 '''
-7 2 5 4 3 5(no)
-principles:
-    - when we backtrack, we must return the dot back!
-    - we go forward only because the current was valid
-operatioin: 
-    if dot
-        for all possible numbers, put the number, and call forward
-        if we get back a true, return that
-        otherwise keep trying numbers
-    revert the dot
+. . .
+. 5 .
+2 . .
+filled slots are pass-through
+others iterate through all numbers seeking validity
+  before putting a char, ensure it isn't violating
+  at the end of a row, be sure to move to next row
+if row,col is 10,1, then can return True
+row = 6, col 7
+6//3 = 2
+2 * 3 = 6
+
+7//3 = 2
+2*3 = 6
 '''
-class Solution:
-    def solveSudoku(self, board: List[List[str]]) -> None:
-        def valid(num:str, r: int, c: int) -> bool:
-            for k in range(9):
-                if board[r][k]==num or board[k][c]==num:
-                    return False
-            # 5->3, (6,7)->6 ... 5//3=1, 1*3 = 3
-            # r (or c) --> (r//3)*3
-            sr,sc=(r//3)*3,(c//3)*3
-            for i in range(sr,sr+3):
-                for j in range(sc,sc+3):
-                    if board[i][j]==num:
-                        return False
-            return True
-
-        def solveit(r: int, c: int) -> bool:
-            if c==9:
-                r,c=r+1,0
-            if r==9:
-                return True
-            if board[r][c]!='.':
-                return solveit(r,c+1)
-            for numi in range(1,10):
-                num=str(numi)
-                if valid(num,r,c):
-                    board[r][c]=num
-                    if solveit(r,c+1):
-                        return True
-            board[r][c]='.'
+def sudoku_solve(board):    
+  def solve(row, col):
+    def valid():
+      for k in range(9):
+        if ch in (board[row][k],board[k][col]):
+          return False
+      srow,scol=(row//3)*3,(col//3)*3
+      for r in range(srow,3+srow):
+        for c in range(scol,3+scol):
+          if ch==board[r][c]:
             return False
-
-
-        solveit(0,0)
+      return True
+    
+    if col==9:
+      row,col=row+1,0
+    if (row,col)==(9,0):
+      return True
+    
+    if board[row][col]!='.':
+      return solve(row,col+1)
+    
+    for i in range(1,10):
+      ch=str(i)
+      if valid():
+        board[row][col]=ch
+        if solve(row,col+1):
+          return True
+        board[row][col]='.'
+    return False
+    
+  return solve(0,0)
