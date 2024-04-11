5d210d05016beab9d97fa74ad9ed1c8cce2ab620
Soji Omiwade <omiwade@gmail.com>
2024-04-11 11:20:51 -0700

    had to sit back and think for this one for some time. came up with dp solution myself, after building on a brute-force

--- longest-increasing-subsequence.py
+++ longest-increasing-subsequence.py
@@ -1,66 +1,28 @@
 '''
-10 9  2  5  3  7  25 18
-10 25
-9 25
-2 5 7 25
-2 3 7 25
-2 3 7 25
+9,8,2,5,3,7,101,18
+0 0 0 1 2 
 
-2, 6, 8, 3, 4, 5, 1 0 2 6 6
--        -  -  -        -
-2 3 4 5 6
-1 2
-0
+0 1 9 8 2 3 4 5
 
-another maybe simpler way is have two arrays
-one owrite, write: owrite is the one to tell the length
-write takes on new elements, 
-if owrite will add a new elem, write it in both.
-otherwise only update owrite
-no ... this won't work: should have thought it through before writing it down
+0 1 0 1 7
+init all to one to start with
 
+outer loop goes from last to first: i: n-1 .. 0
+    inner loop leverages dp if it can: j > i
+    inner loop find j such that a[j]>a[i]
+    then set dp[i] to 1+dp[j] 
 
-the true sequence is under the overrides
-use a parent map p[0]=1, p[1]=(2,0), p[2,0]=None
-the elements can just be tuples (the number and its ans idx)
-for each i, element
-    while parent(element)
-        element=parent[element]
-    ans[i]=element
-
-now in original problem
-    (val,len(ans)-1)
-    on append -- parent[element]=None
-    on overwrite -- parent[element]=prevelement
-
-return [key for key in ans]
-
-bisect left, so existing number is overwritten
-if ans == n, then append
-2 3
-max=3
-
-another maybe simpler way is have two arrays
-one owrite, write: owrite is the one to tell the length
-write takes on new elements, 
-if owrite will add a new elem, write it in both.
-otherwise only update owrite
-3 6 8 .. 2
-2 .. 2
-nums 7 7 
-ans 
-
+nums 0 1 0 3 2 3
+dp   1 1 3 1 2 1
+         i
+             j
 '''
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        ans=[]
-        write=[]
-        for num in nums:
-            if not ans or ans[-1]<num:
-                ans.append(num)
-                write.append(num)
-            else:
-                idx=bisect.bisect_left(ans,num)
-                ans[idx]=num
-        return len(ans)
-
+        n=len(nums)
+        dp=[1]*n
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n):
+                if nums[j]>nums[i]:
+                    dp[i]=max(dp[i],1+dp[j])
+        return max(dp)
