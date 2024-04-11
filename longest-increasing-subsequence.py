73a545ce924dcee50dc44c76a75597e3a080c7b8
Soji Omiwade <omiwade@gmail.com>
2024-04-11 11:22:31 -0700

    add bisection approach

--- longest-increasing-subsequence.py
+++ longest-increasing-subsequence.py
@@ -1,10 +1,22 @@
 '''
-9,8,2,5,3,7,101,18
-0 0 0 1 2 
+
+
+5 8 9 3
+
+5
+3 8 9
 
 0 1 9 8 2 3 4 5
+0 1 2 
 
-0 1 0 1 7
+approach: delete first then comeback
+
+0 5 8 9 3 4 5 11
+----------------
+  5 8 9 
+0 3 4 5 11
+
+max 4
 init all to one to start with
 
 outer loop goes from last to first: i: n-1 .. 0
@@ -16,13 +28,29 @@ nums 0 1 0 3 2 3
 dp   1 1 3 1 2 1
          i
              j
+
+
+nums 0 1 0 3 2 3
+     1 2 . . . . 
+
+9,8,2,5,3,7,101,18
+
+9
+8 5   101 
+2 3 7 18
+if bisect-left is the length of the array, then append
+
+nusm [3 0 4]
+ans [0 1]
 '''
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[1]*n
-        for i in range(n-1,-1,-1):
-            for j in range(i+1,n):
-                if nums[j]>nums[i]:
-                    dp[i]=max(dp[i],1+dp[j])
-        return max(dp)
+        ans=[]
+        for x in nums:
+            pos=bisect.bisect_left(ans,x)
+            if pos==len(ans):
+                ans.append(x)
+            else:
+                ans[pos]=x
+        return len(ans)
