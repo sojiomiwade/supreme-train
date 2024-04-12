06e2822edabfdf41a6183e89e48501b558a684be
Soji Omiwade <omiwade@gmail.com>
2024-04-12 08:45:42 -0700

    32m, extra 10-12 mins because i did not do a basic test. splendid!

--- decode-ways.py
+++ decode-ways.py
@@ -1,35 +1,26 @@
 '''
-11106
-            .
-         /     \
-1 1106
-    1  106
-        10 6
-             .+1  0
-nw(s) is nw(s[1:]) if you can remove a valid char plus (only 0 not ok)
-         nw(s[2:]) if you can remove 2 valid chars 23 ok but 27 not
-                    10 ok but 03 not
-12
-ab and l
-1 -- 2 -- [1]
 
+
+1 | k
+current is sum of
+    prior if cur is between 1 and 9
+    two priors ago if s[cur-1:cur+1] is bet 10 & 26
+    1     1      1    0       6
+0 1 10   11     21   02       2 
+
+
+1110
+aaj
+kj
+12
+i
 '''
 class Solution:
     def numDecodings(self, s: str) -> int:
-        def nw(s: str, idx: int) -> int:
-            if idx>=n:
-                assert idx==n
-                return 1
-            if idx in dp:
-                return dp[idx]
-            count=0
-            if 1<=int(s[idx])<=9:
-                count=nw(s,idx+1)
-            if idx+1<n and 10<=int(s[idx:idx+2])<=26:
-                count+=nw(s,idx+2)
-            dp[idx]=count
-            return count
-
-        dp={}
         n=len(s)
-        return nw(s,0)
+        dp=[0,1]+[0 for _ in range(n)]
+        for i in range(n):
+            dp[i+2]= int(1<= int(s[i])<=9)*dp[i-1+2]
+            # print(f'foo-{i}-{s[i-1:i+1]}-')
+            dp[i+2]+=int(10<=int(s[max(0,i-1):i+1])<=26)*dp[i-2+2]
+        return dp[-1]
