fee1532d0e9bb2afc09e3681d3fdf2d74768cd03
Soji Omiwade <omiwade@gmail.com>
2024-04-13 23:25:39 -0400

    6m lebcowit all the way

--- two-sum.py
+++ two-sum.py
@@ -1,14 +1,20 @@
+'''
+2 7 11 15 | target 9 | exp [0 1]
+0 1  2  3
+idxlookup {20 7}
+9-7 in idxlookup? yes. then lookitup and return i and il[target-x]
+i goes with x
+
+arr 2 7 4 | 9
+idx 0 1 2 
+il {20 }
+[01]
+'''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        '''
-        need location here so we can return result
-        2 11 7  15
-        2:0, 11:1, 7
-        '''
-        loclookup={}
+        il={}
         for i,x in enumerate(nums):
-            if target-x in loclookup:
-                return [i,loclookup[target-x]]
-            loclookup[x]=i
-        raise ValueError('bad input')
+            if target-x in il:
+                return [il[target-x],i]
+            il[x]=i
         
