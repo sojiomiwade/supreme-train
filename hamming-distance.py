ce5fad48e518a98148631cc8243de3d23833c1e3
Soji Omiwade <omiwade@gmail.com>
2024-04-15 16:57:07 -0400

    ~5m, use n&=n-1 to get rid of 1s

--- hamming-distance.py
+++ hamming-distance.py
@@ -1,17 +1,11 @@
-'''
-xor both numbers
-now use mask to count number of 1s
-can apply n&=n-1 to remove a 1 and repeat until n is zero
-0100
-0011
-0000
-count 2
-'''
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        n=x^y
+        xor=x^y
         count=0
-        while n:
+        while xor:
             count+=1
-            n&=n-1
+            xor&=xor-1
         return count
+
+            # 10100
+            # 10011
