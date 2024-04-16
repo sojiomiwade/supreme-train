81817438245a800fb50f3a543c17c683bf257269
Soji Omiwade <omiwade@gmail.com>
2024-04-16 00:57:31 -0400

    DFS approach

--- course-schedule.py
+++ course-schedule.py
@@ -1,27 +1,23 @@
-'''
-use the indegree of each node, quadratic!
-if at any point you cannot find a node with degree 0, return false
-after n repeats, can just return true.
+from typing import List
 
-setup the graph. b is a prereq of a => b --> a
-use that to get the indegree
-'''
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        def canfinish(course: int) -> bool:
+            aux.add(course)
+            for prereq in nbs[course]:
+                if prereq not in vis:
+                    if prereq in aux or not canfinish(prereq):
+                        return False
+            aux.remove(course)
+            vis.add(course)
+            return True
+
         nbs=[[] for _ in range(numCourses)]
-        indegree=Counter()
-        for node,nb in prerequisites:
-            nbs[node].append(nb)
-        for course in range(numCourses):
-            for nb in nbs[course]:
-                indegree[nb]+=1
-        for _ in range(numCourses):
-            for i in range(numCourses):
-                if indegree[i]==0:
-                    indegree[i]=-1
-                    for nb in nbs[i]:
-                        indegree[nb]-=1
-                    break
-            else:
+        for course,prereq in prerequisites:
+            nbs[course].append(prereq)
+        vis,aux=set(),set()
+        for i in range(numCourses):
+            if not canfinish(i):
                 return False
         return True
