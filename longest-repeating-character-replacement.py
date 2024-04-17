fe4894e87180bda87a37e75c8212fb3fde87a0fb
Soji Omiwade <omiwade@gmail.com>
2024-04-17 01:57:08 -0400

    22m, lebcowit ok actually

--- longest-repeating-character-replacement.py
+++ longest-repeating-character-replacement.py
@@ -1,36 +1,48 @@
 '''
-0 1 2 3 4 5 6
-a a b a b b a, k=2
-        r
-l 
-w rest maxf
+abab
 
-highest-freq maxf
-width - maxf cannot be more than k. when this happens, time to 
-slide the window (move l, since r moves already). 
-if width - maxf , don't move l
-at the end the ans is (n-1)-l+1 = n-l
-now maxf cost is m (number of unique chars)=>
-time is O(m*n) 
+0123456
+aababba
+l  r
+add mf to rem=k to get r-l+1
 
-k=1
-A B A B
-      r
-  l 
-freq  A2 B2
-maxf 2
+aabqra
+--kk
+as long as rem<=k, we are good
+and rem = r-l+1 - mf
 
+ABAXYZ
+-------
+l     
+   k
+     r
+r can increase as long as r-l+1-mf<=k
+slide l with r if there is a violation
+ 0-----7
+
+ 10-6<=4
+ 10-1<=4
+ 10-8<=4
+ no need to use max of count.values, 
+ 
+ a b a b | k 1 | exp 4
+   l
+       r
+count {a2 b1}
+mf 2 
+2>2
 '''
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        freq=Counter()
-        l=0
-        maxf=float('-inf')
-        for r in range(len(s)):
-            freq[s[r]]+=1
-            maxf=max(maxf,freq[s[r]])
-            if (r-l+1)-maxf>k:
-                freq[s[l]]-=1
-                l+=1
-                maxf=max(val for val in freq.values())
-        return len(s)-l
+        count=Counter()
+        left,n=0,len(s)
+        mf=-1
+        for right in range(n):
+            count[s[right]]+=1
+            mf=max(mf,count[s[right]])
+            if right-left+1-mf>k:
+                count[s[left]]-=1
+                left+=1
+        return n-left
+
+            
