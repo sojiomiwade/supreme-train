be3cf13329e38c2baaded0958719c810b9b07888
Soji Omiwade <omiwade@gmail.com>
2024-04-17 15:56:02 -0400

    took a really long time, but this way, we dont need a while!!

--- find-all-anagrams-in-a-string.py
+++ find-all-anagrams-in-a-string.py
@@ -1,50 +1,48 @@
 '''
-yabccdex
-l    r
-one service updates frequency of item
+p abcc
+a b x t a c b c f g h i
+l
+r
+a b x c . . .
+  l
+    r
+when you hit the cur, then register the l,r window
+if there's a violation increment l
+what's a violation? something in there beyond what we need?
+need {a1 b2 c2 d0 e0 f0}
 
-if increasing frequency puts item at its bar, increment size
-if decreasing frequency puts item at its bar, decrement size
-if size==SIZE, append idx to result
-a3 b4 c2
-
-and if so 
-a1b1c1
-n,m=len(s),len(p)
-complexity is then O(n)
-bab
-
---+
+iterate entire string with a right index:
+    process s[r] into count
+    if we met a need, increment cur
+        when cur gets to length of p, register left
+    else if we surpass:
+        we must have surpassed a need! so move l (and decrement cur as needed)!!!
+        a b c q a t
+          l
+                r
+b a a
+  l
+    r
+NEED {a2 b1}
+count {b0 a2}
+at,over 0,0
 '''
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        n,m=len(s),len(p)
-        bar=Counter(p)
-        cur=Counter()
-        size=0
-        res=[]
-        '''
-        012
-        baa
-          i
-        cur,bar,p={a2},{a2},aa
-        size=1
-        res []
-        '''
-        for i,x in enumerate(s):
-            if x in bar:
-                cur[x]+=1
-                if cur[x]==bar[x]:
-                    size+=1
-                elif cur[x]==bar[x]+1:
-                    size-=1
-            if i-m>=0 and s[i-m] in bar:
-                y=s[i-m]
-                cur[y]-=1
-                if cur[y]==bar[y]-1:
-                    size-=1
-                elif cur[y]==bar[y]:
-                    size+=1
-            if size==len(bar):
-                res.append(i-m+1)
-        return res
+        left,m,n=0,len(p),len(s)
+        at=over=0; NEED=Counter(p); ans=[]
+        count=Counter()
+        for right in range(n):
+            count[s[right]]+=1
+            at+=count[s[right]]==NEED[s[right]]
+            over+=count[s[right]]>NEED[s[right]]
+            if over>0:
+                at-=count[s[left]]==NEED[s[left]]
+                over-=count[s[left]]>NEED[s[left]]
+                count[s[left]]-=1
+                left+=1
+            if at==len(NEED):
+                ans.append(left)
+        return ans
+            
+        
