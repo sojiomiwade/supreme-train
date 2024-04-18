c6f0ae694f9baa858debbd0538e137941d27d82b
Soji Omiwade <omiwade@gmail.com>
2024-04-18 00:43:03 -0400

    7m, think cases not thorough enough

--- monotonic-array.py
+++ monotonic-array.py
@@ -1,29 +1,20 @@
 '''
-1 2 2 3
-6 5 4 4 
-1 2 2 3 6  5 4 4 
-0 1 1 1 1 -1
+1 2 2 3 3 4 2
+  l e l e l g
 
-if val is not set, 
-    set val to curdiff
-else (val is set)
-    if curdiff is 0, pass 
-    else: 
-        if curdiff is not val, return false
-        otherwise pass
 1 3 2
-    i
-val,curdiff=2,-1
+  l g
+
+1 1 1
 '''
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        val=0
+        signs=set()
         for i in range(1,len(nums)):
-            curdiff=nums[i]-nums[i-1]
-            if not val:
-                val=curdiff
-            elif curdiff!=0 and curdiff*val<0:
-                return False
-        return Trueaggregate into two flags mono_inc and mono_dec; 1st checks we are always monotonically increasing, and similar for second. and just check that either flag is set
-
-
+            if nums[i-1]==nums[i]:
+                pass
+            elif nums[i-1]<nums[i]:
+                signs.add('l')
+            else:
+                signs.add('g')
+        return len(signs)<=1
