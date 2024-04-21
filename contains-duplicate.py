c792d0daa06672b19b9607dec3ed9a0f172c8736
Soji Omiwade <omiwade@gmail.com>
2024-04-20 23:51:40 -0700

    ~3m, lebcowit ok

--- contains-duplicate.py
+++ contains-duplicate.py
@@ -1,16 +1,4 @@
-'''
-could use a set, and if we ever see something we've seen, 
-    then return true
-    at the end return false. 
-    time,space=O(n),O(n) <--may not read all elements (in time and space)
-
-101
-'''
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        count=set()
-        for x in nums:
-            if x in count:
-                return True
-            count.add(x)
-        return Falsecan also use sorting
+        count=Counter(nums)
+        return any(x>1 for x in count.values())
