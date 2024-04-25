8c43abb4ad6a32b3882ffdc54eb61fbe6497077c
Soji Omiwade <omiwade@gmail.com>
2024-04-25 13:06:05 -0700

    lebcowit ok

--- cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops.py
@@ -1,38 +1,32 @@
 '''
-with bfs, can head to dst, but prune at k
-
-so a node abstraction is (l,c,id)
-0 -- 1 -- 2
-k=1 stop => 1+k leaps
-src starts at 0 leaps
-
-time: k(v+e)
-space: queue can have k(v+e)
-when processing a node, (l,c,id)
-    if i am dst, return l
-    if leaps l for me is already L, then discard me
-    for all nbs, if i can't make the cost to nb cheaper, then do not add nb
-    keep a step on each node, so that we can prune when hit k
-
-f {0{1:100 2:500} 1{2:100} 2{}}
-q [(0 0 0)]
-L 1
-cost {0:0, 2:inf}
-l,cv,v (0 0 0)
+now let's use heap
+cost is the picker (cv lv v)
+invariant is if we heap pop, then the node there has its ans as cv,
+    so if dst can return immediately
+before adding neighbors, check lv is not yet L
+can allow cost changes, only if lv is less than leaps[v]
+so can initialize leaps[v] to zero for all nodes
+that's it
+. --- .
+f {0:{1:100, 2:500}, 1:{2:100}, 2:{}}
+leaps {00 12 22}
+heap [ (500 1 2)]
+cv,lv,v (100 1 1)
 '''
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        q=collections.deque([(0,0,src)])
         f,L=defaultdict(dict),1+k
-        cost=defaultdict(lambda :float('inf'))
+        # cost=defaultdict(lambda :float('inf'))
+        leaps=defaultdict(lambda :L)
         for u,v,price in flights:
             f[u][v]=price
-        while q:
-            l,cv,v=q.popleft()
-            if cv>=cost[v]:
-                continue
-            cost[v]=cv
-            if l<L:
+        heap=[(0,0,src)]
+        while heap:
+            cv,lv,v=heapq.heappop(heap)
+            if v==dst:
+                return cv
+            if lv<leaps[v]:
+                leaps[v]=lv
                 for w,cvw in f[v].items():
-                    q.append((1+l,cv+cvw,w))
-        return -1 if cost[dst]==float('inf') else cost[dst]
+                    heapq.heappush(heap,(cv+cvw,1+lv,w))
+        return -1
