e50dcf6f33151a41f57a152e2a1e403da58c93cc
Soji Omiwade <omiwade@gmail.com>
2024-04-27 19:28:08 -0700

    almost there, this is a hard problem

--- inversion-count.py
+++ inversion-count.py
@@ -1,114 +1,46 @@
 '''
-mergesort
-0 2 4 |  3 1
-0 2 4 | 1 3
+1 9 6 5 | 4 8 7 3
+1 9 | 5 6     |    4 8 | 3 7
 
-0 2 
-  t 
-b
-1 3
-a [ 0 1 2 3 ]
-    i 
+1 5 6 9 | 3 4 7 8
+1 3 4 5 6 
+  * -- when we take from b, we must add m-i
+      * -- otherwise we don't add anything
+56 1
+37 1
 
-1 9 | 6 4 5
-1 9 | 6 4 | 5
-       1
-7 8 9
-t   mi
-1 2 
-0 invcount
-
+0 1 2 3 4 5 6 7
+1 9 6 5 4 8 7 3
+0 6 3 2 1 2 1 0 = 15
 '''
 from typing import List
 
-def mergesort(a: List[int]) -> int:
-    '''
-    algorithm: invcount increments by rem of top if 
-        taking from bot and topval is a number
-        8 2 | 1 9
-    '''
-    def merge(lo: int, mi: int, hi: int) -> None:
-        nonlocal invcount
-        b[lo:hi+1]=a[lo:hi+1]
-        #top : lo .. mi  | bot: mi+1 .. hi
-        idx=lo
-        top,bot=lo,mi+1
-        while top<=mi or bot<=hi:
-            topval=botval=INF
-            if top<=mi:
-                topval=b[top]
-            if bot<=hi:
-                botval=b[bot]
-            if topval<botval:
-                top+=1
-            else:
-                if top!=mi+1:
-                    invcount+=mi-top+1
-                bot+=1
-            val=min(topval,botval)
-            assert type(val) is int and type(val) is int
-            a[idx]=val
-            idx+=1
 
+def inversion_count(arr: List[int]) -> int:
     def mergesort(lo: int, hi: int) -> None:
-        if lo<hi:
+        if lo<=hi:
             mi=lo+(hi-lo)//2
             mergesort(lo,mi)
             mergesort(mi+1,hi)
-            merge(lo,mi,hi)
+            merge(lo,hi)
+
+    def merge(lo: int, hi: int):
+        nonlocal count
+        mi=lo+(hi-lo)//2
+        i,j=lo,mi+1
+        #lo...mi | mi+1 .. hi
+        while i<=mi or j<=hi:
+            bval=INF if j>hi else 
+        arr[lo:hi+1]=b[lo:hi+1]
 
     INF=float('inf')
-    n=len(a)
-    b=[0]*n
-    invcount=0
+    count,n=0,len(arr)
+    b=[0 for _ in range(n)]
     mergesort(0,n-1)
-    return invcount
+    return count
+    
+    
 
-from random import randrange as rr, shuffle
-a=[i for i in range(10)]
-b=a[:]
-assert mergesort(a) == 0
-assert a==b
-
-a=[i for i in range(10)]
-b=a[:]
-shuffle(a)
-mergesort(a)
-assert a==b
-
-a=[2,3,0,1]
-invcount=mergesort(a)
-assert a==[i for i in range(4)]
-print(invcount)
-
-a=[1, 9, 6, 4, 5]
-b=a[:]
-invcount=mergesort(a)
-assert a==sorted(b)
-print(invcount)
-
-'''
-inversion count
-top 0 | bot 1
-
-2 3 | 0 1
-
-2 3 | 0 1
-0
-4 count
-
-
-01 
-02 1
-03 1
-12 1
-13 1
-23 
-
-1, 9, 6, 4, 5
-   3. 2.     
-1
-9 3
-6 2
-5 
-'''
+print(inversion_count(arr)) # 15
+print(inversion_count([i for i in range(10)])) # 0
+print(inversion_count([i for i in range(10)])) # 45
