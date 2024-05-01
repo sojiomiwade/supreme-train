dd2100533450cda2d9d21510a6cc6ba8fe8e1200
Soji Omiwade <omiwade@gmail.com>
2024-05-01 07:39:51 -0700

    23m, lebcowit missed the walkthrough on each function

--- convert-sorted-list-to-binary-search-tree.py
+++ convert-sorted-list-to-binary-search-tree.py
@@ -10,35 +10,59 @@
 #         self.left = left
 #         self.right = right
 '''
-1 2 3 4 5
+abcdn
   s
     f
-return slow
-mid find the mid (not prev)
-make it root.
-call convert to (head,mid) and (mid.next,tail) <--tail is exclusive
-return root
-
-1 t
+landing on null ok, but can't go past it
+complexity n lg n
+r(head, none)
+get the middle make it root
+set root.left to recurse(head to me), set root.right to r(me.next to right)
+l,r = -10 0
+m=-3
+r(-10,-3)
+r(0,0)
+if the left is right, no range and can return
+range for -3 is ()
+1 3
+  f
+s
+since the tail is not inclusive, then if f senses the end, s must not move
+can't go beyond the terminal!
+so if next or next.next is the terminal, then end
+1 2 3
+  s
+    f
+               0
+            /     \
+           -10     5
+             \      \
+             -3      9
+0 1 2 n
 s
 f
+mid 1
 
-h t
-h,h -- t,t
+     1
+   /   \
+  0
 '''
 class Solution:
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        def findmid(head,tail):
-            slow=fast=head
-            while fast.next is not tail and fast.next.next is not tail:
+        def findmid(start, term):
+            slow=fast=start
+            while fast is not term and fast.next is not term:
                 fast=fast.next.next
                 slow=slow.next
             return slow
 
-        def convert(head,tail):
-            if head is tail:
+        def list2bst(head, term):
+            if head is term:
                 return None
-            mid=findmid(head,tail)
-            return TreeNode(mid.val,convert(head,mid),convert(mid.next,tail))
+            mid=findmid(head,term)
+            root=TreeNode(mid.val)
+            root.left=list2bst(head,mid)
+            root.right=list2bst(mid.next,term)
+            return root
 
-        return convert(head,None)
+        return list2bst(head,None)
