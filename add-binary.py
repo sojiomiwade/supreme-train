b50325ff895c5f8751a6c0fd2c8c84598b25f93d
Soji Omiwade <omiwade@gmail.com>
2024-05-03 12:59:21 -0700

    use built-in utility function int with base

--- add-binary.py
+++ add-binary.py
@@ -1,31 +1,8 @@
 '''
-1010
-1011
+101
+1
 
-0101
-110
-
-
-tval,bval,cval
-come from the back, and always get a val!
 '''
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        cval=0
-        m,n=len(a),len(b)
-        maxmn=max(m,n)
-        ans=[]
-        for i in range(maxmn):
-            aval=0
-            if m-1-i>=0:
-                aval=int(a[m-1-i])
-            bval=0
-            if n-1-i>=0:
-                bval=int(b[n-1-i])
-            valsum=cval+aval+bval
-            ans.append(str(valsum%2))
-            cval=valsum//2
-            
-        finalans='1' if cval else '' 
-        finalans+=''.join(reversed(ans))
-        return finalans
+        return bin(int(a,base=2)+int(b,base=2))[2:]
