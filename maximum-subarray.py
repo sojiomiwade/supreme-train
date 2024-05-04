01d2a6da4013600c19db9960ace26d1720317f2b
Soji Omiwade <omiwade@gmail.com>
2024-05-04 15:21:05 -0700

    lebcowit quite good, where i opted to skip test given simplicity of solution

--- maximum-subarray.py
+++ maximum-subarray.py
@@ -1,18 +1,17 @@
 '''
-        --------
--2 1 -3 4 -1 2 1 -5 4
--2 1 -2 4  3 5 6  1 5   
-        L      R
+nums -2 1 -3 4 -1 2 1 -5 4
+dp   -2 1 -2 4  3 5 6  1 5 
+return max of dp
+can skip dp array, using a prev val instead
+init maxsum to -inf, and update it to max(maxsum,rsum)
+where rsum for that iteration is max(0,rsum)+nums[i]
 
+5 4 
 '''
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        ans=nums[0]
-        prev=float('-inf')
-        for x in nums:
-            if prev<0:
-                prev=x
-            else:
-                prev+=x
-            ans=max(ans,prev)
-        return ans
+        rsum=maxsum=float('-inf')
+        for num in nums:
+            rsum=max(0,rsum)+num
+            maxsum=max(maxsum,rsum)
+        return maxsum
