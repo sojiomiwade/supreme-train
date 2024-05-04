7001e82f4f8b2e8738fe648509c4ddd551e3eb3d
Soji Omiwade <omiwade@gmail.com>
2024-05-04 15:48:51 -0700

    nlgn solution without a headache to myself!

--- maximum-subarray.py
+++ maximum-subarray.py
@@ -1,17 +1,35 @@
 '''
-nums -2 1 -3 4 -1 2 1 -5 4
-dp   -2 1 -2 4  3 5 6  1 5 
-return max of dp
-can skip dp array, using a prev val instead
-init maxsum to -inf, and update it to max(maxsum,rsum)
-where rsum for that iteration is max(0,rsum)+nums[i]
+-2 1 -3 4 | -1 2 1 -5 4 
+going from the middle in any direction, there is a greatest sum you can get
+that is the allleft (or allright)
+10
+5 5
+3 3 3 3
+1 1 1 1 
+n + n + ... + n = nlgn
+go
+msa=max of (left half, right half, whole thing)
 
-5 4 
+-2 | -1
+
 '''
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        rsum=maxsum=float('-inf')
-        for num in nums:
-            rsum=max(0,rsum)+num
-            maxsum=max(maxsum,rsum)
-        return maxsum
+        def msa(lo: int, hi: int) -> int:
+            if lo>hi:
+                return 0
+            if lo==hi:
+                return nums[lo]
+            mi=lo+(hi-lo)//2
+            left,right=msa(lo,mi),msa(mi+1,hi)
+            cur,thisleft=0,-INF
+            for i in range(mi,lo-1,-1):
+                cur+=nums[i]
+                thisleft=max(thisleft,cur)
+            cur,thisright=0,-INF
+            for i in range(mi+1,hi+1):
+                cur+=nums[i]
+                thisright=max(thisright,cur)
+            return max(left,right,thisleft+thisright)
+        INF=float('inf')
+        return msa(0,len(nums)-1)
