00277a3ae496e1a155c13cb416497ca9f3385721
Soji Omiwade <omiwade@gmail.com>
2024-05-08 13:19:27 -0700

    specify approach where we can just use DFS and return a true internally, when it gets found

--- find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph.py
@@ -1,24 +1,5 @@
 '''
-n=5
-0 1 ... 4
-bidirectional
+can use DFS after building graph from G
+if we DFS the entire graph without setting found to true, then return false
+otherwise return true. 
 '''
-class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        def validpath(v: int):
-            if v==destination:
-                return True
-            
-            visited[v]=True
-            for w in nbs[v]:
-                if not visited[w] and validpath(w):
-                    return True
-            return False
-
-        nbs=defaultdict(list)
-        for u,v in edges:
-            nbs[u].append(v)
-            nbs[v].append(u)
-
-        visited=[False]*n
-        return validpath(source)
