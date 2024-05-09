91ab5eb9a3b85638c12345e1cf0eddc79171ba38
Soji Omiwade <omiwade@gmail.com>
2024-05-09 16:46:45 -0700

    not a straight forward problem. you solve a couple problems to arrive at the answer

--- sort-characters-by-frequency.py
+++ sort-characters-by-frequency.py
@@ -1,19 +1,29 @@
 '''
-can just call sort on the -ve of the frequency 
-t r e e 
+tree122223
+{e:2,t:1,r:1,2:4,3:1,1:1}
+{4:[2,],2:[e],1:[t,r,]}
+now go through the lookup, going down by frequency.
+and put freq*each char into an ans list
+know the max
+ans: []
+loop down the frequencies: 4 3 2 1 0
 
-e 2, r 1, t 1
-1 [r,t]
-2 [e]
-
-first get lcount.
-then use that to get chars 
-time: O(n); space also O(n)
+tree
+count {t1 r1 e2}
+maxf 2
+chars {1:[t,r] 2:[e]}
+freq 2 [2 .. 1]
+ans [eetr]
 '''
 class Solution:
     def frequencySort(self, s: str) -> str:
-        lcount=collections.Counter(s)
-        # chars=collections.defaultdict(list)
-        # for count,let in lcount.items():
-        #     chars[count].append(let)
-        return ''.join(sorted(s,key=lambda ch: (-lcount[ch],ord(ch))))
+        count=Counter(s)
+        chars=defaultdict(list)
+        maxf=max(count.values())
+        for ch,chcount in count.items():
+            chars[chcount].append(ch)
+        ans=[]
+        for freq in range(maxf,0,-1):
+            for ch in chars[freq]:
+                ans.append(ch*freq)
+        return ''.join(ans)
