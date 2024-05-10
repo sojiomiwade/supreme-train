a24c3c96fda0b51a4c73796e0cc137db7c3c47b6
Soji Omiwade <omiwade@gmail.com>
2024-05-10 12:05:30 -0700

    le bad, implemented max-stack instead!

--- min-stack.py
+++ min-stack.py
@@ -1,18 +1,39 @@
--2 0 -3
-(-2 -2) (0 -2) (0 -3)
+'''
+st 2 2 2 1 5
+aux 2 2 2 1
 
-g p t g : -3 n 0 -2
-on push: if stack empty, can just push (el,el)
-    otherwise, push (el,min(el,stack[-1]))
-on pop: remove top of stack 
-top: return top of stack's first arg
-getMin : return top of stack's 2nd arg
-time comp of all ops is O(1)
+pop(x): if top of aux is x, then pop from aux as well
+push(x): 
+    push x onto st
+    if x is smaller than aux, no action on aux. 
+    otherwise, push x on aux
+'''
+class MinStack:
 
-2nd way: use 2nd stack to track only the mins
--2 0 -3
--2 -3
-key, 
-push: push onto minstack only if no minst or cur less than minst[-1]
-    otherwise dont
-pop: pop from minst if st[-1] equals minst[-1]
+    def __init__(self):
+        self.st,self.aux=[],[]
+
+    def push(self, val: int) -> None:
+        self.st.append(val)
+        if self.aux and val>self.aux[-1]:
+            return
+        self.aux.append(val)
+
+    def pop(self) -> None:
+        val=self.st.pop()
+        if self.aux[-1]==val:
+            self.aux.pop()
+
+    def top(self) -> int:
+        return self.st[-1]
+
+    def getMin(self) -> int:
+        return self.aux[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
