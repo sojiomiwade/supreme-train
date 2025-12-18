c6b86c3eef28b0a0d1eaa41433d6b0b861b00ef1
Soji Omiwade <omiwade@gmail.com>
2025-12-18 09:34:43 -0800

    lebcowit seemed good

--- roman-to-integer.py
+++ roman-to-integer.py
@@ -1,42 +1,30 @@
-/***
-iii 3
-lviii 50 
-if no i, no deductions
-i iv ix
-xl xc
-cd cm
-ixc require looking forward
-
-1st pass
-just add everything
-
-2nd pass
-iv/ix -- remove 2; 
-xl/xc -- remove 20
-cd/cm -- remove 200
-
+/*
 liv
-***/
+^^
+if i beats i+1, then add i. otherwise take i away
+at end, add the val of last
+*/
 func romanToInt(s string) int {
-   s = strings.ToLower(s)
-   ans := 0
-   for i,c := range s {
-    switch c {
-        case 'i': ans += 1
-        case 'v': ans += 5
-        case 'x': ans += 10
-        case 'l': ans += 50
-        case 'c': ans += 100
-        case 'd': ans += 500
-        case 'm': ans += 1000
+    val := map[rune]int{
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
     }
-    if i!=len(s)-1 {
-        switch s[i:i+2] {
-            case "iv","ix" : ans -= 2
-            case "xl","xc": ans -= 20
-            case "cd","cm": ans -= 200 
+    // liv --> 
+    // ans -- 50 - 1 + 5 = 54 
+    // i 2
+    ans := 0
+    for i := 0; i < len(s) - 1; i++ {
+        if val[rune(s[i])] < val[rune(s[i+1])] {
+            ans -= val[rune(s[i])] 
+        } else {
+            ans += val[rune(s[i])] 
         }
     }
-   }
-   return ans
+    return ans + val[rune(s[len(s)-1])]
+
 }
