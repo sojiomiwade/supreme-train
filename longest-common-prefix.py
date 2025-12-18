6e101b4f32a16cba6d9830ce72cf0c20752834cb
Soji Omiwade <omiwade@gmail.com>
2025-12-18 09:50:34 -0800

    didn't walkthrough multiple times, and each time, the solution was wrong

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -1,40 +1,23 @@
 /*
-flower
-flow
-flight
+dog, racecar, car, da
+fish fid fia
+sort the strings and consider only the 1st and last: append to ans until no more match
 
---
-""
---
-loop j forever
-    loop i on 1..m
-        if j is outside of bound of any string OR doesn't match strs[i][j] and strs[i-1][j]
-            return strs[0][0..j]
-
-return strs[0]
-
-lebcowit
-complexity: mn -- m number of strings, n is length of longest string
-
-fl
-f
-ans --> f
-i,j: 21
-st,s: fl, f
-
-
----
-""
----
+f, l: ab ac
+ab de
 */
 func longestCommonPrefix(strs []string) string {
-    for j := 0; j<len(strs[0]); j++ {
-        for i := 1; i < len(strs); i ++ {
-            st,s:=strs[i-1],strs[i]
-            if j>=len(st) || j>=len(s) || st[j]!=s[j] {
-                return s[:j]
-            }
+    sort.Strings(strs)
+    n := len(strs)
+    f, l := strs[0], strs[n - 1]
+    if len(l) < len(f) {
+        f, l = l, f
+    }
+    i:=0
+    for i = 0; i < len(f); i++ {
+        if f[i] != l[i] {
+            break
         }
     }
-    return strs[0]
+    return f[:i] // could panic too
 }
