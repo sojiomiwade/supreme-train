269bacb57ff1d201a0c7ec156d0d84f3d4517f47
Soji Omiwade <omiwade@gmail.com>
2025-12-30 09:22:59 -0800

    12m: lebowit ok, rushed on e

--- remove-duplicates-from-sorted-list.go
+++ remove-duplicates-from-sorted-list.go
@@ -5,11 +5,24 @@
  *     Next *ListNode
  * }
  */
+// d 1 -1 1 2 3 n
+//   p-------
+//           c
+// so p moves only when they are not the same
+// c always moves
+// d 1 1 2
+// d 1   2
+//   --- p
+//         c
 func deleteDuplicates(head *ListNode) *ListNode {
-    cur := head
-    for cur != nil {
-        for cur.Next != nil && cur.Next.Val == cur.Val { cur.Next = cur.Next.Next } 
-        cur = cur.Next
+    d := &ListNode {Val: -101, Next: head}
+    p := d
+    for c := head; c != nil; c = c.Next {
+        if p.Val == c.Val { 
+            p.Next = c.Next
+        } else {
+            p = c
+        }
     }
-    return head
+    return d.Next
 }
