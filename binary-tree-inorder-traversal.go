9d3fab04940e475c913190ff87f2d04e3cc6fa78
Soji Omiwade <omiwade@gmail.com>
2025-12-31 14:07:16 -0800

    w in lebowit did not catch exposed variable must be capital; array of pointer declaration needs work too

--- binary-tree-inorder-traversal.go
+++ binary-tree-inorder-traversal.go
@@ -6,18 +6,39 @@
  *     Right *TreeNode
  * }
  */
-// inoleft, print (node), ino right
-// a = [1 3 2]
+
+// 1 
+//  \
+//   2
+//  /
+// 3
+// ans = [1 3 2]
+// need st for saving r, to process left first. 
+// as we loop, st tells us if there a saved. 
+// r nil means no left, so pop for saved. if cant pop either we are done
+// if r defined, push it, and set r to r.left
+// otherwise, pop and print, then set r to the right 
+// 1 
+//   2
+// expected ans = [1 2]
+// ans = [1]
+// s = [2]
+// root = 2
+// slen = 1
 func inorderTraversal(root *TreeNode) []int {
-    var ino func (root *TreeNode)
+    s := []*TreeNode{}
     a := []int{}
-    ino = func (root *TreeNode) {
+    for len(s) > 0 || root != nil {
         if root != nil {
-            ino(root.Left)
+            s = append(s, root)
+            root = root.Left
+        } else {
+            slen := len(s)
+            root = s[slen - 1]
+            s = s[:slen-1]
             a = append(a, root.Val)
-            ino(root.Right)
+            root = root.Right
         }
     }
-    ino(root)
     return a
 }
