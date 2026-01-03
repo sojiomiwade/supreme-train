1a2065bd457f60c2d3e8eee1a4510551cde20f2d
Soji Omiwade <omiwade@gmail.com>
2026-01-03 12:16:39 -0800

    oops, did it in Go instead of python

--- longest-common-prefix.go
+++ longest-common-prefix.go
@@ -1,23 +1,27 @@
-/*
-dog, racecar, car, da
-fish fid fia
-sort the strings and consider only the 1st and last: append to ans until no more match
-
-f, l: ab ac
-ab de
-*/
+// # flight
+// # flighhhh <-- don't matter
+// # flow
+// # flower
+// # sh <-- wrench
+// # a simple way: sort the list, and only consider the 1st and last elems
+// # O(nm lg n)
+// # in consider: move forward, and break when not equal, return ans
+// fl
+//  i
+// flk
+// ans 2
 func longestCommonPrefix(strs []string) string {
-    sort.Strings(strs)
-    n := len(strs)
-    f, l := strs[0], strs[n - 1]
-    if len(l) < len(f) {
-        f, l = l, f
-    }
-    i:=0
-    for i = 0; i < len(f); i++ {
-        if f[i] != l[i] {
-            break
+        n := len(strs)
+        ans := 0
+        sort.Strings(strs)
+        a, b := strs[0], strs[n - 1]
+        for i := range len(a) {
+            if a[i] == b[i] { 
+                ans++ 
+            } else {
+                break
+            }
         }
-    }
-    return f[:i] // could panic too
+        return a[:ans]    
 }
+
