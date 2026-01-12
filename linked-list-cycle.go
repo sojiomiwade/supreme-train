d25acc816af5dce15612da5b8aeb43470ac8a514
Soji Omiwade <omiwade@gmail.com>
2026-01-12 08:40:01 -0800

    lebowit 9/10, using hashset for go practice

--- linked-list-cycle.go
+++ linked-list-cycle.go
@@ -5,48 +5,14 @@
  *     Next *ListNode
  * }
  */
-//  3 2 0 4 8  7
-//  ^         /
-//   \       /
-//     -----
-// if we had 2 pointers, fast would reach nil, before slow
-// otherwise fast will hit slow
-
-// 1 2 3
-//     s
-//     f
-
-// 1 2 3 4
-//   s
-//       f
-
-// 1 2 3 4
-// 2 4 2 4
-
-// 1 2 3 4 5 6 7 8
-// 2 4 6 8 2 4 6 8
-
-// 1 2 3 4 5 6 7 
-// 2 4 6 1 3 5 7
-
-// 1
-// s
-// f
-
-// check: is fast next slow?
-// start fast at head.next, and slow at head
-// loop until fast gets to end (careful it may not be able to move 2ce); return false if this happens
-// if fa
-// 1 2 3
-//   s
-//       f
+// 1 2 1
+//     c 
+// nodes [1n, 2n]
 func hasCycle(head *ListNode) bool {
-    if head == nil { return false }
-    slow, fast := head, head.Next
-    for ; ; {
-        if fast == slow { return true }
-        if fast == nil || fast.Next == nil { break }
-        slow, fast = slow.Next, fast.Next.Next
-    }
+    nodes := map[*ListNode]struct{} {}
+    for curr := head; curr != nil; curr = curr.Next {
+        if _, ok := nodes[curr]; ok { return true }
+        nodes[curr] = struct{}{}
+    } 
     return false
 }
