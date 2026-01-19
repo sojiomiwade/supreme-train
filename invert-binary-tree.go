95945c09471589287b51f88edb8394775569db86
Soji Omiwade <omiwade@gmail.com>
2026-01-19 07:57:31 -0600

    lebowit ok but rushed; time yourself instead of rushing
    and stop at the timer.

--- invert-binary-tree.go
+++ invert-binary-tree.go
@@ -9,16 +9,46 @@
 /*
         4
 dfs: do swap left and right, and recurse on each one
-bfs: iterate through q swapping left and right
+bfs: iterate through q (or stack) swapping left and right. dont put nil in st
         1
     2-3       3-2
-              4
+                   4
+3 2
 
+            1
+             \
+               2
+             /
+            3
+
+st  []  
+top 3
+            1
+            /
+           2
+            \
+             3
 */
 func invertTree(root *TreeNode) *TreeNode {
     if root == nil {
         return nil
     }
-    root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+    st := []*TreeNode {root}
+    for ; len(st) > 0; {
+        top := st[len(st)-1]
+        st = st[:len(st)-1]
+        top.Left, top.Right = top.Right, top.Left
+        if top.Left != nil {
+            st = append(st, top.Left) 
+        }
+        if top.Right != nil {
+            st = append(st, top.Right) 
+        }
+    }
     return root
+    // if root == nil {
+    //     return nil
+    // }
+    // root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+    // return root
 }
