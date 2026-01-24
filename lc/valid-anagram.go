75ce2a5c716224b1202d2ea32eb9167197890739
Soji Omiwade <omiwade@gmail.com>
2026-01-24 08:19:32 -0800

    now using frequency map of *integers*; note that an array of 26 ints, means each element is an int!

--- lc/valid-anagram.go
+++ lc/valid-anagram.go
@@ -13,18 +13,42 @@ car rat
 ss: acr
      i
 ts: art
+
+[26]byte
+'a' - 'a'
+
+cat
+tar
+freq [a c1 t r-1]
 */
 func isAnagram(s string, t string) bool {
     if len(s) != len(t) {
         return false
     }
-    sbs, tbs := []byte(s), []byte(t)
-    slices.Sort(sbs)
-    slices.Sort(tbs)
-    for i := range sbs {
-        if sbs[i] != tbs[i] {
+    freq := [26]int{}
+    for i := range s {
+        freq[s[i]-'a']++
+        freq[t[i]-'a']--
+    }
+    for _,v := range freq {
+        if v != 0 {
             return false
         }
     }
     return true
+    // if len(s) != len(t) {
+    //     return false
+    // }
+    // sbs, tbs := []byte(s), []byte(t)
+    // bcomp := func(a, b byte) int {
+    //     return cmp.Compare(a, b)
+    // }
+    // slices.SortFunc(sbs, bcomp)
+    // slices.SortFunc(tbs, bcomp)
+    // for i := range sbs {
+    //     if sbs[i] != tbs[i] {
+    //         return false
+    //     }
+    // }
+    // return true
 }
