4bc0412f4403ad06296cb90f9ac47ae2a85286eb
Soji Omiwade <omiwade@gmail.com>
2026-01-26 11:14:46 -0800

    bfs approach thanks to kehe

--- lc/binary-tree-paths.go
+++ lc/binary-tree-paths.go
@@ -6,22 +6,42 @@
  *     Right *TreeNode
  * }
  */
-
-func btp(root *TreeNode, buf []string, ans []string) []string {
-    if root == nil {
-        return ans
-    }
-    buf = append(buf, strconv.Itoa(root.Val))
-    if root.Left == nil && root.Right == nil {
-        s := strings.Join(buf, "->")
-        fmt.Println(s)
-        ans = append(ans, s)
-    }
-    ans = btp(root.Left,buf,ans)
-    ans = btp(root.Right,buf,ans)
-    buf = buf[:len(buf)-1]
-    return ans
+type pair struct {
+    node *TreeNode
+    path []string
 }
+/*
+            1
+          /    \
+         2      3
+          \    /
+           5  6
+        ans: 125, 136
+*/
 func binaryTreePaths(root *TreeNode) []string {
-    return btp(root, []string {}, []string {})
+    q := []pair {pair{root,[]string{strconv.Itoa(root.Val)}}}
+    ans := []string {}
+    for ; len(q) > 0; {
+        pr := q[len(q)-1]
+        q = q[:len(q)-1]
+        root, rootpath := pr.node, pr.path
+        if root.Left == nil && root.Right == nil {
+            ans = append(ans, strings.Join(rootpath, "->"))
+        } else {
+            if root.Left != nil {
+                leftpath := append([]string{}, rootpath...)
+                copy(leftpath, rootpath)
+                leftpath = append(leftpath, strconv.Itoa(root.Left.Val))
+                lpr := pair {root.Left, leftpath}
+                q = append(q, lpr)
+            }
+            if root.Right != nil {
+                rightpath := append([]string{}, rootpath...)
+                rightpath = append(rightpath, strconv.Itoa(root.Right.Val))
+                rpr := pair {root.Right, rightpath}
+                q = append(q, rpr)
+            }
+        }
+    }
+    return ans
 }
