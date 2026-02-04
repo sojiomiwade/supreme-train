ef09ed9bdbfde28651eebc86759da61785468d76
Soji Omiwade <omiwade@gmail.com>
2026-02-04 09:16:54 -0800

    could use a little more rigor in walkthrough of approach before implementation

---
+++ lc/binary-tree-preorder-traversal.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# 
+#                1
+#             /     \
+#            2       3
+#          /   \      \
+#         4     5      6
+# stack for what to process "next"
+# set root to the left on each iteration, and then put the right on stack
+# if no root to process, then pop stack into root for processing
+# ans [1 2 4 5 3 6]
+# r 
+# st [6]
+
+# ans [1 2 3]
+# r x
+# s []
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        s = []
+        while root or s:
+            if not root:
+                root = s.pop()
+            else:
+                ans.append(root.val)
+                s.append(root.right)                
+                root = root.left
+        return ans
+
+        # def preo(root: Optional[TreeNode]) -> None:
+        #     if not root:
+        #         return
+        #     ans.append(root.val)
+        #     preo(root.left)
+        #     preo(root.right)
+
+        # ans = []
+        # preo(root)
+        # return ans      
