c3fb4bfbc8bdddb36f55d912139466134b393942
Soji Omiwade <omiwade@gmail.com>
2026-02-08 08:09:05 -0800

    walkthrough  and testing lacking in quality, and it showed in several mistakes, logical and accidental

--- lc/py/majority-element.py
+++ lc/py/majority-element.py
@@ -12,18 +12,33 @@
 # 000000001111 --> 32 
 # 1:2 2:5 3:1 -- O(n) time and space
 # could use a dictionary counter
-# go through each number to see if bit i is set more than the floor, if so 
-# ans gets that bit set.
+# 0010 -- 
+# go through each number to see if bit i is set: n & mask != 0
+# if more numbers than the floor have it set, then: 
+# ans gets that bit set. 1001(1)01: ans |= mask
 # for each bit i, for each number
 # 1010010 
-# mask shift by 1 to the right 31 times
-from counter import Counter
-from typing import List
+# mask shift by 1 to the right [31] times
+# -2  -3  -2
+# 110 101 110
+# mask 100
+# n    010
+# count 1
+#    ....     
+# ans 1xx
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        c = Counter(nums)
-        mv = max(c.values())
-        for i in c:
-            if c[i] == mv:
-                return i
-        raise Exception("should not be here!")
+        ans = 0
+        n = len(nums)
+        mask = 1
+        for _ in range(32):
+            count = 0
+            for num in nums:
+                if num & mask != 0:
+                    count += 1                
+            if count > n//2:
+                ans |= mask
+            mask <<= 1
+        if (1 << 31) & ans != 0:
+            ans |= (-1 << 32)
+        return ans
